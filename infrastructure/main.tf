3ebf3a79225d1d45d228d728842982539d1a1b97
abidfs <abid.sayyad@hmcts.net>
2021-11-29 18:19:11 +0000

    added terraform code to copy s2s secrets in adoption-kv keyvault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,28 @@
 provider "azurerm" {
   features {}
 }
+
+locals {
+  vaultName = "${var.product}-kv-${var.env}"
+}
+
+data "azurerm_key_vault" "adoption_key_vault" {
+  name = local.vaultName
+  resource_group_name = "${var.raw_product}-${var.env}"
+}
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "microservicekey_adoption_cos_api" {
+  name         = "microservicekey-adoption-cos-api"
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "s2s-secret-cos-api" {
+  name         = "s2s-secret-cos-api"
+  value        = data.azurerm_key_vault_secret.microservicekey_adoption_cos_api.value
+  key_vault_id = data.azurerm_key_vault.adoption_key_vault.id
+}
