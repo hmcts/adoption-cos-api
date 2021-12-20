5cc4c85bddd27938c80e674cb11cf0ad79b3e40b
Pankaj Kumar <pankaj.kumar@hmcts.net>
2021-12-20 21:49:15 +0000

    ccd change

--- bin/idam-user-token.sh
+++ bin/idam-user-token.sh
@@ -16,12 +16,12 @@ IDAM_API_URL=${IDAM_API_URL_BASE:-http://localhost:5000}
 IDAM_URL=${IDAM_STUB_LOCALHOST:-$IDAM_API_URL}
 
 clientSecret=${OAUTH2_CLIENT_SECRET}
-redirectUri=http://localhost:3001/receiver
+redirectUri=http://localhost:3000/receiver
 
 if [ -z "$IDAM_STUB_LOCALHOST" ]; then
-  code=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=adoption" -d "" | docker run --rm --interactive stedolan/jq -r .code)
+  code=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=adoption-web" -d "" | docker run --rm --interactive stedolan/jq -r .code)
 else
   code=stubbed-value
 fi
 
-curl --insecure --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" --user "adoption:${clientSecret}" "${IDAM_URL}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | docker run --rm --interactive stedolan/jq -r .access_token
+curl --insecure --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" --user "adoption-web:${clientSecret}" "${IDAM_URL}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | docker run --rm --interactive stedolan/jq -r .access_token
