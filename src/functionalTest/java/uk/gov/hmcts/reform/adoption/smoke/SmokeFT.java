0d9826398d9babc59eb5d9e721d8003c57b1364e
Pankaj Kumar <pankaj.kumar@hmcts.net>
2022-01-01 01:53:11 +0000

    test change

--- src/functionalTest/java/uk/gov/hmcts/reform/adoption/smoke/SmokeFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/adoption/smoke/SmokeFT.java
@@ -1,29 +1,24 @@
 package uk.gov.hmcts.reform.adoption.smoke;
 
+import io.restassured.RestAssured;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import io.restassured.RestAssured;
-//import org.springframework.beans.factory.annotation.Value;
 
-//@SpringBootTest()
 public class SmokeFT {
-
-    /*@Value("${test.url:http://localhost:4550}")
-    private String testUrl;*/
 
     @Test
     public void testHealthEndpoint() {
+        String testUrl = "http://ccd-data-store-api-aat.service.core-compute-aat.internal";
         Assert.assertTrue(true);
-        /*RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.useRelaxedHTTPSValidation();
         RestAssured
             .given()
             .baseUri(testUrl)
-            .get("/health")
+            .get("/")
             .then()
             .statusCode(200)
             .extract()
             .body()
-            .asString();*/
+            .asString();
     }
 }
