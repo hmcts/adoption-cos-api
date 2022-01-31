f12075d85e5a73ecf1b177e6e50dc1b5bde77d22
abidfs <abid.sayyad@hmcts.net>
2022-01-31 18:18:19 +0000

    changing application state on citizen-submit event

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CitizenSubmitApplication.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CitizenSubmitApplication.java
@@ -6,16 +6,20 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+// import uk.gov.hmcts.reform.adoption.adoptioncase.model.Application;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
 
+import java.util.List;
+
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Draft;
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Submitted;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.AwaitingPayment;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CITIZEN;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions.CREATE_READ_UPDATE;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions.READ;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.validation.ApplicationValidation.validateReadyForPayment;
 
 @Slf4j
 @Component
@@ -23,38 +27,66 @@ public class CitizenSubmitApplication implements CCDConfig<CaseData, State, User
 
     public static final String CITIZEN_SUBMIT = "citizen-submit-application";
 
-    //@Autowired
-    //private PaymentService paymentService;
+    // @Autowired
+    // private PaymentService paymentService;
 
-    /*@Autowired
-    private SubmissionService submissionService;*/
+    /*
+     * @Autowired
+     * private SubmissionService submissionService;
+     */
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
 
         configBuilder
-            .event(CITIZEN_SUBMIT)
-            .forStates(Draft, Submitted)
-            .name("Applicant Statement of Truth")
-            .description("The applicant confirms SOT")
-            .retries(120, 120)
-            .grant(CREATE_READ_UPDATE, CITIZEN)
-            .grant(READ, SUPER_USER)
-            .aboutToSubmitCallback(this::aboutToSubmit);
+                .event(CITIZEN_SUBMIT)
+                .forStates(Draft, AwaitingPayment)
+                .name("Applicant Statement of Truth")
+                .description("The applicant confirms SOT")
+                .retries(120, 120)
+                .grant(CREATE_READ_UPDATE, CITIZEN)
+                .grant(READ, SUPER_USER)
+                .aboutToSubmitCallback(this::aboutToSubmit);
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
-                                                                       CaseDetails<CaseData, State> beforeDetails) {
+            CaseDetails<CaseData, State> beforeDetails) {
         log.info("Submit application about to submit callback invoked");
 
         CaseData data = details.getData();
         State state = details.getState();
 
+        log.info("Validating case data");
+        final List<String> validationErrors = validateReadyForPayment(data);
+
+        if (!validationErrors.isEmpty()) {
+            log.info("Validation errors: ");
+            for (String error : validationErrors) {
+                log.info(error);
+            }
+
+            return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+                    .data(data)
+                    .errors(validationErrors)
+                    .state(state)
+                    .build();
+        }
+
+        // TODO uncomment this when there is help with fees flow
+        // Application application = data.getApplication();
+        // if (application.isHelpWithFeesApplication()) {
+        // var submittedDetails = submissionService.submitApplication(details);
+        // data = submittedDetails.getData();
+        // state = submittedDetails.getState();
+        // } else
+        {
+            state = AwaitingPayment;
+        }
+
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(data)
-            .state(state)
-            .build();
+                .data(data)
+                .state(state)
+                .build();
     }
 
 }
-
