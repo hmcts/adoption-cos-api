0f66b76f18ec5277d378c67d417c6d255ca60c62
dilipsamrahmcts <dilip.samra@hmcts.net>
2022-02-02 11:31:16 +0000

    Added notification files

---
+++ src/main/java/uk/gov/hmcts/reform/adoption/notification/NotificationDispatcher.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.adoption.notification;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+
+@Service
+public class NotificationDispatcher {
+    public void send(final ApplicantNotification applicantNotification, final CaseData caseData, final Long caseId) {
+
+        if (!caseData.getApplicant1().getEmailAddress().isEmpty()) {
+            applicantNotification.sendToApplicant1(caseData, caseId);
+    }
+    }
+}
