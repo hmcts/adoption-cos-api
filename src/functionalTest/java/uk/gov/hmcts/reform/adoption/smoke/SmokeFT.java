f8fadfccdbe734f0a492a670aefd6b44f3e144f6
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-02-15 14:00:51 +0000

    removed build files

--- src/functionalTest/java/uk/gov/hmcts/reform/adoption/smoke/SmokeFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/adoption/smoke/SmokeFT.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.adoption.smoke;
 
 import io.restassured.RestAssured;
-import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(SerenityRunner.class)
 public class SmokeFT {
 
     @Test
@@ -21,13 +23,5 @@ public class SmokeFT {
             .extract()
             .body()
             .asString();
-    }
-
-    @Test
-    public void dummyFT() {
-        String testUrl = "http://ccd-data-store-api-aat.service.core-compute-aat.internal";
-        SerenityRest.given()
-            .get(testUrl);
-        Assert.assertTrue(true);
     }
 }
