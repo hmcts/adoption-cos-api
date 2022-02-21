9628ec9411eee3c2870dc729eecef5c9104fb538
rajatkgupta29 <97742145+rajatkgupta29@users.noreply.github.com>
2022-02-21 15:24:34 +0000

    Update Applicant.java (#122)
    * Update Applicant.java
    
    * Update Applicant.java

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/Applicant.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/Applicant.java
@@ -72,6 +72,9 @@ public class Applicant {
     @CCD(label = "Applicant phoneNumber")
     private String phoneNumber;
 
+    @CCD(label = "The court may want to use your email to serve you court orders. Are you happy to be served court orders by email?")
+    private YesOrNo contactDetailsConsent;
+
     @CCD(label = "Nationality")
     private Set<Nationality> nationality;
 
