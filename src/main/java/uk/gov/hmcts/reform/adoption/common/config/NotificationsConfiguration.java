d9ac1694294afb866c55162c7157b6047ef8435e
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-02-28 17:45:17 +0000

    Update NotificationsConfiguration.java

--- src/main/java/uk/gov/hmcts/reform/adoption/common/config/NotificationsConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/common/config/NotificationsConfiguration.java
@@ -1,17 +1,21 @@
 package uk.gov.hmcts.reform.adoption.common.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.service.notify.NotificationClient;
 
 @Configuration
+@Slf4j
 public class NotificationsConfiguration {
     @Bean
     public NotificationClient notificationClient(
         @Value("${uk.gov.notify.api.key}") String apiKey,
         @Value("${uk.gov.notify.api.baseUrl}") final String baseUrl
     ) {
+
+        log.info("uk.gov.notify.api.key", apiKey);
         return new NotificationClient(apiKey, baseUrl);
     }
 }
