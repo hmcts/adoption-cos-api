4b4e7cc5916102ecc636343153c16195788c40b6
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-03-01 15:04:40 +0000

    Update RootController.java

--- src/main/java/uk/gov/hmcts/reform/adoption/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/controllers/RootController.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.reform.adoption.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.adoption.notification.ApplicationSubmittedNotification;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -16,9 +14,6 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 @Slf4j
 public class RootController {
-
-    @Autowired
-    private ApplicationSubmittedNotification applicationSubmittedNotification;
 
     @Value("azure.application-insights.instrumentation-key")
     String appInsights;
