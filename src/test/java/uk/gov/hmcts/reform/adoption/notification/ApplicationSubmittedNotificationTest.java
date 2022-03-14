6fd44b358a9d5bd4baa87109e50ebf757d4e4e3f
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-03-14 16:19:45 +0000

    Update ApplicationSubmittedNotificationTest.java

--- src/test/java/uk/gov/hmcts/reform/adoption/notification/ApplicationSubmittedNotificationTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/notification/ApplicationSubmittedNotificationTest.java
@@ -38,9 +38,16 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.LanguagePreference.ENGLISH;
+import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.HYPHENATED_REF;
+import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.NO;
+import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.YES;
 import static uk.gov.hmcts.reform.adoption.notification.CommonContent.APPLICATION_REFERENCE;
 import static uk.gov.hmcts.reform.adoption.notification.CommonContent.SUBMISSION_RESPONSE_DATE;
 import static uk.gov.hmcts.reform.adoption.notification.EmailTemplateName.APPLICANT_APPLICATION_SUBMITTED;
+import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.APPLICANT_1_FULL_NAME;
+import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.APPLICANT_2_FULL_NAME;
+import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.HAS_SECOND_APPLICANT;
+import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.LOCAL_COURT_NAME;
 import static uk.gov.hmcts.reform.adoption.testutil.TestConstants.TEST_USER_EMAIL;
 import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.caseData;
 import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.getMainTemplateVars;
@@ -68,15 +75,28 @@ class ApplicationSubmittedNotificationTest {
 
     @Test
     void shouldSendEmailToApplicantsWithSubmissionResponseDate() {
-        CaseData data = caseData();
-        data.setDueDate(LocalDate.of(2021, 4, 21));
-        when(commonContent.mainTemplateVars(data, 1234567890123456L, data.getApplicant1(), data.getApplicant2()))
+        CaseData caseData = caseData();
+        caseData.setDueDate(LocalDate.of(2021, 4, 21));
+        when(commonContent.mainTemplateVars(caseData, 1234567890123456L, caseData.getApplicant1(), caseData.getApplicant2()))
             .thenReturn(getMainTemplateVars());
-
-        notification.sendToApplicants(data, 1234567890123456L);
+        caseData.setFamilyCourtName(StringUtils.EMPTY);
         Map<String, Object> templateVars = new HashMap<>();
-        templateVars.put(APPLICATION_REFERENCE, "1234-5678-9012-3456");
-        templateVars.put(SUBMISSION_RESPONSE_DATE, "21 April 2021");
+        templateVars.put(HYPHENATED_REF, caseData.getHyphenatedCaseRef());
+        templateVars.put(APPLICANT_1_FULL_NAME, caseData.getApplicant1().getFirstName() + " "
+            + caseData.getApplicant1().getLastName());
+        templateVars.put(LOCAL_COURT_NAME, caseData.getFamilyCourtName());
+        if (caseData.getApplicant2() != null) {
+            templateVars.put(
+                APPLICANT_2_FULL_NAME,
+                caseData.getApplicant2().getFirstName() + " " + caseData.getApplicant2().getLastName()
+            );
+            templateVars.put(HAS_SECOND_APPLICANT, YES);
+        } else {
+            templateVars.put(HAS_SECOND_APPLICANT, NO);
+            templateVars.put(APPLICANT_2_FULL_NAME, StringUtils.EMPTY);
+        }
+
+        notification.sendToApplicants(caseData, 1234567890123456L);
 
         verify(notificationService, times(2)).sendEmail(
             eq(TEST_USER_EMAIL),
@@ -84,7 +104,7 @@ class ApplicationSubmittedNotificationTest {
             eq(templateVars),
             eq(ENGLISH)
         );
-        verify(commonContent).mainTemplateVars(data, 1234567890123456L, data.getApplicant1(), data.getApplicant2());
+        verify(commonContent).mainTemplateVars(caseData, 1234567890123456L, caseData.getApplicant1(), caseData.getApplicant2());
     }
 
     @Test
