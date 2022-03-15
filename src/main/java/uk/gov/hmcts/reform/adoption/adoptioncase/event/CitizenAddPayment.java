afc873a21a920f971cdedd9f9e5dd141d47b6f11
rajatkgupta29 <97742145+rajatkgupta29@users.noreply.github.com>
2022-03-15 12:47:53 +0000

    Adop 455 (#164)
    * added updated case data to notification task
    
    * Update SubmissionService.java
    
    * Added new service
    
    * Update SubmissionService.java
    
    * Update SubmissionServiceTest.java
    
    * Update SubmissionServiceTest.java

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CitizenAddPayment.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CitizenAddPayment.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.adoption.common.service.SendNotificationService;
 import uk.gov.hmcts.reform.adoption.common.service.SubmissionService;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
@@ -34,6 +35,9 @@ public class CitizenAddPayment implements CCDConfig<CaseData, State, UserRole> {
 
     @Autowired
     private SubmissionService submissionService;
+
+    @Autowired
+    private SendNotificationService sendNotificationService;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -87,9 +91,16 @@ public class CitizenAddPayment implements CCDConfig<CaseData, State, UserRole> {
 
         final CaseDetails<CaseData, State> updatedCaseDetails = submissionService.submitApplication(details);
 
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+        AboutToStartOrSubmitResponse.<CaseData, State>builder()
             .data(updatedCaseDetails.getData())
             .state(updatedCaseDetails.getState())
+            .build();
+
+        final CaseDetails<CaseData, State> notificationSentUpdatedDetails = sendNotificationService.sendNotifications(updatedCaseDetails);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(notificationSentUpdatedDetails.getData())
+            .state(notificationSentUpdatedDetails.getState())
             .build();
     }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/adoption/common/service/SendNotificationService.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.adoption.common.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
+import uk.gov.hmcts.reform.adoption.adoptioncase.task.CaseTaskRunner;
+import uk.gov.hmcts.reform.adoption.common.service.task.SendCitizenSubmissionNotifications;
+
+@Service
+public class SendNotificationService {
+
+
+    @Autowired
+    private SendCitizenSubmissionNotifications sendCitizenSubmissionNotifications;
+
+    public CaseDetails<CaseData, State> sendNotifications(final CaseDetails<CaseData, State> caseDetails) {
+        return CaseTaskRunner.caseTasks(sendCitizenSubmissionNotifications).run(caseDetails);
+    }
+}
