61329a6d687e8a94f695febfeaedc81d58aadfa1
pankajhmcts <94961980+pankajhmcts@users.noreply.github.com>
2022-04-25 12:15:43 +0100

    sync R2master with master branch (#226)
    * Addev spring CVE fix (#200)
    
    * Addev spring CVE fix
    
    * updated smoke test
    
    * updated test
    
    * Upgraded jar versions
    
    * Bump spring-aspects from 5.3.17 to 5.3.18 (#203)
    
    Bumps [spring-aspects](https://github.com/spring-projects/spring-framework) from 5.3.17 to 5.3.18.
    - [Release notes](https://github.com/spring-projects/spring-framework/releases)
    - [Commits](https://github.com/spring-projects/spring-framework/compare/v5.3.17...v5.3.18)
    
    ---
    updated-dependencies:
    - dependency-name: org.springframework:spring-aspects
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: pankajhmcts <94961980+pankajhmcts@users.noreply.github.com>
    
    * renamed docmosis templates (#201)
    
    * updated audit.json (#207)
    
    * Nightly build zap fix (#209)
    
    * updated audit.json
    
    * test
    
    * updated swaggerconfig
    
    * updated version to v3
    
    * updated audit.json
    
    * Nightly build zap fix (#210)
    
    * updated audit.json
    
    * test
    
    * updated swaggerconfig
    
    * updated version to v3
    
    * updated audit.json
    
    * Updated audit.json
    
    * Nightly build zap fix (#211)
    
    * updated audit.json
    
    * test
    
    * updated swaggerconfig
    
    * updated version to v3
    
    * updated audit.json
    
    * Updated audit.json
    
    * updated audit.json
    
    * enabled high level data setup for prod (#212)
    
    * Prod readiness (#216)
    
    * enabled high level data setup for prod
    
    * removed prod check
    
    * Nightly build zap fix (#209)
    
    * updated audit.json
    
    * test
    
    * updated swaggerconfig
    
    * updated version to v3
    
    * updated audit.json
    
    * Nightly build zap fix (#210)
    
    * updated audit.json
    
    * test
    
    * updated swaggerconfig
    
    * updated version to v3
    
    * updated audit.json
    
    * Updated audit.json
    
    * Nightly build zap fix (#211)
    
    * updated audit.json
    
    * test
    
    * updated swaggerconfig
    
    * updated version to v3
    
    * updated audit.json
    
    * Updated audit.json
    
    * updated audit.json
    
    Co-authored-by: pankajhmcts <94961980+pankajhmcts@users.noreply.github.com>
    
    * sonar fixes (#217)
    
    * added prod definition generate logic (#218)
    
    * updated charts
    
    * Bumping chart version
    
    * updated suppressions.xml
    
    * updated charts (#221)
    
    * updated charts
    
    * Bumping chart version
    
    * updated suppressions.xml
    
    Co-authored-by: hmcts-jenkins-cft <93778408+hmcts-jenkins-cft[bot]@users.noreply.github.com>
    
    * Health url modification (#222)
    
    * sonar fixes
    
    * Update values.yaml
    
    * updated suppressions.xml
    
    * upadated build.gradle
    
    * updated suppressions.xml
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: rajatkgupta29 <97742145+rajatkgupta29@users.noreply.github.com>
    Co-authored-by: abidfs <abid.sayyad@hmcts.net>
    Co-authored-by: hmcts-jenkins-cft <93778408+hmcts-jenkins-cft[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/adoption/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/config/SwaggerConfiguration.java
@@ -1,26 +1,24 @@
 package uk.gov.hmcts.reform.adoption.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.adoption.Application;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .useDefaultResponseMessages(false)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
-            .paths(PathSelectors.any())
-            .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info().title("Adoption Case Orchestration Service API")
+                      .version("v1.0.0")
+                      .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")))
+            .externalDocs(new ExternalDocumentation()
+                              .description("README")
+                              .url("https://github.com/hmcts/adoption-cos-api"));
     }
 
 }
