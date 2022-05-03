232c80348a787e5cd92e659a121476ca7a7f4e2b
Praneeth G <praneethkumar.gadhe@hmcts.net>
2022-05-03 09:02:51 +0100

    incoraprated review comments

--- src/main/java/uk/gov/hmcts/reform/adoption/Application.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/Application.java
@@ -8,7 +8,6 @@ import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import uk.gov.hmcts.reform.adoption.document.CaseDocumentClient;
 import uk.gov.hmcts.reform.adoption.document.DocAssemblyClient;
-import uk.gov.hmcts.reform.adoption.document.DocumentManagementClient;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.ccd.client.CaseAssignmentApi;
 import uk.gov.hmcts.reform.ccd.client.CaseUserApi;
@@ -29,7 +28,6 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
         DocAssemblyClient.class,
         CoreCaseDataApi.class,
         CaseAssignmentApi.class,
-        DocumentManagementClient.class,
         CaseDocumentClientApi.class,
         CaseDocumentClient.class
     }
