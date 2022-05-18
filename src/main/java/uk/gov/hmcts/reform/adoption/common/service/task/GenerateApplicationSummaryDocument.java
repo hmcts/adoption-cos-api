82cee409c72083a361450d0a05f49bc590a02dee
Pankaj Kumar <pankaj.kumar@hmcts.net>
2022-05-18 23:43:08 +0100

    Revert "added try/catch in generateapplication"
    This reverts commit 7b1522168a2cd01aeefe1e1b64638ee1925778d6.

--- src/main/java/uk/gov/hmcts/reform/adoption/common/service/task/GenerateApplicationSummaryDocument.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/common/service/task/GenerateApplicationSummaryDocument.java
@@ -28,7 +28,7 @@ import static uk.gov.hmcts.reform.adoption.document.DocumentUtil.formatDocumentN
 
 @Component
 @Slf4j
-public class GenerateApplicationSummaryDocument implements CaseTask {
+public class GenerateApplicationSummaryDocument  implements CaseTask {
 
     @Autowired
     private CaseDataDocumentService caseDataDocumentService;
@@ -38,68 +38,57 @@ public class GenerateApplicationSummaryDocument implements CaseTask {
 
     @Override
     public CaseDetails<CaseData, State> apply(CaseDetails<CaseData, State> caseDetails) {
-        try {
-            final CaseData caseData = caseDetails.getData();
-            final Long caseId = caseDetails.getId();
-            final State state = caseDetails.getState();
+        final CaseData caseData = caseDetails.getData();
+        final Long caseId = caseDetails.getId();
+        final State state = caseDetails.getState();
 
-            @SuppressWarnings("unchecked")
-            Map<String, Object> templateContent = objectMapper.convertValue(caseData, Map.class);
-            if (caseData.getBirthMother() != null && caseData.getBirthMother().getNationality() != null) {
-                templateContent.put("birthMotherNationality", caseData.getBirthMother().getNationality().stream()
-                    .filter(item -> item != Nationality.OTHER).collect(
-                        Collectors.toCollection(TreeSet<Nationality>::new)));
-            }
-            if (caseData.getBirthFather() != null && caseData.getBirthFather().getNationality() != null) {
-                templateContent.put("birthFatherNationality", caseData.getBirthFather().getNationality().stream()
-                    .filter(item -> item != Nationality.OTHER).collect(
-                        Collectors.toCollection(TreeSet<Nationality>::new)));
-            }
-            if (caseData.getChildren() != null && caseData.getChildren().getNationality() != null) {
-                templateContent.put("childrenNationality", caseData.getChildren().getNationality().stream()
-                    .filter(item -> item != Nationality.OTHER).collect(
-                        Collectors.toCollection(TreeSet<Nationality>::new)));
-            }
-
-            if (EnumSet.of(Submitted).contains(state)) {
-                log.info("Generating summary document for caseId: {}", caseId);
-
-                final CompletableFuture<Void> appSummaryEn = CompletableFuture
-                    .runAsync(() -> caseDataDocumentService.renderDocumentAndUpdateCaseData(
-                        caseData,
-                        APPLICATION_SUMMARY_EN,
-                        templateContent,
-                        caseDetails.getId(),
-                        ADOPTION_APPLICATION_SUMMARY,
-                        LanguagePreference.ENGLISH,
-                        formatDocumentName(
-                            caseDetails.getId(),
-                            ADOPTION_APPLICATION_FILE_NAME,
-                            LocalDateTime.now()
-                        )
-                    ));
-                final CompletableFuture<Void> appSummaryCy = CompletableFuture
-                    .runAsync(() -> caseDataDocumentService.renderDocumentAndUpdateCaseData(
-                        caseData,
-                        APPLICATION_SUMMARY_CY,
-                        templateContent,
-                        caseDetails.getId(),
-                        ADOPTION_APPLICATION_SUMMARY,
-                        LanguagePreference.WELSH,
-                        formatDocumentName(
-                            caseDetails.getId(),
-                            ADOPTION_APPLICATION_FILE_NAME,
-                            LocalDateTime.now()
-                        )
-                    ));
-
-                CompletableFuture.allOf(appSummaryEn, appSummaryCy).join();
-            } else {
-                log.error("Could not generate summary document for caseId: {}", caseId);
-            }
-        } catch (Exception e) {
-            log.error("Error occurred : " + e.getMessage());
+        @SuppressWarnings("unchecked")
+        Map<String, Object> templateContent = objectMapper.convertValue(caseData, Map.class);
+        if (caseData.getBirthMother() != null && caseData.getBirthMother().getNationality() != null) {
+            templateContent.put("birthMotherNationality", caseData.getBirthMother().getNationality().stream()
+                .filter(item -> item != Nationality.OTHER).collect(
+                Collectors.toCollection(TreeSet<Nationality>::new)));
         }
+        if (caseData.getBirthFather() != null && caseData.getBirthFather().getNationality() != null) {
+            templateContent.put("birthFatherNationality", caseData.getBirthFather().getNationality().stream()
+                .filter(item -> item != Nationality.OTHER).collect(
+                Collectors.toCollection(TreeSet<Nationality>::new)));
+        }
+        if (caseData.getChildren() != null && caseData.getChildren().getNationality() != null) {
+            templateContent.put("childrenNationality", caseData.getChildren().getNationality().stream()
+                .filter(item -> item != Nationality.OTHER).collect(
+                Collectors.toCollection(TreeSet<Nationality>::new)));
+        }
+
+        if (EnumSet.of(Submitted).contains(state)) {
+            log.info("Generating summary document for caseId: {}", caseId);
+
+            final CompletableFuture<Void> appSummaryEn = CompletableFuture
+                .runAsync(() -> caseDataDocumentService.renderDocumentAndUpdateCaseData(caseData,
+                                                                                        APPLICATION_SUMMARY_EN,
+                                                                                        templateContent,
+                                                                                        caseDetails.getId(),
+                                                                                        ADOPTION_APPLICATION_SUMMARY,
+                                                                                        LanguagePreference.ENGLISH,
+                                                                                        formatDocumentName(caseDetails.getId(),
+                                                                                        ADOPTION_APPLICATION_FILE_NAME,
+                                                                                        LocalDateTime.now())));
+            final CompletableFuture<Void> appSummaryCy = CompletableFuture
+                .runAsync(() -> caseDataDocumentService.renderDocumentAndUpdateCaseData(caseData,
+                                                                                        APPLICATION_SUMMARY_CY,
+                                                                                        templateContent,
+                                                                                        caseDetails.getId(),
+                                                                                        ADOPTION_APPLICATION_SUMMARY,
+                                                                                        LanguagePreference.WELSH,
+                                                                                        formatDocumentName(caseDetails.getId(),
+                                                                                        ADOPTION_APPLICATION_FILE_NAME,
+                                                                                        LocalDateTime.now())));
+
+            CompletableFuture.allOf(appSummaryEn, appSummaryCy).join();
+        } else {
+            log.error("Could not generate summary document for caseId: {}", caseId);
+        }
+
         return caseDetails;
     }
 }
