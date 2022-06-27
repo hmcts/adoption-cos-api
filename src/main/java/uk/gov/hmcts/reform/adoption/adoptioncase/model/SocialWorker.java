a32510521055f72a152e90ff979ac2236d57abdc
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-06-27 14:48:22 +0100

    added applicant social worker

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/SocialWorker.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/SocialWorker.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.adoption.adoptioncase.model;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +12,6 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.Email;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 @Builder
 public class SocialWorker {
 
