a7dbc71281bfb1a512d01d7ba1faa18795c9cfee
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-07-12 16:21:45 +0100

    Updated properties

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/Sibling.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/Sibling.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.SystemUpdateAccess;
+
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
 
 @Data
 @AllArgsConstructor
@@ -14,10 +17,16 @@ public class Sibling {
     @CCD(label = "Sibling Id")
     private String siblingId;
 
-    @CCD(label = "Sibling Relation")
+    @CCD(label = "Sibling Relation",
+        access = {SystemUpdateAccess.class},
+        typeOverride = FixedList,
+        typeParameterOverride = "SiblingRelation")
     private SiblingRelation siblingRelation;
 
-    @CCD(label = "Sibling Placement Order Type")
+    @CCD(label = "Sibling Placement Order Type",
+        access = {SystemUpdateAccess.class},
+        typeOverride = FixedList,
+        typeParameterOverride = "SiblingPoType")
     private SiblingPoType siblingPoType;
 
     @CCD(label = "Sibling Placement Other Order Type")
