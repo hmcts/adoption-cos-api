50c77910c21f10b0b653d37ea1ed0ebff69994ce
pankajhmcts <94961980+pankajhmcts@users.noreply.github.com>
2022-07-19 23:46:24 +0100

    Sync r2master (#353)
    * ADOP-817 implemented changes for cdam integration (#321)
    
    * Suppressed CVEs (#350)
    
    Co-authored-by: praneethhm <101885945+praneethhm@users.noreply.github.com>
    Co-authored-by: priteshkr <101888106+priteshkr@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/access/DefaultAccess.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/access/DefaultAccess.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER;
+// import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER_SYSTEM_UPDATE;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CITIZEN;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.COURT_ADMIN;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CREATOR;
@@ -29,6 +30,7 @@ public class DefaultAccess implements HasAccessControl {
         grants.putAll(CREATOR, Permissions.CREATE_READ_UPDATE);
         grants.putAll(CITIZEN, Permissions.CREATE_READ_UPDATE);
         grants.putAll(SYSTEM_UPDATE, Permissions.READ);
+        // grants.putAll(CASE_WORKER_SYSTEM_UPDATE, Permissions.READ);
 
         return grants;
     }
