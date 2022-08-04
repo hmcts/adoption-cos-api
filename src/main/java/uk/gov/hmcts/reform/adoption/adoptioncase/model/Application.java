336458661d471c0b6f8cec98fe8a377321f66ef4
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-08-04 22:56:15 +0100

    Java-17 Upgrade changes (#358)
    Co-authored-by: Pankaj Kumar <pankaj.kumar@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/Application.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/Application.java
@@ -38,8 +38,7 @@ public class Application {
     @CCD(label = "Here are your order details", access = { CollectionAccess.class })
     private OrderSummary applicationFeeOrderSummary;
 
-    @CCD(label = "Payment Method", typeOverride = Collection, typeParameterOverride = "Payment", access = {
-            CollectionAccess.class })
+    @CCD(label = "Payment Method", typeOverride = Collection, typeParameterOverride = "Payment", access = { CollectionAccess.class })
     private List<ListValue<Payment>> applicationPayments;
 
     @JsonIgnore
