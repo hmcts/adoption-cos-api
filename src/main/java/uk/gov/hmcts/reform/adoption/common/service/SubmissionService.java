391b2e422237cbe1d98492bc133fddb5d139d0d5
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-08-12 08:36:02 +0100

    Dummy Commit

--- src/main/java/uk/gov/hmcts/reform/adoption/common/service/SubmissionService.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/common/service/SubmissionService.java
@@ -30,5 +30,7 @@ public class SubmissionService {
             setStateAfterSubmission,
             setDateSubmitted// TODO, generateApplicationSummaryDocument
         ).run(caseDetails);
+
+        
     }
 }
