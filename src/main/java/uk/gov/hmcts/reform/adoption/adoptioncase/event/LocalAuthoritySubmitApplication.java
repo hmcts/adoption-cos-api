eca7208a2b51479b4f3f71d167d05205bf1f169f
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-08-16 04:47:58 +0100

    Added logic to make LA submitted state update

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/LocalAuthoritySubmitApplication.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/LocalAuthoritySubmitApplication.java
@@ -14,6 +14,7 @@ import uk.gov.hmcts.reform.adoption.common.service.SendNotificationService;
 import uk.gov.hmcts.reform.adoption.common.service.SubmissionService;
 
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.AwaitingAdminChecks;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.LaSubmitted;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions.READ;
@@ -54,7 +55,7 @@ public class LocalAuthoritySubmitApplication implements CCDConfig<CaseData, Stat
             updatedCaseDetails);
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
             .data(notificationSentUpdatedDetails.getData())
-            .state(notificationSentUpdatedDetails.getState())
+            .state(LaSubmitted)
             .build();
 
     }
