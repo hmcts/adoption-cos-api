123b0a6a9151c8a9be6b62a3f0303d0719d4e877
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-08-24 17:00:28 +0100

    Removed AboutToSubmit callback

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/CaseworkerCaseNote.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/CaseworkerCaseNote.java
@@ -3,9 +3,7 @@ package uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page.AddCaseNote;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
@@ -13,8 +11,6 @@ import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions;
 import uk.gov.hmcts.reform.adoption.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
-
-import java.util.List;
 
 @Slf4j
 @Component
@@ -39,24 +35,24 @@ public class CaseworkerCaseNote implements CCDConfig<CaseData, State, UserRole> 
         );
         return new PageBuilder(configBuilder
                                    .event(CASEWORKER_ADD_CASE_NOTE)
-                                   .forStates(State.Draft)
+                                   .forAllStates()
                                    .name(ADD_CASE_NOTE)
                                    .description(ADD_CASE_NOTE)
                                    .showSummary()
-                                   .grant(Permissions.CREATE_READ_UPDATE, UserRole.CASE_WORKER)
-                                   .aboutToSubmitCallback(this::aboutToSubmit));
+                                   .grant(Permissions.CREATE_READ_UPDATE, UserRole.CASE_WORKER));
     }
 
-    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
+    /*public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
                                                                        CaseDetails<CaseData, State> beforeDetails) {
 
         log.info("{} about to submit callback invoked for Case Id: {}", CASEWORKER_ADD_CASE_NOTE, details.getId());
         CaseData caseData = details.getData();
 
         if (caseData.getCaseNote() != null) {
+            log.info("---------------Inside Not Null Condition------------");
             if (caseData.getCaseNote().getNote() != null
-                && !caseData.getCaseNote().getNote().isEmpty()
-                && !caseData.getCaseNote().getNote().isBlank()) {
+                && caseData.getCaseNote().getNote().isEmpty()) {
+                log.info("---------------Inside Not Null Condition of Note------------");
                 return AboutToStartOrSubmitResponse.<CaseData, State>builder()
                     .data(caseData)
                     .errors(List.of("Enter details for the case note"))
@@ -64,8 +60,9 @@ public class CaseworkerCaseNote implements CCDConfig<CaseData, State, UserRole> 
             }
 
             if (caseData.getCaseNote().getSubject() != null
-                && !caseData.getCaseNote().getSubject().isEmpty()
-                && !caseData.getCaseNote().getSubject().isBlank()) {
+                && caseData.getCaseNote().getSubject().isEmpty()
+                && caseData.getCaseNote().getSubject().isBlank()) {
+                log.info("---------------Inside Not Null Condition of Subject------------");
                 return AboutToStartOrSubmitResponse.<CaseData, State>builder()
                     .data(caseData)
                     .errors(List.of("Enter a subject for the case note"))
@@ -77,6 +74,6 @@ public class CaseworkerCaseNote implements CCDConfig<CaseData, State, UserRole> 
             .data(caseData)
             .build();
 
-    }
+    }*/
 
 }
