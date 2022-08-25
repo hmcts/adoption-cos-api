b38f2e9da6b0bf2b3dbd6be2bafeeb1604a958ef
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-08-25 14:11:40 +0100

    Changed display order

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/CaseNote.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/CaseNote.java
@@ -14,11 +14,13 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 @Builder
 public class CaseNote {
 
-    @CCD(label = "Subject")
+    @CCD(label = "Subject",
+        displayOrder = 0)
     private String subject;
 
     @CCD(label = "Note",
         hint = "Include relevant dates and the people involved.",
-        typeOverride = TextArea)
+        typeOverride = TextArea,
+        displayOrder = 1)
     private String note;
 }
