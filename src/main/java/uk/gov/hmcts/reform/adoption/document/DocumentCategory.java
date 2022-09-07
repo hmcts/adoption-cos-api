1c4101195aa7ed05d01108c690a5025fc3efe4f9
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-09-07 11:05:33 +0100

    Added hint to radio button

--- src/main/java/uk/gov/hmcts/reform/adoption/document/DocumentCategory.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/document/DocumentCategory.java
@@ -9,22 +9,23 @@ import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 @AllArgsConstructor
 public enum DocumentCategory implements HasLabel {
     @JsonProperty("applicationDocuments")
-    APPLICATION_DOCUMENTS("Application documents"),
+    APPLICATION_DOCUMENTS("Application documents", "For example, birth or death certificates."),
 
     @JsonProperty("courtOrders")
-    COURT_ORDERS("Court orders"),
+    COURT_ORDERS("Court orders","For example placement and care orders."),
 
     @JsonProperty("reports")
-    REPORTS("Reports"),
+    REPORTS("Reports","ABC"),
 
     @JsonProperty("statements")
-    STATEMENTS("Statements"),
+    STATEMENTS("Statements","dfdsfds"),
 
     @JsonProperty("correspondence")
-    CORRESPONDENCE("Correspondence"),
+    CORRESPONDENCE("Correspondence","dfsdvcsd"),
 
     @JsonProperty("additionalDocuments")
-    ADDITIONAL_DOCUMENTS("Additional documents");
+    ADDITIONAL_DOCUMENTS("Additional documents","efdwef");
 
     private final String label;
+    private final String hint;
 }
