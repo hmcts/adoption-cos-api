1d4f7bff58a9a89467b7917d62b09ca45ede1b10
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-09-09 00:31:35 +0100

    adding the sequence

--- src/main/java/uk/gov/hmcts/reform/adoption/common/service/task/SetDateSubmitted.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/common/service/task/SetDateSubmitted.java
@@ -33,7 +33,7 @@ public class SetDateSubmitted implements CaseTask {
             || AwaitingHWFDecision.equals(state)*/) {
 
             caseData.getApplication().setDateSubmitted(LocalDate.now());
-            caseData.getApplication().setTimetable20week(LocalDate.now().plusWeeks(20));
+            caseData.getApplication().setTimetable20Week(LocalDate.now().plusWeeks(20));
             caseData.setDueDate(caseData.getApplication().getDateOfSubmissionResponse());
         }
 
