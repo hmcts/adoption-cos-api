869da6cde8aac3eb724df809149c113ea09d5e87
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-09-08 16:24:11 +0100

    adding the sequence

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/tab/CaseTypeTab.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/tab/CaseTypeTab.java
@@ -145,16 +145,6 @@ public class CaseTypeTab implements CCDConfig<CaseData, State, UserRole> {
             .field("siblings");
     }
 
-//    private void buildConfidentialTab(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-//        configBuilder.tab("Confidential", "Confidential Details")
-//            .forRoles(CASE_WORKER)
-//            .field("applicant1PhoneNumber")
-//            .field("applicant1EmailAddress")
-//            .field("childrenFirstName")
-//            .field("childrenLastName")
-//            .field("applyingWith");
-//    }
-
     private void buildDocumentsTab(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         configBuilder.tab("documents", "Documents")
             .forRoles(CASE_WORKER)
