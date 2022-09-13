c29021024b9d8188240aa1ca747e8ee9fcbce76d
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-09-13 09:18:44 +0100

    FT for add a case note

--- src/functionalTest/java/uk/gov/hmcts/reform/adoption/caseworker/CaseworkerAddNoteFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/adoption/caseworker/CaseworkerAddNoteFT.java
@@ -1,7 +1,22 @@
 package uk.gov.hmcts.reform.adoption.caseworker;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.adoption.testutil.FunctionalTest;
+
+import java.util.Map;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.CaseworkerCaseNote.CASEWORKER_ADD_CASE_NOTE;
+import static uk.gov.hmcts.reform.adoption.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
+import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.expectedResponse;
 
 
 @SpringBootTest
@@ -9,19 +24,21 @@ public class CaseworkerAddNoteFT extends FunctionalTest {
 
     private static final String REQUEST = "classpath:casedata/ccd-callback-casedata-application-payment-ready.json";
 
-    private static final String RESPONSE = "classpath:casedata/response-application-payment-ready.json";
+    private static final String RESPONSE = "classpath:casedata/response/response-caseworker-add-notes-about-to-submit.json";
 
-    /*@Test
+    @Test
     public void shouldUpdateCaseDataWithNotesWhenAboutToSubmitCallbackIsInvoked() throws Exception {
         Map<String, Object> request = caseData(REQUEST);
 
         Response response = triggerCallback(request, CASEWORKER_ADD_CASE_NOTE, ABOUT_TO_SUBMIT_URL);
 
-        *//*assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
 
+        System.out.println("<<<<<<<<<<STRING>>>>>>>> "+response.asString());
+        System.out.println("<<<<<<<<<<JSON>>>>>>>> "+json(response.asString()));
         assertThatJson(response.asString())
             .when(IGNORING_EXTRA_FIELDS)
             .when(IGNORING_ARRAY_ORDER)
-            .isEqualTo(json(expectedResponse(RESPONSE)));*//*
-    }*/
+            .isEqualTo(json(expectedResponse(RESPONSE)));
+    }
 }
