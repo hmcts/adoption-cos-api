594d87bf5daf25c63a3644c2c5ea88678b870757
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-09-14 12:39:13 +0100

    Update ManageDocuments.java

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/ManageDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/ManageDocuments.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page;
 
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
-import uk.gov.hmcts.reform.adoption.adoptioncase.model.OtherParty;
 import uk.gov.hmcts.reform.adoption.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
-import uk.gov.hmcts.reform.adoption.document.DocumentSubmitter;
 import uk.gov.hmcts.reform.adoption.document.model.AdoptionDocument;
 
 import static uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.CaseworkerUploadDocument.MANAGE_DOCUMENT;
@@ -26,15 +24,11 @@ public class ManageDocuments implements CcdPageConfiguration {
         pageBuilder.page("uploadDocumentPage2")
             .pageLabel("Who submitted the document?")
             .complex(CaseData::getAdoptionDocument)
-            .complex(AdoptionDocument::getDocumentSubmitter)
-            .mandatory(DocumentSubmitter::getDocumentSubmittedBy)
+            .mandatory(AdoptionDocument::getDocumentSubmitter)
             .done();
         pageBuilder.page("uploadDocumentPage2")
             .complex(CaseData::getAdoptionDocument)
-            .complex(AdoptionDocument::getDocumentSubmitter)
-            .complex(DocumentSubmitter::getOtherParty)
-            .mandatory(OtherParty::getOtherPartyName, "documentSubmittedBy=\"otherParty\"")
-            .mandatory(OtherParty::getOtherPartyRole, "documentSubmittedBy=\"otherParty\"")
+            .mandatory(AdoptionDocument::getDocumentSubmitter)
             .done();
 
     }
