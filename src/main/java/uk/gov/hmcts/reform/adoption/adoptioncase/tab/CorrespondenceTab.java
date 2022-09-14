ad04e8b097ab254d145c13209a30cd149d7a1274
ShakiraKP <103694861+ShakiraKP@users.noreply.github.com>
2022-09-14 08:14:44 +0100

    Create CorrespondenceTab.java

---
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/tab/CorrespondenceTab.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.adoption.adoptioncase.tab;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.Tab;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
+
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER;
+
+@Component
+public class CorrespondenceTab implements CCDConfig<CaseData, State, UserRole> {
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        final Tab.TabBuilder<CaseData, UserRole> tabBuilder = configBuilder.tab("applicationCorrespondence", "Correspondence")
+            .forRoles(CASE_WORKER);
+        addCorrespondence(tabBuilder);
+    }
+
+
+    private void addCorrespondence(final Tab.TabBuilder<CaseData, UserRole> tabBuilder) {
+        tabBuilder
+            .label("LabelNotes-Correspondence", null, "### Correspondence")
+            .label("Upload correspondence",
+                   null,
+                   "[Upload documents](/cases/case-details/${[CASE_REFERENCE]}"
+                       + "/trigger/caseworker-upload-document/caseworker-upload-documentuploadDocument)"
+            );
+    }
+}
+
