e9fe4f1c9e4572e9851f6c52be8e9f0c3c865775
Raghu Goud <nraghu31@gmail.com>
2022-09-14 16:16:41 +0100

    XUI Automation framework

--- src/e2e/tests/smote_test.js
+++ src/e2e/tests/smote_test.js
@@ -3,9 +3,9 @@ const config = require('../config');
 const laHelper = require('../helpers/la_portal_case');
 
 
-Feature('Smoke tests').retry(1);
+Feature('Create case @smoke').retry(1);
 
-Scenario('Verify applicant eligible to adopt', async ({I,loginPage, caseListPage }) => {
+Scenario('Verify case created and able to search in ExUI', async ({I,loginPage, caseListPage }) => {
 
   console.log('Smoke test triggered now');
   const caseID=await laHelper.createCompleteCase();
