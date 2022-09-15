df9c4f6461f936fd71743a2dce28d2f2272e694c
Sairam Kantheti <sai.kantheti@hmcts.net>
2022-09-15 11:32:43 +0100

    ADOP-1259 changes

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/CaseworkerAmendOtherPartiesDetails.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/CaseworkerAmendOtherPartiesDetails.java
@@ -6,8 +6,6 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page.AddCaseNote;
 import uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page.AmendOtherPartiesDetails;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
@@ -15,14 +13,10 @@ import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions;
 import uk.gov.hmcts.reform.adoption.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
-import uk.gov.hmcts.reform.adoption.document.model.AdoptionDocument;
-
-import java.util.UUID;
-
 
 @Component
 @Slf4j
-public class CaseworkerAmendOtherPartiesDetails implements CCDConfig<CaseData, State, UserRole>{
+public class CaseworkerAmendOtherPartiesDetails implements CCDConfig<CaseData, State, UserRole> {
     public static final String CASEWORKER_AMEND_OTHER_PARTIES_DETAILS = "caseworker-amend-other-parties-details";
     public static final String AMEND_OTHER_PARTIES_DETAILS = "Amend other parties details";
 
@@ -39,7 +33,7 @@ public class CaseworkerAmendOtherPartiesDetails implements CCDConfig<CaseData, S
         configBuilder.grant(State.Draft, Permissions.READ_UPDATE, UserRole.CASE_WORKER, UserRole.COURT_ADMIN,
                             UserRole.LEGAL_ADVISOR, UserRole.DISTRICT_JUDGE
         );
-       return new PageBuilder(configBuilder
+        return new PageBuilder(configBuilder
                                   .event(CASEWORKER_AMEND_OTHER_PARTIES_DETAILS)
                                   .forAllStates()
                                   .name(AMEND_OTHER_PARTIES_DETAILS)
