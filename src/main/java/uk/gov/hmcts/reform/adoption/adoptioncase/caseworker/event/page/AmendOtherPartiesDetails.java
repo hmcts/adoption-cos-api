d24709677aec65c375e2f70590e72ae3c93a7716
Sairam Kantheti <sai.kantheti@hmcts.net>
2022-09-15 11:14:07 +0100

    ADOP-1259 changes

---
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/AmendOtherPartiesDetails.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page;
+
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.Children;
+import uk.gov.hmcts.reform.adoption.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
+
+public class AmendOtherPartiesDetails implements CcdPageConfiguration{
+
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+
+
+        pageBuilder.page("amendOtherParties")
+            .complex(CaseData::getChildren)
+            .mandatoryWithLabel(Children::getFirstName,"First names")
+            .mandatoryWithLabel(Children::getLastName,"Last names")
+            .mandatoryWithLabel(Children::getDateOfBirth,"Date of birth")
+            .mandatoryWithLabel(Children::getSexAtBirth,"Sex of birth")
+            .mandatoryWithLabel(Children::getNationality,"Nationality")
+            .mandatoryWithLabel(Children::getFirstNameAfterAdoption,"First names after adoption")
+            .mandatoryWithLabel(Children::getLastNameAfterAdoption,"Last names after adoption")
+            .done();
+
+
+
+    }
+}
