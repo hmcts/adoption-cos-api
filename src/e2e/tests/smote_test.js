f22f775e80c0d9a2e1c731e43a34a37d8caad5e8
Raghu Goud <nraghu31@gmail.com>
2022-09-15 14:29:59 +0100

    XUI Automation framework

--- src/e2e/tests/smote_test.js
+++ src/e2e/tests/smote_test.js
@@ -5,10 +5,11 @@ const laHelper = require('../helpers/la_portal_case');
 
 Feature('Create case @smoke').retry(1);
 
-Scenario('Verify case created and able to search in ExUI', async ({I,loginPage, caseListPage }) => {
-
+Scenario('Verify case created and able to search in ExUI', async ({loginPage, caseListPage }) => {
+  console.log('APP_INSIGHTS_KEY - '+ process.env.APP_INSIGHTS_KEY);
   console.log('Smoke test triggered now');
   const caseID=await laHelper.createCompleteCase();
+  console.log('CCD Case number - '+ caseID);
   const hyphanisedCaseId = caseID.replace(/(.{4})/g,"$1-").substring(0,19);
   console.log('hyphanisedCaseId '+ hyphanisedCaseId);
   console.log('CCD Case number - '+ hyphanisedCaseId);
