7495acf549c849ab40a766485fa9a9d4c94e2cb2
Thirunavukkarasu Soundararajan <Thirunavukkarasu.Soundararajan@hmcts.net>
2022-09-16 11:28:18 +0100

    feat-ExUI-Event-AmendCase

---
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/CaseworkerAmendCase.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event;
+
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page.AmendCaseDetails;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions;
+import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
+
+/**
+ * Contains method to define Event Configuration for ExUI.
+ * Enable Amend Case Details functionality for Adoption Cases.
+ */
+public class CaseworkerAmendCase implements CCDConfig<CaseData, State, UserRole> {
+    public static final String AMEND_CASE_DETAILS = "Amend Case Details";
+    public static final String CASEWORKER_AMEND_CASE = "caseworker-amend-case";
+
+    private final AmendCaseDetails amendCaseDetails = new AmendCaseDetails();
+
+    @Override
+    public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        var pageBuilder = addEventConfig(configBuilder);
+        amendCaseDetails.addTo(pageBuilder);
+    }
+
+    /**
+     * Helper method to make custom changes to the CCD Config in order to add the event to respective Page Configuration.
+     *
+     * @param configBuilder - Base CCD Config Builder updated to add Event for Page
+     * @return - PageBuilder updated to use on overridden method.
+     */
+    private PageBuilder addEventConfig(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        return new PageBuilder(configBuilder
+                                   .event(CASEWORKER_AMEND_CASE)
+                                   .forAllStates()
+                                   .name(AMEND_CASE_DETAILS)
+                                   .description(AMEND_CASE_DETAILS)
+                                   .showSummary()
+                                   .grant(Permissions.CREATE_READ_UPDATE, UserRole.CASE_WORKER));
+    }
+}
