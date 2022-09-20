de51fed6a9d28b39f1c0c8f784d898d924ee43ed
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-09-20 15:22:46 +0100

    Amend applicant details

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/AmendApplicantDetails.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/AmendApplicantDetails.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page;
 
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.Applicant;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
 
@@ -14,6 +16,35 @@ public class AmendApplicantDetails implements CcdPageConfiguration {
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder.page("amendCaseDetails")
             .pageLabel(AMEND_APPLICANT_DETAILS)
+            .label("applicant1Label","## First applicant","applyingWith=\"alone\"")
+            .complex(CaseData::getApplicant1)
+            .mandatory(Applicant::getFirstName)
+            .mandatory(Applicant::getLastName)
+            .mandatory(Applicant::getAdditionalNames)
+            .mandatory(Applicant::getDateOfBirth)
+            .mandatory(Applicant::getOccupation)
+            .mandatory(Applicant::getAddress1)
+            .mandatory(Applicant::getAddress2)
+            .mandatory(Applicant::getAddressTown)
+            .mandatory(Applicant::getAddressCountry)
+            .mandatory(Applicant::getAddressPostCode)
+            .mandatory(Applicant::getEmailAddress)
+            .mandatory(Applicant::getPhoneNumber)
+            .done()
+            .label("applicant2Label","## Second applicant","applyingWith=!\"alone\"")
+            .complex(CaseData::getApplicant2)
+            .mandatory(Applicant::getFirstName)
+            .mandatory(Applicant::getLastName)
+            .mandatory(Applicant::getAdditionalNames)
+            .mandatory(Applicant::getDateOfBirth)
+            .mandatory(Applicant::getOccupation)
+            .mandatory(Applicant::getAddress1)
+            .mandatory(Applicant::getAddress2)
+            .mandatory(Applicant::getAddressTown)
+            .mandatory(Applicant::getAddressCountry)
+            .mandatory(Applicant::getAddressPostCode)
+            .mandatory(Applicant::getEmailAddress)
+            .mandatory(Applicant::getPhoneNumber)
             .done();
     }
 }
