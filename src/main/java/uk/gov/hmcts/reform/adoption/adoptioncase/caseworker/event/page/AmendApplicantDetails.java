5e181c17e5e135f7f66c92f019a7d02e97f1effe
tsounda <111878461+tsounda@users.noreply.github.com>
2022-09-26 11:38:00 +0100

    Adop 1254 ex UI event amend applicant details (#444)
    * feat-ExUI-Event-AmendApplicant
    
    * feat-ExUI-Event-AmendApplicant-MC
    
    Co-authored-by: Pankaj Kumar <pankaj.kumar@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/AmendApplicantDetails.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page;
+
+import uk.gov.hmcts.reform.adoption.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
+
+import static uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.CaseworkerAmendApplicant.AMEND_APPLICANT_DETAILS;
+
+/**
+ * Contains method to add Page Configuration for ExUI.
+ * Display the Amend Case Details screen with all required fields.
+ */
+public class AmendApplicantDetails implements CcdPageConfiguration {
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+        pageBuilder.page("amendCaseDetails")
+            .pageLabel(AMEND_APPLICANT_DETAILS)
+            .done();
+    }
+}
