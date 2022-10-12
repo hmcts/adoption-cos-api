3967b0dfafb6d5c797027b0c43a7e21bcefb5438
CharchitHmcts <113587746+CharchitHmcts@users.noreply.github.com>
2022-10-12 20:17:10 +0100

    Transfer Court  (#497)
    * ADOP-1256: Gatekeeping - Transfer court
    
    * ADOP-1256: Gatekeeping - Transfer Court test case added
    
    * ADOP-1256: Gatekeeping - District Judge permission added
    
    * ADOP-1256: Transfer Court Changes fix
    
    * ADOP-1256: Transfer Court Changes fix
    
    * ADOP-1256: Transfer Court Changes fix

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/CaseData.java
@@ -454,6 +454,12 @@ public class CaseData {
     private String familyCourtName;
 
     @CCD(
+        label = "Enter court name",
+        access = {DefaultAccess.class}
+    )
+    private String transferCourt;
+
+    @CCD(
         label = "Family court email",
         access = {DefaultAccess.class}
     )
---
+++ src/test/java/uk/gov/hmcts/reform/adoption/caseworker/event/CaseworkerTransferCourtTest.java
@@ -0,0 +1,88 @@
+package uk.gov.hmcts.reform.adoption.caseworker.event;
+
+import com.google.common.collect.ImmutableSet;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.ResolvedCCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.HasRole;
+import uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.CaseworkerTransferCourt;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.platform.commons.util.ReflectionUtils.findMethod;
+import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.CaseworkerTransferCourt.CASEWORKER_TRANSFER_COURT;
+
+@ExtendWith(MockitoExtension.class)
+public class CaseworkerTransferCourtTest {
+
+    @InjectMocks
+    CaseworkerTransferCourt caseworkerTransferCourt;
+
+
+    /**
+     * Test Scenario: Should be able to ADD Configuration to the CCD Config Builder.
+     */
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+        caseworkerTransferCourt.configure(configBuilder);
+        assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(CASEWORKER_TRANSFER_COURT);
+    }
+
+
+    public static ConfigBuilderImpl<CaseData, State, UserRole> createCaseDataConfigBuilder() {
+        return new ConfigBuilderImpl<>(new ResolvedCCDConfig<>(
+            CaseData.class,
+            State.class,
+            UserRole.class,
+            new HashMap<>(),
+            ImmutableSet.copyOf(State.class.getEnumConstants())));
+    }
+
+
+    @SuppressWarnings({"unchecked"})
+    public static <T, S, R extends HasRole> Map<String, Event<T, R, S>> getEventsFrom(
+        final ConfigBuilderImpl<T, S, R> configBuilder) {
+
+        return (Map<String, Event<T, R, S>>) findMethod(ConfigBuilderImpl.class, "getEvents")
+            .map(method -> {
+                try {
+                    method.setAccessible(true);
+                    return method.invoke(configBuilder);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new AssertionError("Unable to invoke ConfigBuilderImpl.class method getEvents", e);
+                }
+            })
+            .orElseThrow(() -> new AssertionError("Unable to find ConfigBuilderImpl.class method getEvents"));
+    }
+
+    private CaseDetails<CaseData, State> getCaseDetails() {
+        final var details = new CaseDetails<CaseData, State>();
+        final var data = caseData();
+        data.setTransferCourt("TEST");
+        details.setData(data);
+        details.setId(1L);
+        return details;
+    }
+
+    @Test
+    public void shouldSuccessfullyTransferCourt() {
+        var caseDetails = getCaseDetails();
+        var result = caseworkerTransferCourt.aboutToSubmit(caseDetails, caseDetails);
+        assertThat(result.getData().getFamilyCourtName()).isNotNull();
+    }
+}
