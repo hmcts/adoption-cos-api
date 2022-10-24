b66572a0de006abd78af74de40d300f516088935
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2022-10-24 11:32:47 +0100

    Changes related to PMD Rules

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/ManageHearings.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/ManageHearings.java
@@ -54,28 +54,117 @@ public class ManageHearings implements CcdPageConfiguration {
         /* Check if Application is applied alone or with someone
         and based on that display proper Recipient */
         caseData.setRecipientsInTheCase(new TreeSet<>());
+        settingApplicantRelatedRecipients(caseData);
+        settingChildRelatedRecipients(caseData);
+        settingParentRelatedRecipients(caseData);
+        settingAdoptionAgencyRelatedRecipients(caseData);
+        settingOtherPersonRelatedRecipients(caseData);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> midEventAfterRecipientSelection(
+        CaseDetails<CaseData, State> details,
+        CaseDetails<CaseData, State> detailsBefore
+    ) {
+        var caseData = details.getData();
+        List<String> error = new ArrayList<>();
+
+        checkingApplicantRelatedSelectedRecipients(caseData, error);
+
+        checkingChildRelatedSelectedRecipient(caseData, error);
+
+        checkingParentRelatedSelectedRecipients(caseData, error);
+
+        checkingOtherPersonRelatedSelectedRecipients(caseData, error);
+
+        checkingAdoptionAgencyRelatedSelectedRecipients(caseData, error);
+
+        checkForMandatoryCheckbox(caseData, error, RecipientsInTheCase.APPLICANT1);
+        checkForMandatoryCheckbox(caseData, error, RecipientsInTheCase.ADOPTION_AGENCY);
+        checkForMandatoryCheckbox(caseData, error, RecipientsInTheCase.CHILD_LOCAL_AUTHORITY);
+        checkForMandatoryCheckbox(caseData, error, RecipientsInTheCase.APPLICANT_LOCAL_AUTHORITY);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .errors(error)
+            .build();
+    }
+
+    private void checkingAdoptionAgencyRelatedSelectedRecipients(CaseData caseData, List<String> error) {
+        if (YesOrNo.NO.equals(caseData.getHasAnotherAdopAgencyOrLAinXui()) || caseData.getHasAnotherAdopAgencyOrLAinXui() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.OTHER_ADOPTION_AGENCY);
+        }
+    }
+
+    private void checkingOtherPersonRelatedSelectedRecipients(CaseData caseData, List<String> error) {
+        if (YesOrNo.NO.equals(caseData.getOtherParent().getToBeServed())
+            || caseData.getOtherParent().getToBeServed() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.OTHER_PERSON_WITH_PARENTAL_RESPONSIBILITIES);
+        }
+
+        /* Check if Other Parent is represented by Solicitor */
+        if (YesOrNo.NO.equals(caseData.getIsOtherParentRepresentedBySolicitor())
+            || caseData.getIsOtherParentRepresentedBySolicitor() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.OTHER_PERSON_WITH_PARENTAL_RESPONSIBILITIES_SOLICITOR);
+        }
+    }
+
+    private void checkingParentRelatedSelectedRecipients(CaseData caseData, List<String> error) {
+        if (YesOrNo.NO.equals(caseData.getBirthMother().getToBeServed()) || caseData.getBirthMother().getToBeServed() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.RESPONDENT_MOTHER);
+        }
+
+        if (YesOrNo.NO.equals(caseData.getIsBirthMotherRepresentedBySolicitor())
+            || caseData.getIsBirthMotherRepresentedBySolicitor() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.BIRTH_MOTHER_SOLICITOR);
+        }
+
+        if (YesOrNo.NO.equals(caseData.getBirthFather().getToBeServed()) || caseData.getBirthFather().getToBeServed() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.RESPONDENT_FATHER);
+        }
+
+        if (YesOrNo.NO.equals(caseData.getIsBirthFatherRepresentedBySolicitor())
+            || caseData.getIsBirthFatherRepresentedBySolicitor() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.BIRTH_FATHER_SOLICITOR);
+        }
+    }
+
+    private void checkingChildRelatedSelectedRecipient(CaseData caseData, List<String> error) {
+        if (YesOrNo.NO.equals(caseData.getIsChildRepresentedByGuardian()) || caseData.getIsChildRepresentedByGuardian() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.LEGAL_GUARDIAN);
+        }
+
+        if (YesOrNo.NO.equals(caseData.getIsChildRepresentedBySolicitor()) || caseData.getIsChildRepresentedBySolicitor() == null) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.CHILD_SOLICITOR);
+        }
+    }
+
+    private void checkingApplicantRelatedSelectedRecipients(CaseData caseData, List<String> error) {
         if (ApplyingWith.ALONE.equals(caseData.getApplyingWith())) {
-            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT1);
-        } else {
-            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT1);
-            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT2);
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.APPLICANT2);
         }
 
-        /* Check if Applicant is represented by Solicitor */
-        if (YesOrNo.YES.equals(caseData.getIsApplicantRepresentedBySolicitor())) {
-            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT_SOLICITOR);
+        if (YesOrNo.NO.equals(caseData.getIsApplicantRepresentedBySolicitor())) {
+            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.APPLICANT_SOLICITOR);
+        }
+    }
+
+    private void settingOtherPersonRelatedRecipients(CaseData caseData) {
+        /* Check if Other Parent has selected To Be Served as Yes */
+        if (YesOrNo.YES.equals(caseData.getOtherParent().getToBeServed())) {
+            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.OTHER_PERSON_WITH_PARENTAL_RESPONSIBILITIES);
         }
 
-        /* Check if Child is represented by Guardian */
-        if (YesOrNo.YES.equals(caseData.getIsChildRepresentedByGuardian())) {
-            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.LEGAL_GUARDIAN);
+        /* Check if Other Parent is represented by Solicitor */
+        if (YesOrNo.YES.equals(caseData.getIsOtherParentRepresentedBySolicitor())) {
+            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.OTHER_PERSON_WITH_PARENTAL_RESPONSIBILITIES_SOLICITOR);
         }
+    }
 
-        /* Check if Child is represented by Solicitor */
-        if (YesOrNo.YES.equals(caseData.getIsChildRepresentedBySolicitor())) {
-            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.CHILD_SOLICITOR);
-        }
-
+    private void settingAdoptionAgencyRelatedRecipients(CaseData caseData) {
         /* Adoption Agency is by default selected */
         caseData.getRecipientsInTheCase().add(RecipientsInTheCase.ADOPTION_AGENCY);
 
@@ -83,11 +172,9 @@ public class ManageHearings implements CcdPageConfiguration {
         if (YesOrNo.YES.equals(caseData.getHasAnotherAdopAgencyOrLAinXui())) {
             caseData.getRecipientsInTheCase().add(RecipientsInTheCase.OTHER_ADOPTION_AGENCY);
         }
+    }
 
-        /* Child and Applicant Local Authority is by default selected */
-        caseData.getRecipientsInTheCase().add(RecipientsInTheCase.CHILD_LOCAL_AUTHORITY);
-        caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT_LOCAL_AUTHORITY);
-
+    private void settingParentRelatedRecipients(CaseData caseData) {
         /* Check if Birth Mother has selected To Be Served as Yes */
         if (YesOrNo.YES.equals(caseData.getBirthMother().getToBeServed())) {
             caseData.getRecipientsInTheCase().add(RecipientsInTheCase.RESPONDENT_MOTHER);
@@ -107,88 +194,38 @@ public class ManageHearings implements CcdPageConfiguration {
         if (YesOrNo.YES.equals(caseData.getIsBirthFatherRepresentedBySolicitor())) {
             caseData.getRecipientsInTheCase().add(RecipientsInTheCase.BIRTH_FATHER_SOLICITOR);
         }
-
-        /* Check if Other Parent has selected To Be Served as Yes */
-        if (YesOrNo.YES.equals(caseData.getOtherParent().getToBeServed())) {
-            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.OTHER_PERSON_WITH_PARENTAL_RESPONSIBILITIES);
-        }
-
-        /* Check if Other Parent is represented by Solicitor */
-        if (YesOrNo.YES.equals(caseData.getIsOtherParentRepresentedBySolicitor())) {
-            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.OTHER_PERSON_WITH_PARENTAL_RESPONSIBILITIES_SOLICITOR);
-        }
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseData)
-            .build();
     }
 
-    public AboutToStartOrSubmitResponse<CaseData, State> midEventAfterRecipientSelection(
-        CaseDetails<CaseData, State> details,
-        CaseDetails<CaseData, State> detailsBefore
-    ) {
-        var caseData = details.getData();
-        List<String> error = new ArrayList<>();
+    private void settingChildRelatedRecipients(CaseData caseData) {
+        /* Check if Child is represented by Guardian */
+        if (YesOrNo.YES.equals(caseData.getIsChildRepresentedByGuardian())) {
+            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.LEGAL_GUARDIAN);
+        }
 
+        /* Check if Child is represented by Solicitor */
+        if (YesOrNo.YES.equals(caseData.getIsChildRepresentedBySolicitor())) {
+            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.CHILD_SOLICITOR);
+        }
+
+        /* Child Local Authority is by default selected */
+        caseData.getRecipientsInTheCase().add(RecipientsInTheCase.CHILD_LOCAL_AUTHORITY);
+    }
+
+    private void settingApplicantRelatedRecipients(CaseData caseData) {
         if (ApplyingWith.ALONE.equals(caseData.getApplyingWith())) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.APPLICANT2);
+            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT1);
+        } else {
+            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT1);
+            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT2);
         }
 
-        log.info("Checking if Applicant Solicitor");
-        if (YesOrNo.NO.equals(caseData.getIsApplicantRepresentedBySolicitor())) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.APPLICANT_SOLICITOR);
+        /* Check if Applicant is represented by Solicitor */
+        if (YesOrNo.YES.equals(caseData.getIsApplicantRepresentedBySolicitor())) {
+            caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT_SOLICITOR);
         }
 
-        if (YesOrNo.NO.equals(caseData.getIsChildRepresentedByGuardian()) || caseData.getIsChildRepresentedByGuardian() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.LEGAL_GUARDIAN);
-        }
-
-        if (YesOrNo.NO.equals(caseData.getIsChildRepresentedBySolicitor()) || caseData.getIsChildRepresentedBySolicitor() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.CHILD_SOLICITOR);
-        }
-
-        if (YesOrNo.NO.equals(caseData.getHasAnotherAdopAgencyOrLAinXui()) || caseData.getHasAnotherAdopAgencyOrLAinXui() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.OTHER_ADOPTION_AGENCY);
-        }
-
-        if (YesOrNo.NO.equals(caseData.getBirthMother().getToBeServed()) || caseData.getBirthMother().getToBeServed() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.RESPONDENT_MOTHER);
-        }
-
-        if (YesOrNo.NO.equals(caseData.getIsBirthMotherRepresentedBySolicitor())
-            || caseData.getIsBirthMotherRepresentedBySolicitor() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.BIRTH_MOTHER_SOLICITOR);
-        }
-
-        if (YesOrNo.NO.equals(caseData.getBirthFather().getToBeServed()) || caseData.getBirthFather().getToBeServed() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.RESPONDENT_FATHER);
-        }
-
-        if (YesOrNo.NO.equals(caseData.getIsBirthFatherRepresentedBySolicitor())
-            || caseData.getIsBirthFatherRepresentedBySolicitor() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.BIRTH_FATHER_SOLICITOR);
-        }
-
-        if (YesOrNo.NO.equals(caseData.getOtherParent().getToBeServed())
-            || caseData.getOtherParent().getToBeServed() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.OTHER_PERSON_WITH_PARENTAL_RESPONSIBILITIES);
-        }
-
-        /* Check if Other Parent is represented by Solicitor */
-        if (YesOrNo.NO.equals(caseData.getIsOtherParentRepresentedBySolicitor())
-            || caseData.getIsOtherParentRepresentedBySolicitor() == null) {
-            checkForInvalidCheckboxSelection(caseData, error, RecipientsInTheCase.OTHER_PERSON_WITH_PARENTAL_RESPONSIBILITIES_SOLICITOR);
-        }
-
-        checkForMandatoryCheckbox(caseData, error, RecipientsInTheCase.APPLICANT1);
-        checkForMandatoryCheckbox(caseData, error, RecipientsInTheCase.ADOPTION_AGENCY);
-        checkForMandatoryCheckbox(caseData, error, RecipientsInTheCase.CHILD_LOCAL_AUTHORITY);
-        checkForMandatoryCheckbox(caseData, error, RecipientsInTheCase.APPLICANT_LOCAL_AUTHORITY);
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseData)
-            .errors(error)
-            .build();
+        /* Applicant Local Authority is by default selected */
+        caseData.getRecipientsInTheCase().add(RecipientsInTheCase.APPLICANT_LOCAL_AUTHORITY);
     }
 
     private void checkForMandatoryCheckbox(CaseData caseData, List<String> error, RecipientsInTheCase recipientsInTheCase) {
