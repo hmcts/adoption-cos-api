47e8099d699f759535d166a7b5ecfc490d873491
Raghu Goud <nraghu31@gmail.com>
2022-10-25 18:36:53 +0100

    ADOP-1572 E2E changes

---
+++ src/e2e/fixtures/manageOrderDetails.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+  caseManagementOrderDetails: {
+    preamble: 'Test preamble details',
+    nameOfReallocatedJudge: 'Reallocated Judge Name',
+    errorMessage: 'Field is required',
+    chooseAllErrorMessage: 'Choose all that are relevant. is required',
+    hearingDate: {
+      second: '00',
+      minute: '15',
+      hour: '15',
+      day: '01',
+      month: '01',
+      year: '2050',
+    },
+    lengthOfHearingFirstHearing: '2 hours 30 minutes',
+    localAuthorityOption1: {
+      day: '01',
+      month: '01',
+      year: '2029',
+    },
+    localAuthorityOption2: {
+      day: '01',
+      month: '01',
+      year: '2030',
+    },
+    applicantAttendanceNotAttendOption: 'The child will not be brought to this hearing.',
+    childAttendanceNotBeBroughtOption: 'The applicants are NOT to attend the next hearing',
+    localAuthorityAttendanceOption: 'A representative of the Local Authority shall attend the next hearing.',
+    birthParentAttendanceCourtOption: '3. the court shall arrange personal service of notice of the hearing/this order on the respondent parents'
+  },
+};
--- src/e2e/pages/manageOrders.page.js
+++ src/e2e/pages/manageOrders.page.js
@@ -1,18 +1,61 @@
 const config = require('../config');
 const { I } = inject();
-
+const manageOrderDetails = require('../fixtures/manageOrderDetails.js');
 module.exports = {
   fields: {
-      caseId: 'h3:nth-child(1)',
-      ChildName: 'h3:nth-child(2)',
-      continueButton: 'button[type="submit"]',
-      errorMessage: '//span[starts-with(@class,"error-message")]',
-      createOrder:'#manageOrderActivity-createOrder',
-      caseManagementOrder: '#manageOrderType-caseManagementOrder',
-      generalDirectionsOrder: '#manageOrderType-generalDirectionsOrder',
-      finalAdoptionOrder: '#manageOrderType-finalAdoptionOrder',
-      manageOrdersUpdate: '//div[contains(text(), "has been updated with event: Manage orders")]',
+    caseId: 'h3:nth-child(1)',
+    ChildName: 'h3:nth-child(2)',
+    continueButton: 'button[type="submit"]',
+    errorMessage: '//span[starts-with(@class,"error-message")]',
+    createOrder:'#manageOrderActivity-createOrder',
+    caseManagementOrder: '#manageOrderType-caseManagementOrder',
+    generalDirectionsOrder: '#manageOrderType-generalDirectionsOrder',
+    finalAdoptionOrder: '#manageOrderType-finalAdoptionOrder',
+    manageOrdersUpdate: '//div[contains(text(), "has been updated with event: Manage orders")]',
+    preambleDetails: '#preambleDetails',
+    reallocateJudge: '#allocationJudge-reallocateJudge',
+    nameOfJudge: '#nameOfJudge',
+    listForFirstHearing: '#hearingNotices-listForFirstHearing',
+    hearingDetails:{
+      dateAndTimeFirstHearingErrorMessage: '#dateAndTimeFirstHearing .error-message',
+      dateAndTimeFirstHearingDay: '#dateAndTimeFirstHearing-day',
+      dateAndTimeFirstHearingMonth: '#dateAndTimeFirstHearing-month',
+      dateAndTimeFirstHearingYear: '#dateAndTimeFirstHearing-year',
     },
+    lengthOfHearingFirstHearing: '#lengthOfHearingFirstHearing',
+    lengthOfHearingFirstHearingErrorMessage: 'ccd-field-write[field_id=\'lengthOfHearingFirstHearing\'] .error-message',
+    localAuthority: {
+      selectLocalAuthority: '#selectedLocalAuthority-fileAdoptionAgencyReport',
+      dateAndTimeForOption1Day: '#dateAndTimeForOption1-day',
+      dateAndTimeForOption1Month: '#dateAndTimeForOption1-month',
+      dateAndTimeForOption1Year: '#dateAndTimeForOption1-year',
+      dateAndTimeForOption2Day: '#timeForOption2-day',
+      dateAndTimeForOption2Month: '#timeForOption2-month',
+      dateAndTimeForOption2Year: '#timeForOption2-year',
+      dateAndTimeForOption1Error:'#dateAndTimeForOption1 .error-message',
+      dateAndTimeForOption2Error:'#timeForOption2 .error-message',
+
+    },
+    attendance: {
+      applicantsAttendance: '#attendance-applicantsAttendance',
+      childAttendance: '#attendance-childAttendance',
+      localAuthorityAttendance: '#attendance-localAuthorityAttendance',
+      birthParentsAttendance: '#attendance-birthParentsAttendance',
+      applicantAttendanceNotAttend: '#applicantAttendance-applicantAttendanceNotAttend',
+      childAttendanceNotBeBrought: '#childAttendance-childAttendanceNotBeBrought',
+      laAttendanceAttend: '#laAttendance-laAttendanceAttend',
+      birthParentAttendanceCourtArrange: '#birthParentAttendance-birthParentAttendanceCourtArrange',
+      applicantAttendanceError: '#applicantAttendance .error-message',
+      childAttendanceError: '#childAttendance .error-message',
+      laAttendanceError: '#laAttendance .error-message',
+      birthParentAttendanceError: '#birthParentAttendance .error-message',
+
+
+
+    },
+
+
+  },
 
   async verifyCaseDetails() {
     await I.wait(3);
@@ -43,8 +86,104 @@ module.exports = {
     await I.retry(3).seeElement(this.fields.errorMessage);
     await I.retry(3).click(this.fields.caseManagementOrder);
     await I.retry(3).click(this.fields.continueButton);
+  },
+  async addPreambleAndReallocateJudgeInCaseManagementOrder(){
+    await I.wait(3);
+    await I.retry(3).fillField(this.fields.preambleDetails, manageOrderDetails.caseManagementOrderDetails.preamble);
+    await I.wait(3);
+    await I.retry(3).click(this.fields.reallocateJudge);
     await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.preamble);
+    await I.retry(3).fillField(this.fields.nameOfJudge, manageOrderDetails.caseManagementOrderDetails.nameOfReallocatedJudge);
     await I.retry(3).click(this.fields.continueButton);
-    await I.retry(3).seeEventSubmissionConfirmation(caseId,config.administrationActions.manageOrders);
+  },
+  async caseManagementOrderFirstDirections(){
+    await I.wait(3);
+    await I.retry(3).click(this.fields.listForFirstHearing);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.errorMessage,this.fields.hearingDetails.dateAndTimeFirstHearingErrorMessage);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.errorMessage,this.fields.lengthOfHearingFirstHearingErrorMessage);
+    await I.retry(3).fillField(this.fields.hearingDetails.dateAndTimeFirstHearingDay, manageOrderDetails.caseManagementOrderDetails.hearingDate.day);
+    await I.retry(3).fillField(this.fields.hearingDetails.dateAndTimeFirstHearingMonth, manageOrderDetails.caseManagementOrderDetails.hearingDate.month);
+    await I.retry(3).fillField(this.fields.hearingDetails.dateAndTimeFirstHearingYear, manageOrderDetails.caseManagementOrderDetails.hearingDate.year);
+    await I.retry(3).fillField(this.fields.lengthOfHearingFirstHearing, manageOrderDetails.caseManagementOrderDetails.lengthOfHearingFirstHearing);
+    await I.retry(3).click(this.fields.continueButton);
+  },
+
+  async caseManagementOrderPreambleAndReallocateJudgeCYAPage(){
+    await I.wait(3);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.preamble);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.nameOfReallocatedJudge);
+    await I.retry(3).click(this.fields.continueButton);
+  },
+
+  async caseManagementOrderFirstDirectionsCYAPage(){
+    await I.wait(3);
+    await I.retry(3).see('1 Jan 2050, 12:00:00 AM');
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.lengthOfHearingFirstHearing);
+    await I.retry(3).click(this.fields.continueButton);
+  },
+
+  async caseManagementOrderLocalAuthority(){
+    await I.wait(3);
+    await I.retry(3).click(this.fields.localAuthority.selectLocalAuthority);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.errorMessage,this.fields.localAuthority.dateAndTimeForOption1Error);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.errorMessage,this.fields.localAuthority.dateAndTimeForOption2Error);
+    await I.wait(3);
+    await I.retry(3).fillField(this.fields.localAuthority.dateAndTimeForOption1Day, manageOrderDetails.caseManagementOrderDetails.localAuthorityOption1.day);
+    await I.retry(3).fillField(this.fields.localAuthority.dateAndTimeForOption1Month, manageOrderDetails.caseManagementOrderDetails.localAuthorityOption1.month);
+    await I.retry(3).fillField(this.fields.localAuthority.dateAndTimeForOption1Year, manageOrderDetails.caseManagementOrderDetails.localAuthorityOption1.year);
+    await I.retry(3).fillField(this.fields.localAuthority.dateAndTimeForOption2Day, manageOrderDetails.caseManagementOrderDetails.localAuthorityOption2.day);
+    await I.retry(3).fillField(this.fields.localAuthority.dateAndTimeForOption2Month, manageOrderDetails.caseManagementOrderDetails.localAuthorityOption2.month);
+    await I.retry(3).fillField(this.fields.localAuthority.dateAndTimeForOption2Year, manageOrderDetails.caseManagementOrderDetails.localAuthorityOption2.year);
+    await I.retry(3).click(this.fields.continueButton);
+  },
+
+  async caseManagementOrderLocalAuthorityCYAPage(){
+    await I.wait(3);
+    await I.retry(3).see('1 Jan 2029, 12:00:00 AM');
+    await I.retry(3).see('1 Jan 2030, 12:00:00 AM');
+    await I.retry(3).click(this.fields.continueButton);
+  },
+
+  async caseManagementOrderAttendance(){
+    await I.retry(3).click(this.fields.attendance.applicantsAttendance);
+    await I.retry(3).click(this.fields.attendance.childAttendance);
+    await I.retry(3).click(this.fields.attendance.localAuthorityAttendance);
+    await I.retry(3).click(this.fields.attendance.birthParentsAttendance);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+    await I.retry(3).waitForText('Choose the direction for the attendees',30);
+    await I.retry(3).see('Choose the direction for the attendees');
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.errorMessage,this.fields.attendance.applicantAttendanceError);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.errorMessage,this.fields.attendance.childAttendanceError);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.errorMessage,this.fields.attendance.laAttendanceError);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.chooseAllErrorMessage,this.fields.attendance.birthParentAttendanceError);
+    await I.retry(3).click(this.fields.attendance.applicantAttendanceNotAttend);
+    await I.retry(3).click(this.fields.attendance.childAttendanceNotBeBrought);
+    await I.retry(3).click(this.fields.attendance.laAttendanceAttend);
+    await I.retry(3).click(this.fields.attendance.birthParentAttendanceCourtArrange);
+    await I.retry(3).click(this.fields.continueButton);
+
+  },
+
+  async caseManagementOrderLocalAuthorityCYAPage(){
+    await I.wait(3);
+    await I.retry(3).waitForText('Check your answers',30);
+    await I.retry(3).see('Check your answers');
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.applicantAttendanceNotAttendOption);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.childAttendanceNotBeBroughtOption);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.localAuthorityAttendanceOption);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.birthParentAttendanceCourtOption);
+    await I.retry(3).click(this.fields.continueButton);
   },
 };
