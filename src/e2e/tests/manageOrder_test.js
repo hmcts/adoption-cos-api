ea3fb4ee47d0b736c57cfd0e07486a6b2316788e
tsounda <111878461+tsounda@users.noreply.github.com>
2022-10-26 19:38:28 +0100

    Adop 1566 gatekeeping screen2 part2.3 (#513)

--- src/e2e/tests/manageOrder_test.js
+++ src/e2e/tests/manageOrder_test.js
@@ -5,14 +5,14 @@ const manageOrderDetails = require('../fixtures/manageOrderDetails.js');
 
 let caseId;
 
-Feature('Manage order tests').retry(0);
+Feature('Manage order tests').retry(1);
 
 async function setupScenario(I) {
     caseId = await laHelper.createCompleteCase();
     console.log('CCD Case number - '+ caseId);
     await I.navigateToCaseDetailsAs(config.caseWorkerUserOne, caseId);
 }
-Scenario('Verify case management order Preamble and Reallocation details', async ({I, caseViewPage, manageOrdersPage }) => {
+Scenario('Verify case management order Preamble and Reallocation judge details', async ({I, caseViewPage, manageOrdersPage }) => {
   await setupScenario(I);
   await caseViewPage.goToNewActions(config.administrationActions.manageOrders);
   await manageOrdersPage.verifyCaseDetails();
@@ -23,8 +23,27 @@ Scenario('Verify case management order Preamble and Reallocation details', async
   await manageOrdersPage.caseManagementOrderPreambleReAllocatedCYAPage();
   await I.retry(3).seeEventSubmissionConfirmation(caseId,config.administrationActions.manageOrders);
 });
-
-Scenario('Verify case management order Preamble and allocation details', async ({I, caseViewPage, manageOrdersPage }) => {
+Scenario('Verify Case management order Local authority File adoption agency report details', async ({I, caseViewPage, manageOrdersPage }) => {
+  await setupScenario(I);
+  await caseViewPage.goToNewActions(config.administrationActions.manageOrders);
+  await manageOrdersPage.selectCreateNewOrder();
+  await manageOrdersPage.verifyTypeOfOrdersListed();
+  await manageOrdersPage.selectCaseManagementOrderAndVerify(caseId);
+  await manageOrdersPage.caseManagementOrderLocalAuthority();
+  await manageOrdersPage.caseManagementOrderLocalAuthorityCYAPage();
+  await I.retry(3).seeEventSubmissionConfirmation(caseId,config.administrationActions.manageOrders);
+});
+Scenario('Verify Case management order List for first hearing details', async ({I, caseViewPage, manageOrdersPage }) => {
+  await setupScenario(I);
+  await caseViewPage.goToNewActions(config.administrationActions.manageOrders);
+  await manageOrdersPage.selectCreateNewOrder();
+  await manageOrdersPage.verifyTypeOfOrdersListed();
+  await manageOrdersPage.selectCaseManagementOrderAndVerify(caseId);
+  await manageOrdersPage.caseManagementOrderListForFirstHearing();
+  await manageOrdersPage.caseManagementOrderListForFirstHearingCYAPage();
+  await I.retry(3).seeEventSubmissionConfirmation(caseId,config.administrationActions.manageOrders);
+});
+Scenario('Verify case management order Preamble and pre allocation judge details', async ({I, caseViewPage, manageOrdersPage }) => {
   await setupScenario(I);
   await caseViewPage.goToNewActions(config.administrationActions.manageOrders);
   await manageOrdersPage.selectCreateNewOrder();
