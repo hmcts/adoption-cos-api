8866482eb5d8a9f42cb2a343f0074cb4d5223cd4
tsounda <111878461+tsounda@users.noreply.github.com>
2022-10-26 16:51:05 +0100

    Feature/adop manage hearing (#511) (#524)
    * ADOP-1530 & ADOP-1530 Manage hearing changes
    
    * ADOP-1530 & ADOP-1530 Manage hearing changes
    
    * Manage Hearings test
    
    * ADOP-1530 & ADOP-1530 Manage hearing changes
    
    * ADOP-1530 & ADOP-1530 Manage hearing changes
    
    * Manage Hearings test
    
    * ADOP-1530 & ADOP-1530 Manage hearing changes
    
    * Defect fix for ADOP-1313
    
    * ADOP-1313 Add new hearing part 2.1
    
    * ADOP-1313 Check your answers page - Add new hearing part 2.3
    
    * added java doc
    
    Co-authored-by: harshniavadhannla <harshni.avadhannla@hmcts.net>
    
    Co-authored-by: sairamkantheti1 <101887377+sairamkantheti1@users.noreply.github.com>
    Co-authored-by: harshniavadhannla <harshni.avadhannla@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/ManageHearingDetails.java
@@ -0,0 +1,84 @@
+package uk.gov.hmcts.reform.adoption.adoptioncase.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.ccd.sdk.type.FieldType;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.DefaultAccess;
+
+import java.time.LocalDateTime;
+import java.util.SortedSet;
+
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedRadioList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ManageHearingDetails {
+
+
+    @CCD(
+        label = "Type of hearing",
+        hint = "For example, a first hearing"
+
+    )
+    private String typeOfHearing;
+
+    @CCD(
+        label = "Hearing date & time"
+    )
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime hearingDateAndTime;
+
+    @CCD(
+        label = "Judge",
+        hint = "This should be the allocated judge, if possible."
+
+    )
+    private String judge;
+
+    @CCD(
+        label = "Court",
+        hint = "Enter the full name of the court where the hearing will be held "
+    )
+    private String court;
+
+
+    @CCD(
+        label = "Is an interpreter needed?",
+        hint = "Given details of the interpretation needed for example, sign language or Hindu translator"
+    )
+    private String isInterpreterNeeded;
+
+    @CCD(
+        label = "Method of hearing",
+        typeOverride = FixedRadioList,
+        typeParameterOverride = "MethodOfHearing"
+    )
+    private MethodOfHearing methodOfHearing;
+
+
+    @CCD(
+        label = "Accessibility requirements",
+        typeOverride = FieldType.TextArea,
+        hint = "List any accessibility requirements needed such as disabled access"
+    )
+    private String accessibilityRequirements;
+
+    @CCD(
+        label = "Hearing directions",
+        access = {DefaultAccess.class},
+        typeOverride = FieldType.MultiSelectList,
+        typeParameterOverride = "HearingDirections"
+    )
+    private SortedSet<HearingDirections> hearingDirections;
+
+    @CCD(label = " Length of hearing",
+         hint = "Insert the length of the hearing in hours and minutes, for example 2 hours 30 minutes")
+    private String lengthOfHearing;
+
+}
