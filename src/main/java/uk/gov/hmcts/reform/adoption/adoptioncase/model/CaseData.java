474292838b4577068fe2f1040909a912590e6139
sairamkantheti1 <101887377+sairamkantheti1@users.noreply.github.com>
2022-10-27 16:47:59 +0100

    Feature/adop vacate hearing changes (#521)
    * ADOP-1530 & ADOP-1530 Manage hearing changes
    
    * ADOP-1530 & ADOP-1530 Manage hearing changes
    
    * Manage Hearings test
    
    * ADOP-1530 & ADOP-1530 Manage hearing changes
    
    * Defect fix for ADOP-1313
    
    * ADOP-1602 , ADOP-1542 changes
    
    * ADOP-1314 Vacate hearing changes
    
    * ADOP-1314 Vacate hearing changes
    
    * ADOP-1314 Manage Hearing - Part 3.1 - VVacate Hearing
    
    * Update CaseData.java
    
    Co-authored-by: harshniavadhannla <harshni.avadhannla@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/CaseData.java
@@ -8,7 +8,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.CollectionAccess;
@@ -28,11 +30,13 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.SortedSet;
 import java.util.UUID;
 
 import static org.springframework.util.CollectionUtils.isEmpty;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.DynamicRadioList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedRadioList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.MultiSelectList;
@@ -628,11 +632,40 @@ public class CaseData {
     )
     private ManageHearingOptions manageHearingOptions;
 
+
+
+    @CCD(
+        typeOverride = DynamicRadioList,
+        name = "Hearing list",
+        label = "Select a hearing you want to vacate\n"
+    )
+    private DynamicList hearingsList;
+
     @CCD(
         label = "Enter hearing details",
         access = { SystemUpdateAccess.class,DefaultAccess.class}
     )
     private ManageHearingDetails manageHearingDetails;
+
+    @CCD(
+        label = "Reason for vacating a hearing",
+        access = { SystemUpdateAccess.class,DefaultAccess.class}
+    )
+    private ReasonForVacatingHearing reasonForVacatingHearing;
+
+    @CCD(
+        label = "Does the hearing need to be relisted",
+        access = { SystemUpdateAccess.class,DefaultAccess.class}
+    )
+    private YesOrNo isTheHearingNeedsRelisting;
+
+    @CCD(
+        label = "Vacated hearing",
+        typeOverride = Collection,
+        typeParameterOverride = "ManageHearingDetails",
+        access = {DefaultAccess.class}
+    )
+    private List<ListValue<ManageHearingDetails>> vacatedHearings;
 
 
     @CCD(
@@ -820,6 +853,42 @@ public class CaseData {
             this.setManageHearingOptions(null);
             this.setRecipientsInTheCase(null);
         }
+    }
+
+    @JsonIgnore
+    public void updateVacatedHearings() {
+
+        Optional<ListValue<ManageHearingDetails>> vacatedHearingDetails = newHearings.stream().filter(hearing -> StringUtils.equals(
+            hearing.getValue().getHearingId(),
+            hearingsList.getValue().getCode().toString()
+        )).findFirst();
+
+        if (!vacatedHearings.contains(vacatedHearingDetails.get())) {
+            vacatedHearingDetails.get().getValue().setReasonForVacatingHearing(reasonForVacatingHearing);
+
+            if (isEmpty(this.getVacatedHearings())) {
+                List<ListValue<ManageHearingDetails>> listValues = new ArrayList<>();
+                var listValue = ListValue
+                    .<ManageHearingDetails>builder()
+                    .id("1")
+                    .value(vacatedHearingDetails.get().getValue())
+                    .build();
+                listValues.add(listValue);
+                this.setVacatedHearings(listValues);
+            } else {
+                var listValue = ListValue
+                    .<ManageHearingDetails>builder()
+                    .value(vacatedHearingDetails.get().getValue())
+                    .build();
+                int listValueIndex = 0;
+                this.getVacatedHearings().add(0, listValue);
+                for (ListValue<ManageHearingDetails> asListValue : this.getNewHearings()) {
+                    asListValue.setId(String.valueOf(listValueIndex++));
+                }
+            }
+            newHearings.remove(vacatedHearingDetails.get());
+        }
+        this.setManageHearingOptions(null);
     }
 
 }
--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/ManageHearingDetails.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/ManageHearingDetails.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.FieldType;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.DefaultAccess;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.SystemUpdateAccess;
 
 import java.time.LocalDateTime;
 import java.util.SortedSet;
@@ -21,47 +22,54 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedRadioList;
 public class ManageHearingDetails {
 
     @CCD(
-        showCondition = "methodOfHearing=\"never\""
+        showCondition = "methodOfHearing=\"never\"",
+        displayOrder = 1
     )
     private String hearingId;
 
     @CCD(
         label = "Type of hearing",
-        hint = "For example, a first hearing"
+        hint = "For example, a first hearing",
+        displayOrder = 2
 
     )
     private String typeOfHearing;
 
     @CCD(
-        label = "Hearing date & time"
+        label = "Hearing date & time",
+        displayOrder = 3
     )
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime hearingDateAndTime;
 
     @CCD(
         label = "Judge",
-        hint = "This should be the allocated judge, if possible."
+        hint = "This should be the allocated judge, if possible.",
+        displayOrder = 4
 
     )
     private String judge;
 
     @CCD(
         label = "Court",
-        hint = "Enter the full name of the court where the hearing will be held "
+        hint = "Enter the full name of the court where the hearing will be held ",
+        displayOrder = 5
     )
     private String court;
 
 
     @CCD(
         label = "Is an interpreter needed?",
-        hint = "Given details of the interpretation needed for example, sign language or Hindu translator"
+        hint = "Given details of the interpretation needed for example, sign language or Hindu translator",
+        displayOrder = 6
     )
     private String isInterpreterNeeded;
 
     @CCD(
         label = "Method of hearing",
         typeOverride = FixedRadioList,
-        typeParameterOverride = "MethodOfHearing"
+        typeParameterOverride = "MethodOfHearing",
+        displayOrder = 7
     )
     private MethodOfHearing methodOfHearing;
 
@@ -69,7 +77,8 @@ public class ManageHearingDetails {
     @CCD(
         label = "Accessibility requirements",
         typeOverride = FieldType.TextArea,
-        hint = "List any accessibility requirements needed such as disabled access"
+        hint = "List any accessibility requirements needed such as disabled access",
+        displayOrder = 8
     )
     private String accessibilityRequirements;
 
@@ -77,19 +86,29 @@ public class ManageHearingDetails {
         label = "Hearing directions",
         access = {DefaultAccess.class},
         typeOverride = FieldType.MultiSelectList,
-        typeParameterOverride = "HearingDirections"
+        typeParameterOverride = "HearingDirections",
+        displayOrder = 9
     )
     private SortedSet<HearingDirections> hearingDirections;
 
     @CCD(label = " Length of hearing",
-         hint = "Insert the length of the hearing in hours and minutes, for example 2 hours 30 minutes")
+         hint = "Insert the length of the hearing in hours and minutes, for example 2 hours 30 minutes",
+         displayOrder = 10)
     private String lengthOfHearing;
 
     @CCD(
         label = "Recipients",
         access = {DefaultAccess.class},
-        displayOrder = 10
+        displayOrder = 11
     )
     private SortedSet<RecipientsInTheCase> recipientsInTheCase;
+
+    @CCD(
+        label = "Reason for vacating hearing",
+        access = { SystemUpdateAccess.class,DefaultAccess.class},
+        displayOrder = 12
+    )
+    private ReasonForVacatingHearing reasonForVacatingHearing;
+
 
 }
