9ca8a71d6b6453d8ce1af597b1336ebacbf53c0c
charchitkasliwal <charchit.kasliwal@hmcts.net>
2022-10-31 14:36:14 +0000

    ADOP-1205: Test case added for Submission Service

--- src/test/java/uk/gov/hmcts/reform/adoption/common/service/SubmissionServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/common/service/SubmissionServiceTest.java
@@ -1,13 +1,22 @@
 package uk.gov.hmcts.reform.adoption.common.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
 import uk.gov.hmcts.reform.adoption.common.service.task.GenerateApplicationSummaryDocument;
 import uk.gov.hmcts.reform.adoption.common.service.task.SendSubmissionNotifications;
 import uk.gov.hmcts.reform.adoption.common.service.task.SetDateSubmitted;
 import uk.gov.hmcts.reform.adoption.common.service.task.SetStateAfterSubmission;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SubmissionServiceTest {
@@ -27,21 +36,26 @@ class SubmissionServiceTest {
     @InjectMocks
     private SubmissionService submissionService;
 
-    //    @Test
-    //    void shouldProcessSubmissionCaseTasks() {
-    //
-    //        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-    //        final CaseDetails<CaseData, State> expectedCaseDetails = new CaseDetails<>();
-    //
-    //        when(setStateAfterSubmission.apply(caseDetails)).thenReturn(caseDetails);
-    //        when(setDateSubmitted.apply(caseDetails)).thenReturn(caseDetails);
-    //        when(generateApplicationSummaryDocument.apply(caseDetails)).thenReturn(expectedCaseDetails);
-    //
-    //        final CaseDetails<CaseData, State> result = submissionService.submitApplication(caseDetails);
-    //
-    //        assertThat(result).isSameAs(expectedCaseDetails);
-    //
-    //        verify(setStateAfterSubmission).apply(caseDetails);
-    //        verify(setDateSubmitted).apply(caseDetails);
-    //    }
+
+    @Test
+    public void shouldProcessSubmissionCaseTasks() {
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> expectedCaseDetails = new CaseDetails<>();
+
+        when(setStateAfterSubmission.apply(caseDetails)).thenReturn(caseDetails);
+
+        when(setDateSubmitted.apply(caseDetails)).thenReturn(caseDetails);
+
+        when(generateApplicationSummaryDocument.apply(caseDetails)).thenReturn(expectedCaseDetails);
+
+        CaseDetails<CaseData, State> result = submissionService.submitApplication(caseDetails);
+
+        assertThat(result, samePropertyValuesAs(expectedCaseDetails));
+
+        verify(setStateAfterSubmission).apply(caseDetails);
+        verify(setDateSubmitted).apply(caseDetails);
+    }
+
+
 }
