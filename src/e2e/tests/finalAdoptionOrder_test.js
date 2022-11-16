50e9f65062c0a5ee8d14afadbe45d5a11d9fc7c5
raghyn <nraghu31@gmail.com>
2022-11-16 20:25:41 +0000

    ADOP-1714  E2E tests added

---
+++ src/e2e/tests/finalAdoptionOrder_test.js
@@ -0,0 +1,54 @@
+const config = require('../config');
+
+const laHelper = require('../helpers/la_portal_case');
+const manageOrderDetails = require('../fixtures/manageOrderDetails.js');
+const finalAdoptionOrderDetails = require('../fixtures/finalAdoptionOrderDetails.js');
+
+let caseId;
+
+Feature('Manage order tests').retry(1);
+
+async function setupScenario(I) {
+  caseId = await laHelper.createCompleteCase();
+  console.log('CCD Case number - ' + caseId);
+  await I.navigateToCaseDetailsAs(config.caseWorkerUserOne, caseId);
+}
+
+Scenario('Verify final adoption order screen', async ({I, caseViewPage, manageOrdersPage, finalOrderPage}) => {
+  await setupScenario(I);
+  await caseViewPage.goToNewActions(config.administrationActions.manageOrders);
+  await manageOrdersPage.verifyCaseDetails();
+  await manageOrdersPage.selectCreateNewOrder();
+  await manageOrdersPage.verifyTypeOfOrdersListed();
+  await finalOrderPage.selectFinalOrderAndVerify();
+  await finalOrderPage.addJudgeNameAndChildAdoptionName();
+});
+
+Scenario('Verify final adoption place and time of Birth', async ({I, caseViewPage, manageOrdersPage, finalOrderPage}) => {
+  await setupScenario(I);
+  await caseViewPage.goToNewActions(config.administrationActions.manageOrders);
+  await manageOrdersPage.verifyCaseDetails();
+  await manageOrdersPage.selectCreateNewOrder();
+  await manageOrdersPage.verifyTypeOfOrdersListed();
+  await finalOrderPage.selectFinalOrderAndVerify();
+  await finalOrderPage.addJudgeNameAndChildAdoptionName();
+  await finalOrderPage.verifyFAODateAndPlaceOfBirth();
+  await finalOrderPage.verifyFinalAdoptionRegistrationDetails();
+
+});
+
+Scenario('Verify final adoption order recipients and CYA', async ({I, caseViewPage, manageOrdersPage, finalOrderPage}) => {
+  await setupScenario(I);
+  await caseViewPage.goToNewActions(config.administrationActions.manageOrders);
+  await manageOrdersPage.verifyCaseDetails();
+  await manageOrdersPage.selectCreateNewOrder();
+  await manageOrdersPage.verifyTypeOfOrdersListed();
+  await finalOrderPage.selectFinalOrderAndVerify();
+  await finalOrderPage.addJudgeNameAndChildAdoptionName();
+  await finalOrderPage.verifyFAODateAndPlaceOfBirth();
+  await finalOrderPage.verifyFinalAdoptionRegistrationDetails();
+  await finalOrderPage.verifyFinalAdoptionOrderRecipients();
+  await finalOrderPage.verifyFinalAdoptionOrderRecipientsCYA();
+  await I.retry(3).seeEventSubmissionConfirmation(caseId,config.administrationActions.manageOrders);
+
+});
