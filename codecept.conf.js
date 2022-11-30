686f183d0f90b32e6313506e8ee0a5fdb9f4a01b
raghyn <nraghu31@gmail.com>
2022-11-30 17:46:27 +0000

    ADOP-1806  E2E tests added

--- codecept.conf.js
+++ codecept.conf.js
@@ -86,6 +86,7 @@ exports.config = {
     amendOtherPartiesDetailsPage: './src/e2e/pages/amendOtherPartiesDetails.page.js',
     finalOrderPage: './src/e2e/pages/finalAdoptionOrder.page.js',
     correspondenceTabPage: './src/e2e/pages/correspondenceTab.page.js',
+    checkAndSendOrdersPage: './src/e2e/pages/checkAndSendOrders.page.js'
   },
   plugins: {
     retryFailedStep: {
