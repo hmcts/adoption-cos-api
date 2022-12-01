a13d4763c74534915e9fb1f32d4b47a9e11599ca
Raghu Goud <nraghu31@gmail.com>
2022-12-01 15:21:09 +0000

    ADOP-1536 E2E new tests added

--- src/e2e/pages/manageHearings.page.js
+++ src/e2e/pages/manageHearings.page.js
@@ -49,92 +49,94 @@ module.exports = {
     adjournHearingDateToAvoid: '#reasonForAdjournHearing-caseListedOnDatesToAvoid',
   },
 
-  verifyPageDetails() {
-    I.retry(3).seeElement(this.fields.allocateJudgeTitle);
-    I.retry(3).seeElement(this.fields.childNameHeader);
-    I.retry(3).seeElement(this.fields.continueButton);
-    I.retry(3).seeElement(this.fields.newHearing);
-    I.retry(3).seeElement(this.fields.vacateHearing);
-    I.retry(3).seeElement(this.fields.adjournHearing);
+  async verifyPageDetails() {
+    await I.retry(3).seeElement(this.fields.allocateJudgeTitle);
+    await I.retry(3).seeElement(this.fields.childNameHeader);
+    await I.retry(3).seeElement(this.fields.continueButton);
+    await I.retry(3).seeElement(this.fields.newHearing);
+    await I.retry(3).seeElement(this.fields.vacateHearing);
+    await I.retry(3).seeElement(this.fields.adjournHearing);
   },
 
-  verifyManageHearingsPageFunctionality(){
-    I.retry(3).click(this.fields.continueButton);
-    I.retry(6).seeElement(this.fields.errorMessage);
-    I.retry(5).click(this.fields.newHearing);
-    I.retry(5).click(this.fields.continueButton);
+  async verifyManageHearingsPageFunctionality(){
+    await I.retry(3).click(this.fields.continueButton);
+    await I.retry(6).seeElement(this.fields.errorMessage);
+    await I.retry(5).click(this.fields.newHearing);
+    await I.retry(5).click(this.fields.continueButton);
   },
 
-  addNewHearingOptions(){
-     I.wait(3);
-     I.fillField(this.fields.hearingType, manageHearingFormData.newHearing.typeOfHearing);
-     I.fillField(this.fields.hearingDay, manageHearingFormData.newHearing.dayOfHearing);
-     I.fillField(this.fields.hearingMonth, manageHearingFormData.newHearing.monthOfHearing);
-     I.fillField(this.fields.hearingYear, manageHearingFormData.newHearing.yearOfHearing);
-     I.fillField(this.fields.hearingHour, manageHearingFormData.newHearing.hourOfHearing);
-     I.fillField(this.fields.hearingMinute, manageHearingFormData.newHearing.minuteOfHearing);
-     I.fillField(this.fields.hearingSeconds, manageHearingFormData.newHearing.secondOfHearing);
-     I.fillField(this.fields.lengthOfHearing, manageHearingFormData.newHearing.lengthOfHearing);
-     I.fillField(this.fields.judge, manageHearingFormData.newHearing.judgeOfHearing);
-     I.fillField(this.fields.court, manageHearingFormData.newHearing.courtOfHearing);
-     I.fillField(this.fields.interpreterRequirements, manageHearingFormData.newHearing.interpreterRequired);
-     I.retry(5).click(this.fields.remoteMethod);
-     I.fillField(this.fields.accessibilityRequirements, manageHearingFormData.newHearing.accessibilityRequired);
-     I.retry(5).click(this.fields.hearingDelay);
-     I.retry(5).click(this.fields.backupNotice);
-     I.retry(5).click(this.fields.continueButton);
+  async addNewHearingOptions(){
+     await I.wait(3);
+     await I.fillField(this.fields.hearingType, manageHearingFormData.newHearing.typeOfHearing);
+     await I.fillField(this.fields.hearingDay, manageHearingFormData.newHearing.dayOfHearing);
+     await I.fillField(this.fields.hearingMonth, manageHearingFormData.newHearing.monthOfHearing);
+     await I.fillField(this.fields.hearingYear, manageHearingFormData.newHearing.yearOfHearing);
+     await I.fillField(this.fields.hearingHour, manageHearingFormData.newHearing.hourOfHearing);
+     await I.fillField(this.fields.hearingMinute, manageHearingFormData.newHearing.minuteOfHearing);
+     await I.fillField(this.fields.hearingSeconds, manageHearingFormData.newHearing.secondOfHearing);
+     await I.fillField(this.fields.lengthOfHearing, manageHearingFormData.newHearing.lengthOfHearing);
+     await I.fillField(this.fields.judge, manageHearingFormData.newHearing.judgeOfHearing);
+     await I.fillField(this.fields.court, manageHearingFormData.newHearing.courtOfHearing);
+     await I.fillField(this.fields.interpreterRequirements, manageHearingFormData.newHearing.interpreterRequired);
+     await I.retry(5).click(this.fields.remoteMethod);
+     await I.fillField(this.fields.accessibilityRequirements, manageHearingFormData.newHearing.accessibilityRequired);
+     await I.retry(5).click(this.fields.hearingDelay);
+     await I.retry(5).click(this.fields.backupNotice);
+     await I.retry(5).click(this.fields.continueButton);
+      await I.wait(3);
   },
 
-  addRecepientDetails(){
-     I.wait(5);
-     I.retry(3).seeElement(this.fields.childNameHeader);
-     I.retry(3).seeElement(this.fields.recipientsTitle);
-     I.retry(3).see('Only select people who are party to this case and who need a copy of this order.');
-     I.retry(5).click(this.fields.firstApplicant);
-     I.retry(5).click(this.fields.secondApplicant);
-     I.retry(5).click(this.fields.recipientsBirthMother);
-     I.retry(5).click(this.fields.recipientsBirthFather);
-     I.retry(5).click(this.fields.legalGuardian);
-     I.retry(5).click(this.fields.childsLocalAuthority);
-     I.retry(5).click(this.fields.applicantsLocalAuthority);
-     I.retry(5).click(this.fields.adoptionAgency);
-     I.retry(5).click(this.fields.otherAdoptionAgency);
-     I.retry(5).click(this.fields.otherParentWithParentalResponsibility);
-     I.retry(5).click(this.fields.continueButton);
-     I.wait(3);
-     I.see("Legal guardian (CAFCASS) is not applicable");
-     I.see("Other adoption agency is not applicable");
-     I.retry(5).click(this.fields.legalGuardian);
-     I.retry(5).click(this.fields.otherAdoptionAgency);
-     I.retry(5).click(this.fields.continueButton);
-     I.wait(5);
+  async addRecepientDetails(){
+     await I.wait(5);
+     await I.retry(3).seeElement(this.fields.childNameHeader);
+     await I.retry(3).seeElement(this.fields.recipientsTitle);
+     await I.retry(3).see('Only select people who are party to this case and who need a copy of this order.');
+     await I.retry(5).click(this.fields.firstApplicant);
+     await I.retry(5).click(this.fields.secondApplicant);
+     await I.retry(5).click(this.fields.recipientsBirthMother);
+     await I.retry(5).click(this.fields.recipientsBirthFather);
+     await I.retry(5).click(this.fields.legalGuardian);
+     await I.retry(5).click(this.fields.childsLocalAuthority);
+     await I.retry(5).click(this.fields.applicantsLocalAuthority);
+     await I.retry(5).click(this.fields.adoptionAgency);
+     await I.retry(5).click(this.fields.otherAdoptionAgency);
+     await I.retry(5).click(this.fields.otherParentWithParentalResponsibility);
+     await I.retry(5).click(this.fields.continueButton);
+     await I.wait(3);
+     await I.see("Legal guardian (CAFCASS) is not applicable");
+     await I.see("Other adoption agency is not applicable");
+    await I.wait(3);
+     await I.retry(5).click(this.fields.legalGuardian);
+     await I.retry(5).click(this.fields.otherAdoptionAgency);
+     await I.retry(5).click(this.fields.continueButton);
+     await I.wait(5);
    },
 
 
-  verifyAddNewHearingCheckYourAnswers(){
-    I.wait(3);
-    I.seeTextInPage(['Enter hearing details', 'Type of hearing'], manageHearingFormData.newHearing.typeOfHearing);
-    I.seeTextInPage(['Enter hearing details', 'Hearing date & time'], '15 Oct 2025, 11:15:55 PM');
-    I.seeTextInPage(['Enter hearing details', 'Length of hearing'], manageHearingFormData.newHearing.lengthOfHearing);
-    I.seeTextInPage(['Enter hearing details', 'Judge'], manageHearingFormData.newHearing.judgeOfHearing);
-    I.seeTextInPage(['Enter hearing details', 'Court'], manageHearingFormData.newHearing.courtOfHearing);
-    I.seeTextInPage(['Enter hearing details', 'Is an interpreter needed?'], manageHearingFormData.newHearing.interpreterRequired);
-    I.seeTextInPage(['Enter hearing details', 'Method of hearing'], 'Remote (via video hearing)');
-    I.seeTextInPage(['Enter hearing details', 'Accessibility requirements'], manageHearingFormData.newHearing.accessibilityRequired);
-    I.seeTextInPage(['Enter hearing details', 'Hearing directions'], 'Hearing delay warning');
-    I.seeTextInPage(['Enter hearing details', 'Hearing directions'], 'Backup notice');
-    I.see(manageHearingFormData.checkYourAnswers.recipientApplicant1);
-    I.see(manageHearingFormData.checkYourAnswers.recipientApplicant2);
-    I.see(manageHearingFormData.checkYourAnswers.recipientBirthMother);
-    I.see(manageHearingFormData.checkYourAnswers.recipientBirthFather);
-    I.see(manageHearingFormData.checkYourAnswers.recipientChildLA);
-    I.see(manageHearingFormData.checkYourAnswers.recipientApplicantLA);
-    I.see(manageHearingFormData.checkYourAnswers.recipientAdopAgency);
-    I.see(manageHearingFormData.checkYourAnswers.recipientOtherParentalResponsibility);
-    I.see(manageHearingFormData.checkYourAnswers.addNewHearingOption);
-    I.retry(5).click(this.fields.continueButton);
-    I.wait(3);
-    I.retry(5).seeElement(this.fields.alertMessage);
+  async verifyAddNewHearingCheckYourAnswers(){
+    await I.wait(3);
+    await I.seeTextInPage(['Enter hearing details', 'Type of hearing'], manageHearingFormData.newHearing.typeOfHearing);
+    await I.seeTextInPage(['Enter hearing details', 'Hearing date & time'], '15 Oct 2025, 11:15:55 PM');
+    await I.seeTextInPage(['Enter hearing details', 'Length of hearing'], manageHearingFormData.newHearing.lengthOfHearing);
+    await I.seeTextInPage(['Enter hearing details', 'Judge'], manageHearingFormData.newHearing.judgeOfHearing);
+    await I.seeTextInPage(['Enter hearing details', 'Court'], manageHearingFormData.newHearing.courtOfHearing);
+    await I.seeTextInPage(['Enter hearing details', 'Is an interpreter needed?'], manageHearingFormData.newHearing.interpreterRequired);
+    await I.seeTextInPage(['Enter hearing details', 'Method of hearing'], 'Remote (via video hearing)');
+    await I.seeTextInPage(['Enter hearing details', 'Accessibility requirements'], manageHearingFormData.newHearing.accessibilityRequired);
+    await I.seeTextInPage(['Enter hearing details', 'Hearing directions'], 'Hearing delay warning');
+    await I.seeTextInPage(['Enter hearing details', 'Hearing directions'], 'Backup notice');
+    await I.see(manageHearingFormData.checkYourAnswers.recipientApplicant1);
+    await I.see(manageHearingFormData.checkYourAnswers.recipientApplicant2);
+    await I.see(manageHearingFormData.checkYourAnswers.recipientBirthMother);
+    await I.see(manageHearingFormData.checkYourAnswers.recipientBirthFather);
+    await I.see(manageHearingFormData.checkYourAnswers.recipientChildLA);
+    await I.see(manageHearingFormData.checkYourAnswers.recipientApplicantLA);
+    await I.see(manageHearingFormData.checkYourAnswers.recipientAdopAgency);
+    await I.see(manageHearingFormData.checkYourAnswers.recipientOtherParentalResponsibility);
+    await I.see(manageHearingFormData.checkYourAnswers.addNewHearingOption);
+    await I.retry(5).click(this.fields.continueButton);
+    await I.wait(3);
+    await I.retry(5).seeElement(this.fields.alertMessage);
   },
 
  async selectVacateHearingOptionWithAgreementAndRelisting(){
@@ -171,6 +173,7 @@ module.exports = {
     await I.retry(5).click(this.fields.hearingDelay);
     await I.retry(5).click(this.fields.backupNotice);
     await I.retry(5).click(this.fields.continueButton);
+    await I.wait(3);
  },
 
   async verifyVacateHearingWithRelistingCheckYourAnswers(){
@@ -201,11 +204,11 @@ module.exports = {
   },
 
   async genericAddNewHearing(){
-    this.verifyPageDetails();
-    this.verifyManageHearingsPageFunctionality();
-    this.addNewHearingOptions();
-    this.addRecepientDetails();
-    this.verifyAddNewHearingCheckYourAnswers();
+    await this.verifyPageDetails();
+    await this.verifyManageHearingsPageFunctionality();
+    await this.addNewHearingOptions();
+    await this.addRecepientDetails();
+    await this.verifyAddNewHearingCheckYourAnswers();
   },
 
   async selectVacateHearingOptionWithAgreementAndNoRelisting(){
@@ -227,10 +230,10 @@ module.exports = {
 
   async verifyVacateHearingNoRelistingCheckYourAnswers(){
     await I.wait(3);
-    I.see(manageHearingFormData.checkYourAnswers.vacateHearingOption);
-    I.see(manageHearingFormData.checkYourAnswers.vacateHearingName);
-    I.see(manageHearingFormData.checkYourAnswers.vacateHearingReason);
-    I.see(manageHearingFormData.checkYourAnswers.relistingNo);
+    await I.see(manageHearingFormData.checkYourAnswers.vacateHearingOption);
+    await I.see(manageHearingFormData.checkYourAnswers.vacateHearingName);
+    await I.see(manageHearingFormData.checkYourAnswers.vacateHearingReason);
+    await I.see(manageHearingFormData.checkYourAnswers.relistingNo);
     await I.retry(5).click(this.fields.continueButton);
     await I.retry(5).seeElement(this.fields.alertMessage);
   },
@@ -262,10 +265,10 @@ module.exports = {
 
     async verifyAdjournHearingNoRelistingCheckYourAnswers(){
         await I.wait(3);
-        I.see(manageHearingFormData.checkYourAnswers.adjournHearingOption);
-        I.see(manageHearingFormData.checkYourAnswers.vacateHearingName);
-        I.see(manageHearingFormData.checkYourAnswers.adjournHearingReason);
-        I.see(manageHearingFormData.checkYourAnswers.relistingNo);
+        await I.see(manageHearingFormData.checkYourAnswers.adjournHearingOption);
+        await I.see(manageHearingFormData.checkYourAnswers.vacateHearingName);
+        await I.see(manageHearingFormData.checkYourAnswers.adjournHearingReason);
+        await I.see(manageHearingFormData.checkYourAnswers.relistingNo);
         await I.retry(5).click(this.fields.continueButton);
         await I.wait(3);
         await I.retry(5).seeElement(this.fields.alertMessage);
@@ -277,7 +280,7 @@ module.exports = {
         await I.wait(3);
         await I.retry(5).click(this.fields.continueButton);
         await I.wait(3);
-        await I.retry(3).see('Reason for adjournment');
+        await I.retry(3).see('Select a hearing you want to adjourn is required');
         await I.wait(3);
         await I.retry(5).click(this.fields.vacateHearingToSelect);
         await I.retry(5).click(this.fields.continueButton);
