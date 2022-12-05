7addd5f6cf11c028a9ee98e2cc160c00aa90cf79
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2022-12-05 12:51:15 +0000

    ADOP-1260 General Direction Order Part 1 changes

---
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/page/GeneralDirectionOrders.java
@@ -0,0 +1,66 @@
+package uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page;
+
+
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.DirectionsOrderData;
+import uk.gov.hmcts.reform.adoption.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
+
+public class GeneralDirectionOrders implements CcdPageConfiguration {
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+        pageBuilder.page("manageOrderGD1")
+            .showCondition("manageOrderType=\"generalDirectionsOrder\"")
+            .pageLabel("General directions order")
+            .label("generalDirectionOrderLabel2", "## General directions order")
+            .label("generalDirectionOrderLabel1", "## Enter hearing details")
+            .complex(CaseData::getDirectionsOrderData)
+            .label("generalDirectionOrderBy", "### Order by", null, true)
+            .mandatory(DirectionsOrderData::getOrderBy)
+            .label("generalDirectionDateOfOrderMade", "### Date order made", null, true)
+            .mandatory(DirectionsOrderData::getDateOfGeneralDirectionOrderMade)
+            .label("generalDirectionPreamble", "### Preamble", null, true)
+            .optional(DirectionsOrderData::getGeneralDirectionOrderPreamble)
+            .label("generalDirectionBodyOfOrder", "### Body of the order", null, true)
+            .optional(DirectionsOrderData::getGeneralDirectionBodyOfTheOrder)
+            .label("generalDirectionCostOrders", "### Cost orders", null, true)
+            .optional(DirectionsOrderData::getGeneralDirectionCostOfOrder)
+            .label("generalDirectionIncludedInTheOrder", "### Directions to include in the order",
+                   null, true
+            )
+            .optional(DirectionsOrderData::getGeneraDirectionsIncluded)
+            .label(
+                "generalDirectionOrderTypeYouNeedToSend",
+                "### Is there another order type you need to send instead",
+                null,
+                true
+            )
+            .mandatory(DirectionsOrderData::getIsThereAnyOrderTypeYouNeedToSend)
+            .mandatory(DirectionsOrderData::getGeneralDirectionOrderTypes, "isThereAnyOrderTypeYouNeedToSend=\"Yes\"")
+            .done()
+            .page("manageOrderGD2")
+            .showCondition(
+                "manageOrderType=\"generalDirectionsOrder\" AND generalDirectionOrderTypes=\"generalDirectionProductionOrder\"")
+            .label("manageOrderGdGov", "<p>To the governor<br>"
+                + "HMP{insert address}.</p>")
+            .complex(CaseData::getDirectionsOrderData)
+            .label("manageOrderGdGovLab", "### Name of prison", null, true)
+            .mandatory(DirectionsOrderData::getGeneralDirectionNameOfPrison)
+            .label("manageOrderGdGovLabAddOfPrison", "### Address of prison", null, true)
+            .mandatory(DirectionsOrderData::getGeneralDirectionAddressOfPrison)
+            .label("manageOrderGdGovNameOfPrisoner", "<p>Please  arrange for the prisoner {insert name} <br>"
+                + "(Prisoner number:{insert prisoner number}) to attend at {insert hearing venue} on<br>"
+                + "{insert heating date} at {insert hearing time} for a hearing the above case</p>")
+            .label("manageOrderGdGovLabNameOfPrisoner", "### Name of prisoner", null, true)
+            .mandatory(DirectionsOrderData::getGeneralDirectionNameOfThePrisoner)
+            .label("manageOrderGdGovLabPrisonerNum", "### Prisoner number", null, true)
+            .mandatory(DirectionsOrderData::getGeneralDirectionPrisonerNumber)
+            .label("manageOrderGdGovLabHearingVen", "### Hearing Venue", null, true)
+            .mandatory(DirectionsOrderData::getGeneralDirectionHearingVenue)
+            .label("manageOrderGdGovDateTime", "### Hearing time and date", null, true)
+            .mandatory(DirectionsOrderData::getGeneralDirectionHearingDateTime)
+            .done();
+
+    }
+}
