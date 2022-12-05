2737de569ff77a7707c03fe45b617fbd1a8d4ee5
cshiwarale <chetan.hiwarale@hmcts.net>
2022-12-05 17:17:54 +0000

    Fixing typo issues

--- src/e2e/fixtures/checkAndSendOrderDetails.js
+++ src/e2e/fixtures/checkAndSendOrderDetails.js
@@ -4,11 +4,11 @@ module.exports = {
   selectTheToReview: 'Select the order you want to review',
   documentsToReview: 'Document to review',
   selectedRecipientsToServerOrder: 'These recipients have been selected to receive this order',
-  serveOrderOrAmendment: 'Do you want to server the order or return for amendments?',
+  serveOrderOrAmendment: 'Do you want to serve the order or return for amendments?',
   selectOrderToreview: 'Select the order you want to review',
   serverTheOrder: 'Serve the order',
   gateKeepingOrder: 'Case Management Order (gatekeeping order)',
   finalAdoptionOrder: '',
-  serverOrderError: 'Do you want to server the order or return for amendments? is required'
+  serverOrderError: 'Do you want to serve the order or return for amendments? is required'
 
 }
