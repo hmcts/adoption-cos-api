6561c3b2344ec076d2c97983760979245a2ca45d
CharchitHmcts <113587746+CharchitHmcts@users.noreply.github.com>
2022-12-06 16:15:36 +0000

    R2master (#612)
    * Fixing syntax error after terraform version upgrade
    
    * Orders view tab added (#598)
    
    * ADOP-1644: Orders view tab added
    
    * ADOP-1644: Check and send orders changes
    
    * ADOP-1644: Check and send orders changes common list
    
    * ADOP-1644: Removed unused methods
    
    * ADOP-1644: Added document review method and test cases
    
    * ADOP-1644: About to submit added
    
    * ADOP-1644: About to submit callback added
    
    * ADOP-1644  E2E tests added
    
    * ADOP-1644  E2E tests added
    
    * ADOP-1644: Removed unused import
    
    * ADOP-1644: Removed unused import
    
    * Fixing typo issues
    
    Co-authored-by: raghyn <nraghu31@gmail.com>
    Co-authored-by: cshiwarale <98115959+cshiwarale@users.noreply.github.com>
    Co-authored-by: cshiwarale <chetan.hiwarale@hmcts.net>
    
    Co-authored-by: cshiwarale <98115959+cshiwarale@users.noreply.github.com>
    Co-authored-by: raghyn <nraghu31@gmail.com>
    Co-authored-by: cshiwarale <chetan.hiwarale@hmcts.net>

--- src/e2e/pages/checkAndSendOrders.page.js
+++ src/e2e/pages/checkAndSendOrders.page.js
@@ -1,4 +1,7 @@
 const { I } = inject();
+const manageOrderDetails = require('../fixtures/manageOrderDetails.js');
+const checkAndSendOrderDetails = require('../fixtures/checkAndSendOrderDetails.js');
+const finalAdoptionOrderDetails = require("../fixtures/finalAdoptionOrderDetails.js");
 
 module.exports = {
 
@@ -8,12 +11,14 @@ module.exports = {
     ordersToReviewTitle: '//h2[text()="Orders for review"]',
     ordersToReviewSubTitle: '//span[text()="Select the order you want to review"]',
     continueButton: '//button[@type="submit"]',
-    errorMessage: '//span[@class="error-message ng-star-inserted"]',
+    errorMessage: '[field_id=\'checkAndSendOrderDropdownList\'] .error-message',
     cancelButton: '//a[text()="Cancel"]',
     ordersDropDown: '#checkAndSendOrderDropdownList',
     orderToSelect: '//option[starts-with(@value, "1")]',
-    continueButton: 'button[type="submit"]',
     alertMessage: '//div[@class="alert-message"]',
+    previewOrderDocmosisLink: '//a[text()=" A76_Final adoption order_draft.pdf "]',
+    checkAndSendOrderErrorMessage: '#orderCheckAndSend .error-message',
+    serveOrderRadioBtn: '#orderCheckAndSend-serveTheOrder'
   },
 
   async verifyCheckAndSendOrdersPageDetails() {
@@ -25,17 +30,74 @@ module.exports = {
     await I.click(this.fields.continueButton);
     await I.wait(3);
     await I.retry(3).seeElement(this.fields.errorMessage);
-    await I.click(this.fields.cancelButton);
+
+    let delivery = locate('//select[@id="checkAndSendOrderDropdownList"]/option').at(2);
+    orderType = await I.grabTextFrom(delivery);
+    await I.wait(3);
+    await I.retry(3).selectOption(this.fields.ordersDropDown, orderType);
+    await I.click(this.fields.continueButton);
     await I.wait(5);
   },
 
- async selectOrderToReview(){
-   let delivery = locate('//select[@id="checkAndSendOrderDropdownList"]/option').at(2);
-   let orderType = await I.grabTextFrom(delivery);
-   await I.wait(3);
-   await I.retry(3).selectOption(this.fields.ordersDropDown, orderType);
-   await I.click(this.fields.continueButton);
-   await I.wait(3);
+  async verifyOrderForReview() {
+    await I.wait(3);
+    await I.retry(3).see('Review Order');
+    await I.retry(3).see(checkAndSendOrderDetails.documentsToReview);
   },
+
+  async verifyOrderTypeCYA(){
+    await I.retry(3).waitForText('Check your answers', 30);
+    await I.retry(3).see('Check your answers');
+    await I.retry().see(orderType)
+  },
+
+  async serverOrderCYA(){
+    await I.retry(3).see(checkAndSendOrderDetails.selectOrderToreview);
+    await I.retry(3).see(checkAndSendOrderDetails.serveOrderOrAmendment)
+    await I.retry(3).see(checkAndSendOrderDetails.serverTheOrder);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+  },
+
+
+  async gateKeepingOrderRecipients(){
+    await I.retry(3).see(checkAndSendOrderDetails.selectedRecipientsToServerOrder);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.applicantLocalAuthority);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.birthMother);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.birthFather);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.applicants);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.childLocalAuthority);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.otherAdoptionAgency);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.otherPersonWithParentalResponsibility);
+    await I.retry(3).see(manageOrderDetails.caseManagementOrderDetails.cafCass);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+  },
+
+  async selectServerOrder() {
+    await I.wait(3);
+    await I.retry().see(checkAndSendOrderDetails.serveOrderOrAmendment)
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+    await I.retry(3).see(checkAndSendOrderDetails.serverOrderError, this.fields.checkAndSendOrderErrorMessage);
+    await I.retry(3).click(this.fields.serveOrderRadioBtn);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+  },
+
+  async FinalAdoptionOrderRecipients() {
+    await I.retry(3).seeElement(this.fields.previewOrderDocmosisLink);
+    await I.retry(3).see(checkAndSendOrderDetails.selectedRecipientsToServerOrder);
+    await I.retry(3).see(finalAdoptionOrderDetails.checkYourAnswers.firstApplicant);
+    await I.retry(3).see(finalAdoptionOrderDetails.checkYourAnswers.secondApplicant);
+    //await I.retry(3).see(finalAdoptionOrderDetails.checkYourAnswers.birthFather);
+    //await I.retry(3).see(finalAdoptionOrderDetails.checkYourAnswers.birthMother);
+    await I.retry(3).see(finalAdoptionOrderDetails.checkYourAnswers.childLocalAuthority);
+    await I.retry(3).see(finalAdoptionOrderDetails.checkYourAnswers.applicantLocalAuthority);
+    await I.retry(3).see(finalAdoptionOrderDetails.checkYourAnswers.adoptionAgency);
+    await I.retry(3).click(this.fields.continueButton);
+    await I.wait(3);
+
+  }
 
   };
