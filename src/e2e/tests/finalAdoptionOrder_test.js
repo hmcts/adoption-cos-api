44f3f481d2bfd5a3085357159e1ff0884d63733e
CharchitHmcts <113587746+CharchitHmcts@users.noreply.github.com>
2022-12-06 14:52:33 +0000

    Orders view tab added (#598)
    * ADOP-1644: Orders view tab added
    
    * ADOP-1644: Check and send orders changes
    
    * ADOP-1644: Check and send orders changes common list
    
    * ADOP-1644: Removed unused methods
    
    * ADOP-1644: Added document review method and test cases
    
    * ADOP-1644: About to submit added
    
    * ADOP-1644: About to submit callback added
    
    * ADOP-1644  E2E tests added
    
    * ADOP-1644  E2E tests added
    
    * ADOP-1644: Removed unused import
    
    * ADOP-1644: Removed unused import
    
    * Fixing typo issues
    
    Co-authored-by: raghyn <nraghu31@gmail.com>
    Co-authored-by: cshiwarale <98115959+cshiwarale@users.noreply.github.com>
    Co-authored-by: cshiwarale <chetan.hiwarale@hmcts.net>

--- src/e2e/tests/finalAdoptionOrder_test.js
+++ src/e2e/tests/finalAdoptionOrder_test.js
@@ -14,7 +14,7 @@ async function setupScenario(I) {
   await I.navigateToCaseDetailsAs(config.caseWorkerUserOne, caseId);
 }
 
-Scenario('Verify final adoption order details and CYA', async ({I, caseViewPage, manageOrdersPage, finalOrderPage}) => {
+Scenario('Verify final adoption order details Order Tab Check and Send Orders', async ({I, caseViewPage, manageOrdersPage, finalOrderPage, checkAndSendOrdersPage,manageOrderTabPage}) => {
   await setupScenario(I);
   await caseViewPage.goToNewActions(config.administrationActions.manageOrders);
   await manageOrdersPage.verifyCaseDetails();
@@ -29,6 +29,27 @@ Scenario('Verify final adoption order details and CYA', async ({I, caseViewPage,
   await finalOrderPage.finalAdoptionOrderManagementDateOrderMadeCYAPage();
   await finalOrderPage.verifyFinalAdoptionOrderRecipientsCYA();
   await I.retry(3).seeEventSubmissionConfirmation(caseId, config.administrationActions.manageOrders);
+
+  await manageOrderTabPage.selectOrderTab();
+  await manageOrderTabPage.verifyOrderDetailsUnderOrdersTabs();
+  await manageOrderTabPage.verifyFAOrderStatus()
+  await manageOrderTabPage.faOrderRecipients();
+
+
+  await caseViewPage.goToNewActions(config.administrationActions.checkAndSendOrders);
+  await checkAndSendOrdersPage.verifyCheckAndSendOrdersPageDetails();
+  await checkAndSendOrdersPage.verifyOrderForReview();
+  await checkAndSendOrdersPage.FinalAdoptionOrderRecipients();
+  await checkAndSendOrdersPage.selectServerOrder();
+  await checkAndSendOrdersPage.verifyOrderTypeCYA();
+  await checkAndSendOrdersPage.serverOrderCYA();
+  await I.retry(3).seeEventSubmissionConfirmation(caseId,config.administrationActions.checkAndSendOrders);
+
+  await manageOrderTabPage.selectOrderTab();
+  await manageOrderTabPage.verifyOrderDetailsUnderOrdersTabs();
+  await manageOrderTabPage.verifyOrderServed();
+  await manageOrderTabPage.faOrderRecipients();
+
 
 });
 
