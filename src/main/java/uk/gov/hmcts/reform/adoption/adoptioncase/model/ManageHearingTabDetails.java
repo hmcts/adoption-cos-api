aee1ec3ff80d82ea017fd9bf637e3e4323f3e6f1
Thirunavukkarasu Soundararajan <Thirunavukkarasu.Soundararajan@hmcts.net>
2022-12-14 08:56:55 +0000

    ADOP-1262-MH-Docmosis-A91

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/ManageHearingTabDetails.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/ManageHearingTabDetails.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.adoption.adoptioncase.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,68 +23,43 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedRadioList;
 public class ManageHearingTabDetails {
 
     @CCD(
-        showCondition = "methodOfHearing=\"never\"",
-        displayOrder = 1
-    )
-    private String hearingId;
-
-    @CCD(
-        label = "Type of hearing",
-        hint = "For example, a first hearing",
-        displayOrder = 2
-
+        label = "Type of hearing"
     )
     private String typeOfHearing;
 
     @CCD(
-        label = "Hearing date & time",
-        displayOrder = 3
+        label = "Hearing date & time"
     )
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime hearingDateAndTime;
 
-    @CCD(label = " Length of hearing",
-        hint = "Insert the length of the hearing in hours and minutes, for example 2 hours 30 minutes",
-        displayOrder = 4)
+    @CCD(label = " Length of hearing")
     private String lengthOfHearing;
 
     @CCD(
-        label = "Judge",
-        hint = "This should be the allocated judge, if possible.",
-        displayOrder = 5
-
+        label = "Judge"
     )
     private String judge;
 
     @CCD(
-        label = "Court",
-        hint = "Enter the full name of the court where the hearing will be held ",
-        displayOrder = 6
+        label = "Court"
     )
     private String court;
 
-
     @CCD(
-        label = "Is an interpreter needed?",
-        hint = "Given details of the interpretation needed for example, sign language or Hindu translator",
-        displayOrder = 7
+        label = "Is an interpreter needed?"
     )
     private String isInterpreterNeeded;
 
     @CCD(
         label = "Method of hearing",
         typeOverride = FixedRadioList,
-        typeParameterOverride = "MethodOfHearing",
-        displayOrder = 8
+        typeParameterOverride = "MethodOfHearing"
     )
     private MethodOfHearing methodOfHearing;
 
-
     @CCD(
         label = "Accessibility requirements",
-        typeOverride = FieldType.TextArea,
-        hint = "List any accessibility requirements needed such as disabled access",
-        displayOrder = 9
+        typeOverride = FieldType.TextArea
     )
     private String accessibilityRequirements;
 
@@ -93,30 +67,25 @@ public class ManageHearingTabDetails {
         label = "Hearing directions",
         access = {DefaultAccess.class},
         typeOverride = FieldType.MultiSelectList,
-        typeParameterOverride = "HearingDirections",
-        displayOrder = 10
+        typeParameterOverride = "HearingDirections"
     )
     private SortedSet<HearingDirections> hearingDirections;
 
-
     @CCD(
         label = "Recipients",
-        access = {DefaultAccess.class},
-        displayOrder = 11
+        access = {DefaultAccess.class}
     )
     private SortedSet<RecipientsInTheCase> recipientsInTheCase;
 
     @CCD(
         label = "Reason for vacating hearing",
-        access = { SystemUpdateAccess.class,DefaultAccess.class},
-        displayOrder = 12
+        access = { SystemUpdateAccess.class,DefaultAccess.class}
     )
     private ReasonForVacatingHearing reasonForVacatingHearing;
 
     @CCD(
         label = "Reason for adjournment",
-        access = { SystemUpdateAccess.class,DefaultAccess.class},
-        displayOrder = 13
+        access = { SystemUpdateAccess.class,DefaultAccess.class}
     )
     private ReasonForAdjournHearing reasonForAdjournHearing;
 
