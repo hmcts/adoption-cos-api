afb337e385239b39d9efa68f1a06a2a174676a16
Raghu Goud <nraghu31@gmail.com>
2022-12-15 13:10:47 +0000

    ADOP-1877 E2E new tests added

--- src/e2e/pages/sendOrReplyToMessages.page.js
+++ src/e2e/pages/sendOrReplyToMessages.page.js
@@ -9,6 +9,11 @@ module.exports = {
     messageUrgencyListDropDown: '#messageUrgencyList',
     sendMessageAttachDocumentNo: '#sendMessageAttachDocument_No',
     messageTextBox: '#messageText',
+    messageActionReplyMessage: '#messageAction-replyMessage',
+    replyMsgDynamicListDropDown: '#replyMsgDynamicList',
+    replyMessageYes: '#replyMessage_Yes',
+
+
   },
 
   async sendMessageToJudge() {
@@ -18,6 +23,28 @@ module.exports = {
     await I.retry(3).see('Do you want to send or reply to a message? is required',this.fields.messageActionError);
     await I.retry(3).click(this.fields.messageActionSendMessage);
     await I.retry(3).click('Continue');
+    await this.whoDoYouSendMessage();
+
+  },
+
+  async verifyReplayMessageToJudge(){
+    await I.retry(3).waitForText('Do you want to send or reply to a message?',30);
+    await I.retry(3).click(this.fields.messageActionReplyMessage);
+    let urgency = locate('//select[@id="replyMsgDynamicList"]/option').at(2);
+    let urgencyType = await I.grabTextFrom(urgency);
+    await I.wait(3);
+    await I.retry(3).selectOption(this.fields.replyMsgDynamicListDropDown, urgencyType);
+    await I.retry(3).click('Continue');
+    await I.wait(3);
+    await I.retry(3).waitForText('Reply to message',30);
+    await I.retry(3).click(this.fields.replyMessageYes);
+    await I.retry(3).click('Continue');
+    await I.wait(3);
+    await this.whoDoYouSendMessage();
+
+  },
+
+  async whoDoYouSendMessage(){
     await I.retry(3).waitForText('Who do you want to send a message to?',30);
     await I.retry(3).click(this.fields.messageSendDetailsMessageReceiverRolesJudge);
     let reason = locate('//select[@id="messageReasonList"]/option').at(2);
@@ -34,7 +61,6 @@ module.exports = {
     await I.retry(3).click(this.fields.sendMessageAttachDocumentNo);
     await I.retry(3).click('Continue');
     await I.wait(3);
-
   },
 
   async verifySendMessageToJudgeCYA(){
@@ -47,5 +73,14 @@ module.exports = {
     await I.click('Save and continue');
     await I.wait(3);
   },
-
+  async verifyReplyMessageToJudgeCYA(){
+    await I.retry(3).waitForText('Check your answers', 30);
+    await I.retry(3).see('Reply to a message');
+    await I.retry(3).see('Judge');
+    await I.retry(3).see('List a hearing');
+    await I.retry(3).see('High');
+    await I.retry(3).see(sendOrReplyToMessagesData.message);
+    await I.click('Save and continue');
+    await I.wait(3);
+  },
 };
