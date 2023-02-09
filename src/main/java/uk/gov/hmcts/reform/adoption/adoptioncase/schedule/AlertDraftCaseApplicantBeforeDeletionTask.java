077381a9b2c42d8f229662ed067fe12693241384
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2023-02-09 09:20:49 +0000

    updated template
    Change triggers
    
    ADOP-2132 adding new cron job changes
    
    ADOP-2132 adding new cron job changes

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertDraftCaseApplicantBeforeDeletionTask.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertDraftCaseApplicantBeforeDeletionTask.java
@@ -3,12 +3,61 @@ package uk.gov.hmcts.reform.adoption.adoptioncase.schedule;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
+import uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService;
+import uk.gov.hmcts.reform.adoption.idam.IdamService;
+import uk.gov.hmcts.reform.adoption.notification.DraftApplicationExpiringNotification;
+import uk.gov.hmcts.reform.adoption.notification.NotificationDispatcher;
+import uk.gov.hmcts.reform.adoption.systemupdate.CaseDetailsConverter;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.idam.client.models.User;
+import uk.gov.service.notify.NotificationClientException;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.existsQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Draft;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService.CREATED_DATE;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService.STATE;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class AlertDraftCaseApplicantBeforeDeletionTask implements Runnable {
+public class AlertDraftCaseApplicantBeforeDeletionTask {
+
+
+    @Autowired
+    private CcdSearchService ccdSearchService;
+
+    @Autowired
+    private IdamService idamService;
+
+    @Autowired
+    private AuthTokenGenerator authTokenGenerator;
+
+    @Autowired
+    private NotificationDispatcher notificationDispatcher;
+
+
+    @Autowired
+    private DraftApplicationExpiringNotification draftApplicationExpiringNotification;
+
+    @Autowired
+    private CaseDetailsConverter caseDetailsConverter;
+
+
     /**
      * When an objectclear implementing interface <code>Runnable</code> is used
      * to create a thread, starting the thread causes the object's
@@ -20,9 +69,40 @@ public class AlertDraftCaseApplicantBeforeDeletionTask implements Runnable {
      *
      * @see Thread#run()
      */
-    @Override
+    @Scheduled(cron = "0 0/5 * 1/1 * ?")
     public void run() {
+
+        final User user = idamService.retrieveSystemUpdateUserDetails();
+        final String serviceAuthorization = authTokenGenerator.generate();
+
+        log.info("Modified datee time : " + LocalDateTime.now().minusDays(83));
+        final BoolQueryBuilder query = boolQuery()
+            .must(matchQuery(STATE, Draft))
+            .must(existsQuery(CREATED_DATE))
+            .filter(rangeQuery(CREATED_DATE)
+                        .gte(LocalDate.now().minusDays(83))
+                        .lte(LocalDate.now().minusDays(83)));
         log.info("Scheduled task is executed");
+
+        final List<CaseDetails> casesInDraftNeedingReminder =
+            ccdSearchService.searchForAllCasesWithQuery(Draft, query, user, serviceAuthorization);
+
+        for (final CaseDetails caseDetails : casesInDraftNeedingReminder) {
+            log.info("case details are present: " + caseDetails.getId());
+            sendReminderToApplicantsIfEligible(caseDetails, user, serviceAuthorization);
+        }
+
+    }
+
+    private void sendReminderToApplicantsIfEligible(CaseDetails caseDetails, User user, String serviceAuthorization) {
+
+        uk.gov.hmcts.ccd.sdk.api.CaseDetails<CaseData, State> caseData = caseDetailsConverter.convertToCaseDetailsFromReformModel(
+            caseDetails);
+        try {
+            notificationDispatcher.send(draftApplicationExpiringNotification, caseData.getData(), caseDetails.getId());
+        } catch (NotificationClientException | IOException e) {
+            log.error("Couldn't send notifications");
+        }
 
     }
 }
--- src/test/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertDraftCaseApplicantBeforeDeletionTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertDraftCaseApplicantBeforeDeletionTaskTest.java
@@ -3,13 +3,30 @@ package uk.gov.hmcts.reform.adoption.adoptioncase.schedule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService;
+import uk.gov.hmcts.reform.adoption.idam.IdamService;
+import uk.gov.hmcts.reform.adoption.notification.NotificationDispatcher;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 @ExtendWith(SpringExtension.class)
 class AlertDraftCaseApplicantBeforeDeletionTaskTest {
 
     @InjectMocks
     private AlertDraftCaseApplicantBeforeDeletionTask alertDraftCaseApplicantBeforeDeletionTask;
+
+    @Mock
+    private CcdSearchService ccdSearchService;
+
+    @Mock
+    private IdamService idamService;
+
+    @Mock
+    private AuthTokenGenerator authTokenGenerator;
+
+    @Mock
+    private NotificationDispatcher notificationDispatcher;
 
     @Test
     void alertDraftCaseApplicantBeforeDeletionTaskTest_run() {
