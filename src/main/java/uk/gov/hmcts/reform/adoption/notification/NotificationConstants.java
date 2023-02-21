1fa8fbab49cf8e2056df74df51a0672bdf868234
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2023-02-21 17:38:31 +0000

    fixed email alert issue

--- src/main/java/uk/gov/hmcts/reform/adoption/notification/NotificationConstants.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/notification/NotificationConstants.java
@@ -4,6 +4,7 @@ public final class NotificationConstants {
 
     public static final String APPLICANT_1_FULL_NAME = "applicant1FullName";
     public static final String HAS_SECOND_APPLICANT = "hasSecondApplicant";
+    public static final String HAS_MULTIPLE_APPLICANT = "hasMultipleApplicants";
     public static final String APPLICANT_2_FULL_NAME = "applicant2FullName";
     public static final String LOCAL_COURT_NAME = "localCourtName";
 
