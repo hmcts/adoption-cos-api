29da09b95fe4b8ad627776a747e971b8e59d8272
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2023-03-22 16:29:30 +0000

    reverted change for this file: SwaggerPublisherTest as build is failing at static checks

--- src/integrationTest/java/uk/gov/hmcts/reform/adoption/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/adoption/config/SwaggerPublisherTest.java
@@ -12,7 +12,7 @@ class SwaggerPublisherTest {
 
     @Test
     public void testHealthEndpoint() {
-        String  testUrl = "http://adoption-cos-api-aat.aat.platform.hmcts.net";
+        String  testUrl = "http://adoption-cos-api-aat.service.core-compute-aat.internal";
         Assert.assertTrue(true);
         RestAssured.useRelaxedHTTPSValidation();
         RestAssured
