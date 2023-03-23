7dea4983fb5125eea079a39860c16230f6c4e42f
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-03-23 00:15:53 +0000

    Added new event for DSS Update case journey

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CaseworkerUpdateDssApplication.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CaseworkerUpdateDssApplication.java
@@ -9,7 +9,7 @@ import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
 
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Draft;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Submitted;
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions.READ_UPDATE;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions.READ;
