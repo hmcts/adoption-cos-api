5b361b62d2b467ca580c31ae8ec6377821d3b1be
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2023-04-18 15:59:59 +0100

    Revert "Revert "removed extra states""
    This reverts commit 2d54a07893fdab51f20dae35e36e7dce5841170d.

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/LocalAuthoritySubmitApplication.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/LocalAuthoritySubmitApplication.java
@@ -13,10 +13,8 @@ import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
 import uk.gov.hmcts.reform.adoption.common.service.SendNotificationService;
 import uk.gov.hmcts.reform.adoption.common.service.SubmissionService;
 
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.AwaitingAdminChecks;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.LaSubmitted;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Submitted;
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Draft;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SYSTEM_UPDATE;
@@ -39,7 +37,7 @@ public class LocalAuthoritySubmitApplication implements CCDConfig<CaseData, Stat
     public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         configBuilder
             .event(LOCAL_AUTHORITY_SUBMIT)
-            .forStates(Submitted, Draft, AwaitingAdminChecks)
+            .forStates(Submitted)
             .name("Local Authority Submit")
             .description("Local Authority Application Submit- Awaiting Admin Checks")
             .retries(120, 120)
