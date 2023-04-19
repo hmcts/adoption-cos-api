8cb650c8139e83a9c323e6ed4b4194f3215cf537
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2023-04-19 12:27:52 +0100

    moved notification to Submitted event

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/LocalAuthoritySubmitApplication.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/LocalAuthoritySubmitApplication.java
@@ -12,9 +12,8 @@ import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
 import uk.gov.hmcts.reform.adoption.common.service.SendNotificationService;
 import uk.gov.hmcts.reform.adoption.common.service.SubmissionService;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.AwaitingAdminChecks;
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Draft;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.LaSubmitted;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Submitted;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SUPER_USER;
@@ -39,26 +38,32 @@ public class LocalAuthoritySubmitApplication implements CCDConfig<CaseData, Stat
     public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         configBuilder
             .event(LOCAL_AUTHORITY_SUBMIT)
-            .forStates(Submitted, Draft, AwaitingAdminChecks)
+            .forStates(Submitted)
             .name("Local Authority Submit")
             .description("Local Authority Application Submit- Awaiting Admin Checks")
             .retries(120, 120)
             .grant(CREATE_READ_UPDATE, SYSTEM_UPDATE)
             .grant(READ, SUPER_USER, CASE_WORKER)
-            .aboutToSubmitCallback(this::aboutToSubmit);
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .submittedCallback(this::submitted);
+    }
+
+    private SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> details, CaseDetails<CaseData, State> beforeDetails) {
+        log.info("Local Authority Submit Application submitted callback invoked CaseID: {}", details.getId());
+        log.info("Invoking Notifications for CaseID: {}", details.getId());
+        sendNotificationService.sendNotifications(details);
+        return SubmittedCallbackResponse.builder().build();
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
                                                                        final CaseDetails<CaseData, State> beforeDetails) {
         final Long caseId = details.getId();
 
-        log.info("Citizen Submit Application about to submit callback invoked CaseID: {}", caseId);
+        log.info("Local Authority Submit Application about to submit callback invoked CaseID: {}", caseId);
 
         final CaseDetails<CaseData, State> updatedCaseDetails = submissionService.laSubmitApplication(details);
-        final CaseDetails<CaseData, State> notificationSentUpdatedDetails = sendNotificationService.sendNotifications(
-            updatedCaseDetails);
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(notificationSentUpdatedDetails.getData())
+            .data(updatedCaseDetails.getData())
             .state(LaSubmitted)
             .build();
 
