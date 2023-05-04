46ba1d5babc846301306077a3dafc8404236c252
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2023-05-04 17:27:02 +0100

    FPET-486 adding secrets related sendgrid

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,3 +1,8 @@
 terraform {
   backend "azurerm" {}
 }
+provider "azurerm" {
+  alias           = "sendgrid"
+  subscription_id = var.env != "prod" ? local.sendgrid_subscription.nonprod : local.sendgrid_subscription.prod
+  features {}
+}
