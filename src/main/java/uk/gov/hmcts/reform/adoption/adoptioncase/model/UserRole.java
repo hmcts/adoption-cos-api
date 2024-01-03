3d69c34c4c3b8a8739f117c9127c337b450a629d
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-01-03 14:44:39 +0000

    reverted user access to default

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/UserRole.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/UserRole.java
@@ -8,16 +8,16 @@ import uk.gov.hmcts.ccd.sdk.api.HasRole;
 @AllArgsConstructor
 @Getter
 public enum UserRole implements HasRole {
-    ADOPTION_GENERIC("caseworker-adoption", "D"),
-    CASE_WORKER("caseworker-adoption-caseworker", "D"),
-    COURT_ADMIN("caseworker-adoption-courtadmin", "D"),
-    LEGAL_ADVISOR("caseworker-adoption-la", "D"),
-    DISTRICT_JUDGE("caseworker-adoption-judge", "D"),
-    SUPER_USER("caseworker-adoption-superuser", "D"),
-    SOLICITOR("caseworker-adoption-solicitor", "D"),
-    CITIZEN("citizen", "D"),
-    CREATOR("[CREATOR]", "D"),
-    SYSTEM_UPDATE("caseworker-adoption-systemupdate", "D");//TODO REMOVE DELETE ACCESS
+    ADOPTION_GENERIC("caseworker-adoption", "RU"),
+    CASE_WORKER("caseworker-adoption-caseworker", "RU"),
+    COURT_ADMIN("caseworker-adoption-courtadmin", "R"),
+    LEGAL_ADVISOR("caseworker-adoption-la", "R"),
+    DISTRICT_JUDGE("caseworker-adoption-judge", "RU"),
+    SUPER_USER("caseworker-adoption-superuser", "CRUD"),
+    SOLICITOR("caseworker-adoption-solicitor", "R"),
+    CITIZEN("citizen", "CRUD"),
+    CREATOR("[CREATOR]", "CRU"),
+    SYSTEM_UPDATE("caseworker-adoption-systemupdate", "CRUD");//TODO REMOVE DELETE ACCESS
 
     @JsonValue
     private final String role;
