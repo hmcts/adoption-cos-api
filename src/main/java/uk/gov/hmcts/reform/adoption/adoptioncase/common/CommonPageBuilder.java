88829a44ce8a74f11851b6ca49e47cae7e3d6326
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-01-23 14:28:03 +0000

    deleted few more unused classes

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/common/CommonPageBuilder.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.adoption.adoptioncase.common;
-
-public final class CommonPageBuilder {
-
-    private static final String REPLY_MESSAGE = "messageAction=\"replyMessage\"";
-
-    private CommonPageBuilder() {
-    }
-}
