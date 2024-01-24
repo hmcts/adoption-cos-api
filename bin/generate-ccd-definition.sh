ab1736777cb063c17cc3e4355d83f75a40a11240
suresh-sharada <88669246+suresh-sharada@users.noreply.github.com>
2024-01-24 09:36:40 +0000

    TestBranch

--- bin/generate-ccd-definition.sh
+++ bin/generate-ccd-definition.sh
@@ -8,6 +8,7 @@ definition_dir=${1}
 definition_output_file=${2}
 additionalParameters=${3-}
 
+
 definition_input_dir=${definition_dir}
 
 if [[ ! -e ${definition_output_file} ]]; then
@@ -18,7 +19,7 @@ echo "Definition input directory: ${definition_input_dir}"
 echo "Definition output file: ${definition_output_file}"
 echo "Additional params: ${additionalParameters}"
 
-docker run --pull always --user $UID --rm --name json2xlsx \
+docker run --pull always --rm --name json2xlsx \
   -v ${definition_input_dir}:/tmp/ccd-definition \
   -v ${definition_output_file}:/tmp/ccd-definition.xlsx \
   hmctspublic.azurecr.io/ccd/definition-processor:${definition_processor_version} \
