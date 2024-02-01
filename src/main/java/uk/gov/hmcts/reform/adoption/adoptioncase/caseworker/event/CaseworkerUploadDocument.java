c5513ab1c4dfcf59ac1b800907edc2330648e2e4
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-02-01 14:19:42 +0000

    added back the event for fixing label issue

---
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/caseworker/event/CaseworkerUploadDocument.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.page.ManageDocuments;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions;
+import uk.gov.hmcts.reform.adoption.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.reform.adoption.common.ccd.PageBuilder;
+
+import java.time.Clock;
+
+/**
+ * This class is used to define the Manage Case Event
+ * This will enable the upload document functionality.
+ * It will also would allow user to specify the Category of document
+ */
+
+@Component
+@Slf4j
+public class CaseworkerUploadDocument implements CCDConfig<CaseData, State, UserRole> {
+
+    @Autowired
+    private Clock clock;
+
+    public static final String CASEWORKER_UPLOAD_DOCUMENT = "caseworker-manage-document";
+    public static final String MANAGE_DOCUMENT = "Manage documents";
+
+    private final CcdPageConfiguration manageDocuments = new ManageDocuments();
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        var pageBuilder = addEventConfig(configBuilder);
+        manageDocuments.addTo(pageBuilder);
+    }
+
+    private PageBuilder addEventConfig(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        configBuilder.grant(State.Draft, Permissions.READ_UPDATE, UserRole.CASE_WORKER);
+        return new PageBuilder(configBuilder
+                                   .event(CASEWORKER_UPLOAD_DOCUMENT)
+                                   .forAllStates()
+                                   .name(MANAGE_DOCUMENT)
+                                   .description(MANAGE_DOCUMENT)
+                                   .showSummary()
+                                   .grant(Permissions.CREATE_READ_UPDATE, UserRole.CASE_WORKER)
+                                   .grant(Permissions.CREATE_READ_UPDATE, UserRole.DISTRICT_JUDGE));
+    }
+}
--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CitizenCreateApplication.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CitizenCreateApplication.java
@@ -10,7 +10,6 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
-import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions;
 import uk.gov.hmcts.reform.adoption.adoptioncase.search.CaseFieldsConstants;
 import uk.gov.hmcts.reform.adoption.common.AddSystemUpdateRole;
 
@@ -34,15 +33,6 @@ public class CitizenCreateApplication implements CCDConfig<CaseData, State, User
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-
-        /*
-            Below logic is to provide access to caseworker for all the available states.
-         */
-        configBuilder.grant(State.Draft, Permissions.READ_UPDATE, UserRole.CASE_WORKER);
-        configBuilder.grant(State.AwaitingPayment, Permissions.READ_UPDATE, UserRole.CASE_WORKER);
-        configBuilder.grant(State.Submitted, Permissions.READ_UPDATE, UserRole.CASE_WORKER);
-        configBuilder.grant(State.LaSubmitted, Permissions.READ_UPDATE, UserRole.CASE_WORKER);
-
 
         var defaultRoles = new ArrayList<UserRole>();
         defaultRoles.add(CITIZEN);
