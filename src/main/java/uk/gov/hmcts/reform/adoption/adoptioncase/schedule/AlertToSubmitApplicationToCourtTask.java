da41a6c19b1bdc79e4e8387b630dfa8d0de09aab
Paul Trelease <paul.trelease@justice.gov.uk>
2024-03-04 15:50:58 +0000

    ADOP-2305-LA-Scheduler: removed remining comments to try and pass quality gate

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertToSubmitApplicationToCourtTask.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertToSubmitApplicationToCourtTask.java
@@ -10,17 +10,6 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AlertToSubmitApplicationToCourtTask implements Runnable {
 
-    /**
-     * When an objectclear implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     *
-     * <p>The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
-     */
     @Override
     public void run() {
 
