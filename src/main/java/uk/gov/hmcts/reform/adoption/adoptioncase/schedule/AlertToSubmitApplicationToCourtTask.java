4f3513324af16a6486399027c87ca90eedcc4c88
Paul Trelease <paul.trelease@justice.gov.uk>
2024-03-04 15:30:09 +0000

    ADOP-2305-LA-Scheduler: Removed all unused autowiring for now

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertToSubmitApplicationToCourtTask.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertToSubmitApplicationToCourtTask.java
@@ -2,39 +2,13 @@ package uk.gov.hmcts.reform.adoption.adoptioncase.schedule;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService;
-import uk.gov.hmcts.reform.adoption.idam.IdamService;
-import uk.gov.hmcts.reform.adoption.notification.DraftApplicationExpiringNotification;
-import uk.gov.hmcts.reform.adoption.systemupdate.CaseDetailsConverter;
-import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class AlertToSubmitApplicationToCourtTask implements Runnable {
-
-
-    @Autowired
-    private CcdSearchService ccdSearchService;
-
-    @Autowired
-    private IdamService idamService;
-
-    @Autowired
-    private AuthTokenGenerator authTokenGenerator;
-
-    @Autowired
-    private DraftApplicationExpiringNotification draftApplicationExpiringNotification;
-
-    @Autowired
-    private CaseDetailsConverter caseDetailsConverter;
-
-    @Value("${cron.alertDraftApplicant.offsetDays:69}")
-    public  int emailAlertOffsetDays;
-
 
     /**
      * When an objectclear implementing interface <code>Runnable</code> is used
