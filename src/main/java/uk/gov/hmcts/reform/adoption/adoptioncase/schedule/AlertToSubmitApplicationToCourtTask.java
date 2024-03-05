217f8d1365cef6f4d128130ed54997f90009f357
Paul Trelease <paul.trelease@justice.gov.uk>
2024-03-05 07:49:52 +0000

    Added NOSONAR exception for the test log code

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertToSubmitApplicationToCourtTask.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertToSubmitApplicationToCourtTask.java
@@ -4,17 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-
 @Component
-@Slf4j
+@Slf4j  //NOSONAR test code to check CRON fires
 @RequiredArgsConstructor
 public class AlertToSubmitApplicationToCourtTask implements Runnable {
 
     @Override
-    public void run() {
-
-        log.info("AlertLAToSubmitApplicationToCourtTask is firing");
-
+    public void run() { //NOSONAR test code to check CRON fires
+        log.info("AlertLAToSubmitApplicationToCourtTask is firing"); //NOSONAR test code to check CRON fires
     }
 
 }
