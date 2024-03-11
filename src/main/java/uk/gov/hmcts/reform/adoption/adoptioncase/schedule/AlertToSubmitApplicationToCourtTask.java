39e1d25cbc9e4d2d9513d050dadb9a2907e78a24
Paul Trelease <paul.trelease@justice.gov.uk>
2024-03-11 14:10:05 +0000

    ADOP-2305-LA-Scheduler: Added boilerplate to start getting task ready

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertToSubmitApplicationToCourtTask.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertToSubmitApplicationToCourtTask.java
@@ -2,16 +2,87 @@ package uk.gov.hmcts.reform.adoption.adoptioncase.schedule;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
+import uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService;
+import uk.gov.hmcts.reform.adoption.idam.IdamService;
+import uk.gov.hmcts.reform.adoption.notification.LocalAuthorityAlertToSubmitToCourt;
+import uk.gov.hmcts.reform.adoption.systemupdate.CaseDetailsConverter;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.idam.client.models.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.existsQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Draft;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService.CREATED_DATE;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService.STATE;
+
 
 @Component
 @Slf4j  //NOSONAR test code to check CRON fires
 @RequiredArgsConstructor
 public class AlertToSubmitApplicationToCourtTask implements Runnable {
 
+    @Autowired
+    private CcdSearchService ccdSearchService;
+
+    @Autowired
+    private IdamService idamService;
+
+    @Autowired
+    private AuthTokenGenerator authTokenGenerator;
+
+    @Autowired
+    private LocalAuthorityAlertToSubmitToCourt localAuthorityAlertToSubmitToCourt;
+
+    @Autowired
+    private CaseDetailsConverter caseDetailsConverter;
+
+    @Value("${cron.alertDraftApplicant.offsetDays:69}")
+    public  int emailAlertOffsetDays;
+
     @Override
     public void run() { //NOSONAR test code to check CRON fires
+
         log.info("AlertLAToSubmitApplicationToCourtTask is firing"); //NOSONAR test code to check CRON fires
+
+        final User user = idamService.retrieveSystemUpdateUserDetails();
+        final String serviceAuthorization = authTokenGenerator.generate();
+
+        final BoolQueryBuilder query = boolQuery()
+                .must(matchQuery(STATE, Draft))
+                .must(existsQuery(CREATED_DATE))
+                .filter(rangeQuery(CREATED_DATE)
+                        .gte(LocalDate.now().minusDays(emailAlertOffsetDays))
+                        .lte(LocalDate.now().minusDays(emailAlertOffsetDays)));
+        log.info("Scheduled task is executed");
+
+        final List<CaseDetails> casesInDraftNeedingReminder =
+                ccdSearchService.searchForAllCasesWithQuery(Draft, query, user, serviceAuthorization);
+
+        for (final CaseDetails caseDetails : casesInDraftNeedingReminder) {
+            log.info("case details are present: " + caseDetails.getId());
+//            sendLocalAuthorityAlertToSubmitToCourt(caseDetails);
+        }
+
+    }
+
+    private void sendLocalAuthorityAlertToSubmitToCourt(CaseDetails caseDetails) {
+
+        uk.gov.hmcts.ccd.sdk.api.CaseDetails<CaseData, State> caseData = caseDetailsConverter.convertToCaseDetailsFromReformModel(
+                caseDetails);
+        localAuthorityAlertToSubmitToCourt.sendLocalAuthorityAlertToSubmitToCourt(caseData.getData(), caseDetails.getId());
+
     }
 
 }
