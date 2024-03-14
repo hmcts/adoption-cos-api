af069187651a120a342769d0ddec6ebd89ca8cdd
Paul Trelease <paul.trelease@justice.gov.uk>
2024-03-14 11:29:25 +0000

    linting for LoclAuthoritySubmitToCourt

--- src/main/java/uk/gov/hmcts/reform/adoption/notification/LocalAuthorityAlertToSubmitToCourt.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/notification/LocalAuthorityAlertToSubmitToCourt.java
@@ -9,17 +9,21 @@ import uk.gov.hmcts.reform.adoption.adoptioncase.model.LanguagePreference;
 import uk.gov.hmcts.reform.adoption.common.config.EmailTemplatesConfig;
 import uk.gov.hmcts.reform.adoption.idam.IdamService;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
+import static uk.gov.hmcts.reform.adoption.adoptioncase.search.CaseFieldsConstants.BLANK_SPACE;
+import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.DATE_SUBMITTED;
 import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.HYPHENATED_REF;
 import static uk.gov.hmcts.reform.adoption.notification.EmailTemplateName.LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT;
-import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.CHILD_FULL_NAME;
-import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.LA_PORTAL_URL;
+import static uk.gov.hmcts.reform.adoption.notification.FormatUtil.DATE_TIME_FORMATTER;
+import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.*;
 
 @Component
 @Slf4j
-// todo - still OK to implement this interface - does everything?
+// todo - still OK to implement this interface - does everything?  Field injection not good.
 public class LocalAuthorityAlertToSubmitToCourt implements ApplicantNotification {
 
     @Autowired
@@ -29,13 +33,7 @@ public class LocalAuthorityAlertToSubmitToCourt implements ApplicantNotification
     private NotificationService notificationService;
 
     @Autowired
-    private CommonContent commonContent;
-
-    @Autowired
     private EmailTemplatesConfig emailTemplatesConfig;
-
-
-
 
     @Override
     public void sendLocalAuthorityAlertToSubmitToCourt(final CaseData caseData, final Long id) {
@@ -47,25 +45,31 @@ public class LocalAuthorityAlertToSubmitToCourt implements ApplicantNotification
         notificationService.sendEmail(
                 childLocalAuthorityEmailAddress,
                 LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT,
-                templateVarsForLocalAuthority(caseData),
+                getTemplateVarsForLocalAuthority(caseData),
                 LanguagePreference.ENGLISH
         );
 
         notificationService.sendEmail(
                 applicantLocalAuthorityEmailAddress,
                 LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT,
-                templateVarsForLocalAuthority(caseData),
+                getTemplateVarsForLocalAuthority(caseData),
                 LanguagePreference.ENGLISH
         );
     }
 
-    // Todo: This has been copied from ApplicationSubmittedNotification.  Should be a util?
-    // Sonar lint is highlighting this issue, line 63
-    private Map<String, Object> templateVarsForLocalAuthority(CaseData caseData) {
+    // This has been copied from ApplicationSubmittedNotification.  Should be a util?
+    // The original function has linting issues that this version addresses.
+    private Map<String, Object> getTemplateVarsForLocalAuthority(CaseData caseData) {
         Map<String, Object> templateVars = new HashMap<>();
-        templateVars.put(HYPHENATED_REF, caseData.getHyphenatedCaseRef());
-        String applicantFullName = caseData.getChildren().getFirstName() + " " + caseData.getChildren().getLastName();
-        templateVars.put(CHILD_FULL_NAME, StringUtils.replace(applicantFullName, "'","\\'"));
+
+        String hyphenatedCaseRef = caseData.getHyphenatedCaseRef();
+        String familyCourtName = caseData.getFamilyCourtName();
+
+        templateVars.put(HYPHENATED_REF, hyphenatedCaseRef);
+        templateVars.put(LOCAL_COURT_NAME, familyCourtName);
+        templateVars.put(CHILD_FULL_NAME, caseData.getChildren().getFirstName() + BLANK_SPACE + caseData.getChildren().getLastName());
+        templateVars.put(DATE_SUBMITTED, Optional.ofNullable(caseData.getApplication().getDateSubmitted())
+                .orElse(LocalDate.now()).format(DATE_TIME_FORMATTER));
         templateVars.put(LA_PORTAL_URL, emailTemplatesConfig.getTemplateVars().get(LA_PORTAL_URL));
         return templateVars;
     }
