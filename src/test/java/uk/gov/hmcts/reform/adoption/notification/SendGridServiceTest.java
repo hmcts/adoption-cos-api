3e28d431a13ff6098757d765a5221b054a90dc0d
hfilmore <hannah.filmore@justice.gov.uk>
2024-04-03 11:17:44 +0100

    [ADOP-2324] added new test to increase code coverage

--- src/test/java/uk/gov/hmcts/reform/adoption/notification/SendGridServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/notification/SendGridServiceTest.java
@@ -34,9 +34,10 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
@@ -105,6 +106,7 @@ class SendGridServiceTest {
         Assertions.assertThrows(ArithmeticException.class, () -> {
             sendgridService.sendEmail(caseData, subject, DocumentType.APPLICATION_LA_SUMMARY_EN);
         });  //TODO revert following testing in preview
+        verify(sendgridService, times(1)).exceptionCauser();
 
         verify(caseDocumentClient, times(2)).getDocumentBinary(anyString(), anyString(),any());
     }
@@ -277,9 +279,94 @@ class SendGridServiceTest {
     }
 
     @Test
-    void removeThisTest_exceptionCauser() { //TODO remove
+    void sendEmail_throws_whenSendGridApithrows() throws IOException {
+        String caseId = "1234-1234-1234-1234";
+        CaseData caseData = caseData();
+        caseData.setHyphenatedCaseRef(caseId);
+
+        //extract into method in TestDataHelper? Pass caseData it should be added to?
+        AdoptionDocument adoptionDocumentDocmosis = new AdoptionDocument();
+        adoptionDocumentDocmosis.setDocumentType(DocumentType.APPLICATION_LA_SUMMARY_EN);
+        adoptionDocumentDocmosis.setDocumentFileId("5fc03087-d265-11e7-b8c6-83e29cd24f4c");
+        ListValue<AdoptionDocument> listValue = new ListValue<>();
+        listValue.setValue(adoptionDocumentDocmosis);
+        List<ListValue<AdoptionDocument>> listAdoptionDocument = new ArrayList<>();
+        listAdoptionDocument.add(listValue);
+        caseData.setDocumentsGenerated(listAdoptionDocument);
+
+        //extract into another method in TestDataHelper? Pass caseData it should be added to?
+        Document document = new Document();
+        document.setFilename("TEST_FILE_NAME");
+        document.setUrl("TEST_URL/5fc03087-d265-11e7-b8c6-83e29cd24f4c");
+        AdoptionDocument laUploadedDocument = new AdoptionDocument();
+        laUploadedDocument.setDocumentLink(document);
+        ListValue<AdoptionDocument> documentListValue = new ListValue<>();
+        documentListValue.setValue(laUploadedDocument);
+        List<ListValue<AdoptionDocument>> laUploadedDocumentList = new ArrayList<>();
+        laUploadedDocumentList.add(documentListValue);
+        caseData.setLaDocumentsUploaded(laUploadedDocumentList);
+
+        ResponseEntity<Resource> resource = new ResponseEntity<Resource>(
+            new ByteArrayResource(new byte[]{}), HttpStatus.OK);
+        when(idamService.retrieveSystemUpdateUserDetails()).thenReturn(new User(StringUtils.EMPTY, UserDetails.builder().build()));
+        when(authTokenGenerator.generate()).thenReturn(StringUtils.EMPTY);
+        when(caseDocumentClient.getDocumentBinary(anyString(), anyString(),any())).thenReturn(resource);
+
+        String caseIdForLogging = "1234123412341234";
+        when(sendgridService.getSendGrid(caseIdForLogging)).thenReturn(sendGrid);
+        when(sendGrid.api(any(Request.class))).thenThrow(IOException.class);
+
+        String subject = "TEST_SUBJECT";
+        Assertions.assertThrows(IOException.class, () -> {
+            sendgridService.sendEmail(caseData, subject, DocumentType.APPLICATION_LA_SUMMARY_EN);
+        });
+        verify(sendgridService, times(0)).exceptionCauser();
+    }
+
+    @Test
+    void removeThisTest_exceptionCauser() throws IOException { //TODO remove
+        String caseId = "1234-1234-1234-1234";
+        CaseData caseData = caseData();
+        caseData.setHyphenatedCaseRef(caseId);
+
+        //extract into method in TestDataHelper? Pass caseData it should be added to?
+        AdoptionDocument adoptionDocumentDocmosis = new AdoptionDocument();
+        adoptionDocumentDocmosis.setDocumentType(DocumentType.APPLICATION_LA_SUMMARY_EN);
+        adoptionDocumentDocmosis.setDocumentFileId("5fc03087-d265-11e7-b8c6-83e29cd24f4c");
+        ListValue<AdoptionDocument> listValue = new ListValue<>();
+        listValue.setValue(adoptionDocumentDocmosis);
+        List<ListValue<AdoptionDocument>> listAdoptionDocument = new ArrayList<>();
+        listAdoptionDocument.add(listValue);
+        caseData.setDocumentsGenerated(listAdoptionDocument);
+
+        //extract into another method in TestDataHelper? Pass caseData it should be added to?
+        Document document = new Document();
+        document.setFilename("TEST_FILE_NAME");
+        document.setUrl("TEST_URL/5fc03087-d265-11e7-b8c6-83e29cd24f4c");
+        AdoptionDocument laUploadedDocument = new AdoptionDocument();
+        laUploadedDocument.setDocumentLink(document);
+        ListValue<AdoptionDocument> documentListValue = new ListValue<>();
+        documentListValue.setValue(laUploadedDocument);
+        List<ListValue<AdoptionDocument>> laUploadedDocumentList = new ArrayList<>();
+        laUploadedDocumentList.add(documentListValue);
+        caseData.setLaDocumentsUploaded(laUploadedDocumentList);
+
+        ResponseEntity<Resource> resource = new ResponseEntity<Resource>(
+            new ByteArrayResource(new byte[]{}), HttpStatus.OK);
+        when(idamService.retrieveSystemUpdateUserDetails()).thenReturn(new User(StringUtils.EMPTY, UserDetails.builder().build()));
+        when(authTokenGenerator.generate()).thenReturn(StringUtils.EMPTY);
+        when(caseDocumentClient.getDocumentBinary(anyString(), anyString(),any())).thenReturn(resource);
+
+        String caseIdForLogging = "1234123412341234";
+        when(sendgridService.getSendGrid(caseIdForLogging)).thenReturn(sendGrid);
+        Response response = new Response();
+        response.setStatusCode(200);
+        when(sendGrid.api(any(Request.class))).thenReturn(response);
+        doThrow(ArithmeticException.class).when(sendgridService).exceptionCauser();
+
+        String subject = "TEST_SUBJECT";
         Assertions.assertThrows(ArithmeticException.class, () -> {
-            sendgridService.exceptionCauser();
+            sendgridService.sendEmail(caseData, subject, DocumentType.APPLICATION_LA_SUMMARY_EN);
         });
     }
 }
