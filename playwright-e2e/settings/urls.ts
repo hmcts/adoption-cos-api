bd778ae430b5c03a5489165112a65f04e3c44308
Abigail Smith <abigail.smith@hmcts.net>
2024-05-02 12:17:36 +0100

    install playwright

--- playwright-e2e/settings/urls.ts
+++ playwright-e2e/settings/urls.ts
@@ -4,6 +4,10 @@ interface UrlConfig {
   [key: string]: string;
 }
 
+test.describe('Smoke', ( => {
+  let 
+}))
+
 export const urlConfig: UrlConfig = {
   idamUrl: process.env.AAT_IDAM_URL || `https://idam-api.${env}.platform.hmcts.net`,
   laPortalUrl: process.env.LA_BASE_URL || `https://adoption-web.${env}.platform.hmcts.net/la-portal/kba-case-ref/`,
