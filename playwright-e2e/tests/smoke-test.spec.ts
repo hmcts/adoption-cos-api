e701060820c18ac29ac992c79b8bbdf96c29f215
gavinkeys <gdkeys6@gmail.com>
2024-05-30 15:45:52 +0100

    Updated smoke test and got it running

--- playwright-e2e/tests/smoke-test.spec.ts
+++ playwright-e2e/tests/smoke-test.spec.ts
@@ -4,20 +4,20 @@ import { expect, test } from '@playwright/test';
 import { urlConfig } from '../settings/urls';
 
 test.describe('Adoption', () => {
-  test('should not have any automatically detectable accessibility issues @accessibility', async ({ page }) => {
-    await page.goto(`${urlConfig.laPortalUrl}`);
+  test('should not have any automatically detectable accessibility issues', { tag: ['@accessibility'] }, async ({ page }) => {
+      await page.goto(`${urlConfig.laPortalUrl}`);
 
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22a', 'wcag22aa'])
-      .analyze();
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22a', 'wcag22aa'])
+        .analyze();
 
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
 });
 
-test('has title @smoke-test', async ({ page }) => {
+test('Smoke Test', { tag: ['@smoke-test'] }, async ({ page }) => {
   await page.goto(`${urlConfig.laPortalUrl}`);
 
-  // Expect a title "to contain" a substring.
+  // Expect a title 'to contain' a substring.
   await expect(page).toHaveTitle(/Apply for adoption - Application details - GOV.UK/);
 });
