a104881e6989924009a8491ec694a95fe1c59f18
hfilmore <hannah.filmore@justice.gov.uk>
2024-11-08 12:06:36 +0000

    [ADOP-2544] Removed unused states

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/State.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/State.java
@@ -14,64 +14,29 @@ public enum State {
     Draft("Draft"),
 
     @CCD(
-        label = "Submitted",
-        hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
-    )
-    Submitted("Awaiting LA input"),
-
-    @CCD(
         label = "Application awaiting payment",
         hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
     )
     AwaitingPayment("AwaitingPayment"),
 
     @CCD(
-        label = "Awaiting Admin checks",
+        label = "Submitted",
         hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
     )
-    AwaitingAdminChecks("AwaitingAdminChecks"),
+    Submitted("Awaiting LA input"), //TODO name???
 
     @CCD(
-        label = "Awaiting admin check",
+        label = "LA Submitted",
         hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
     )
     LaSubmitted("LaSubmitted"),
 
-    @CCD(
-        label = "Awaiting admin check",
-        hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
-    )
-    SUBMITTED_AWAITING_ADMIN_CHECK("Awaiting admin check"),
-
-    @CCD(
-        label = "Gatekeeping",
-        hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
-    )
-    JUDICIAL_REVIEW("Gatekeeping"),
-
-    @CCD(
-        label = "Withdrawn",
-        hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
-    )
-    CASE_WITHDRAWN("Withdrawn"),
-
-    @CCD(
-        label = "Hearing",
-        hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
-    )
-    PREPARE_FOR_HEARING_CONDUCT_HEARING("Hearing"),
-
-    @CCD(
-        label = "Closed",
-        hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
-    )
-    ALL_FINAL_ORDERS_ISSUED("Closed"),
-
-    @CCD(
-        label = "Awaiting Applicant Input",
-        hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
-    )
-    AWAITING_RESUBMISSION_TO_HMCTS("Awaiting Applicant Input"),
+    //TODO remove?
+    //@CCD(
+    //    label = "Hearing",
+    //    hint = "### #${[CASE_REFERENCE]}\n ### Child's name: ${childrenFirstName} ${childrenLastName}\n"
+    //)
+    //PREPARE_FOR_HEARING_CONDUCT_HEARING("Hearing"),
     ;
 
     private final String name;
