cbec55a755a3ff1ca1ca681e7461ccb4438558dc
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2024-11-25 15:44:36 +0000

    KEEP the state + citizen event changes

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CitizenAddPayment.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/CitizenAddPayment.java
@@ -24,7 +24,6 @@ import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Draft;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.State.Submitted;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CITIZEN;
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.DISTRICT_JUDGE;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions.CREATE_READ_UPDATE;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions.READ;
@@ -54,7 +53,6 @@ public class CitizenAddPayment implements CCDConfig<CaseData, State, UserRole> {
             .retries(120, 120)
             .grant(CREATE_READ_UPDATE, CITIZEN)
             .grant(READ, SUPER_USER, CASE_WORKER)
-            .grant(CREATE_READ_UPDATE, DISTRICT_JUDGE)
             .aboutToSubmitCallback(this::aboutToSubmit)
             .submittedCallback(this::submitted);
     }
---
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/access/state/SubmittedAccess.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.adoption.adoptioncase.model.access.state;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
+import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
+import uk.gov.hmcts.ccd.sdk.api.HasRole;
+import uk.gov.hmcts.ccd.sdk.api.Permission;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.access.Permissions;
+
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CREATOR;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SYSTEM_UPDATE;
+
+public class SubmittedAccess implements HasAccessControl {
+
+    @Override
+    public SetMultimap<HasRole, Permission> getGrants() {
+        SetMultimap<HasRole, Permission> grants = HashMultimap.create();
+        grants.putAll(CREATOR, Permissions.READ);
+        grants.putAll(CITIZEN, Permissions.READ);
+        grants.putAll(CASE_WORKER, Permissions.READ);
+        grants.putAll(SYSTEM_UPDATE, Permissions.CREATE_READ_UPDATE);
+
+        return grants;
+    }
+}
