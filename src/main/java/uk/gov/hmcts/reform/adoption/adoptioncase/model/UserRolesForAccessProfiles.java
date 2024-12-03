7f9a58e42e77834ae2ab26c3a6921aecbd95ff02
hfilmore <hannah.filmore@justice.gov.uk>
2024-12-03 15:30:50 +0000

    [ADOP-2551] increase caseworker permissions to enable event Next Step dropdown

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/UserRolesForAccessProfiles.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/UserRolesForAccessProfiles.java
@@ -12,7 +12,7 @@ import uk.gov.hmcts.ccd.sdk.api.HasRole;
 @Getter
 public enum UserRolesForAccessProfiles implements HasRole {
     ADOPTION_GENERIC("idam:caseworker-adoption", "RU"),
-    CASE_WORKER("idam:caseworker-adoption-caseworker", "RU"),
+    CASE_WORKER("idam:caseworker-adoption-caseworker", "CRU"),
     COURT_ADMIN("idam:caseworker-adoption-courtadmin", "R"),
     LEGAL_ADVISOR("idam:caseworker-adoption-la", "R"),
     DISTRICT_JUDGE("idam:caseworker-adoption-judge", "RU"),
@@ -21,7 +21,7 @@ public enum UserRolesForAccessProfiles implements HasRole {
     CITIZEN("idam:citizen", "CRUD"),
     CREATOR("[CREATOR]", "CRU"),
     SYSTEM_UPDATE("idam:caseworker-adoption-systemupdate", "CRUD"),
-    TTL_MANAGER("idam:cft-ttl-manager", "R");
+    TTL_MANAGER("idam:cft-ttl-manager", "CRU");
 
     @JsonValue
     private final String role;
