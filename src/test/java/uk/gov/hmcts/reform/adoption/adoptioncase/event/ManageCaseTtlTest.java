d18a7654aa3aff2694c374557610dc47e018ec2d
hfilmore <hannah.filmore@justice.gov.uk>
2024-12-12 15:24:41 +0000

    [ADOP-2551] updates to ManageCaseTtl Test.

--- src/test/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/ManageCaseTtlTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/adoptioncase/event/ManageCaseTtlTest.java
@@ -1,55 +1,32 @@
 package uk.gov.hmcts.reform.adoption.adoptioncase.event;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
-import uk.gov.hmcts.ccd.sdk.type.TTL;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole;
 
-import java.time.LocalDate;
-
 import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.*;
-import static uk.gov.hmcts.reform.adoption.adoptioncase.caseworker.event.CaseworkerCaseNote.CASEWORKER_ADD_CASE_NOTE;
-import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.event.ManageCaseTtl.MANAGE_CASE_TTL;
 
+@ExtendWith(MockitoExtension.class)
 class ManageCaseTtlTest extends EventTest {
 
     @InjectMocks
     private ManageCaseTtl manageCaseTtl;
 
-    //@Test
-    void shouldSetUpManageCaseTtlEvent() throws Exception {
+    @Test
+    void shouldSetUpManageCaseTtlEvent() {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
         manageCaseTtl.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CASEWORKER_ADD_CASE_NOTE);
+            .contains(MANAGE_CASE_TTL);
     }
-
-    private CaseDetails<CaseData, State> setupCaseWithTtl(YesOrNo suspendTtl, LocalDate overrideTtl) {
-        final var details = new CaseDetails<CaseData, State>();
-        final var data = caseData();
-        TTL ttl = new TTL();
-        ttl.setSystemTTL(LocalDate.of(2150,1,1));
-        if (suspendTtl != null) {
-            ttl.setSuspended(suspendTtl);
-        }
-        if (overrideTtl != null) {
-            ttl.setOverrideTTL(overrideTtl);
-        }
-        data.setRetainAndDisposeTimeToLive(ttl);
-        details.setData(data);
-        details.setId(1L);
-
-        return details;
-    }
-
 }
