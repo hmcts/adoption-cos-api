9e084878190eac165611606e3582b7fa97e611b0
hfilmore <hannah.filmore@justice.gov.uk>
2024-12-17 16:01:30 +0000

    [ADOP-2551] removed unused imports

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/access/TtlAccess.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/model/access/TtlAccess.java
@@ -6,8 +6,6 @@ import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.CASE_WORKER;
-import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.SYSTEM_UPDATE;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.UserRole.TTL_PROFILE;
 
 public class TtlAccess implements HasAccessControl {
