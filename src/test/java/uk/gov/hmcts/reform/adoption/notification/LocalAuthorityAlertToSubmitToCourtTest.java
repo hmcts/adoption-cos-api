e91a49af1d5b3f3cc588c1e578f775da0a1e453b
hfilmore <hannah.filmore@justice.gov.uk>
2025-03-20 18:31:56 +0000

    [ADOP-2305] happy path unit test

--- src/test/java/uk/gov/hmcts/reform/adoption/notification/LocalAuthorityAlertToSubmitToCourtTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/notification/LocalAuthorityAlertToSubmitToCourtTest.java
@@ -1,29 +1,33 @@
 package uk.gov.hmcts.reform.adoption.notification;
 
 import org.apache.commons.lang.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-//import uk.gov.hmcts.reform.adoption.adoptioncase.model.Applicant;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.Children;
+import uk.gov.hmcts.reform.adoption.adoptioncase.model.SocialWorker;
 import uk.gov.hmcts.reform.adoption.common.config.EmailTemplatesConfig;
 import uk.gov.hmcts.reform.adoption.idam.IdamService;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.HashMap;
 import java.util.Map;
 
-//import static org.mockito.Mockito.times;
-//import static org.mockito.Mockito.verify;
-//import static uk.gov.hmcts.reform.adoption.adoptioncase.model.LanguagePreference.ENGLISH;
-//import static uk.gov.hmcts.reform.adoption.adoptioncase.model.LanguagePreference.WELSH;
-//import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.NO;
-//import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.YES;
-//import static uk.gov.hmcts.reform.adoption.notification.EmailTemplateName.MULTI_CHILD_SUBMIT_APPLICATION_EMAIL_ALERT;
-//import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.*;
-//import static uk.gov.hmcts.reform.adoption.testutil.TestConstants.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.model.LanguagePreference.ENGLISH;
+import static uk.gov.hmcts.reform.adoption.adoptioncase.search.CaseFieldsConstants.BLANK_SPACE;
+import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.DATE_SUBMITTED;
+import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.HYPHENATED_REF;
+import static uk.gov.hmcts.reform.adoption.notification.EmailTemplateName.LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT;
+import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.CHILD_FULL_NAME;
+import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.LA_PORTAL_URL;
+import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.LOCAL_COURT_NAME;
 import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,53 +49,66 @@ class LocalAuthorityAlertToSubmitToCourtTest {
     @InjectMocks
     private LocalAuthorityAlertToSubmitToCourt localAuthorityAlertToSubmitToCourt;
 
+    private CaseData caseData;
 
-    @Test // Will complete this test, and write others, when the semantics finalised.
-    void localAuthorityAlertToSubmitToCourtTest_sendLocalAuthorityAlertToSubmitToCourt() {
-
-        CaseData caseData = caseData();
+    @BeforeEach
+    void setUp() {
+        caseData = caseData();
         caseData.setDueDate(LocalDate.of(2021, 4, 21));
         caseData.setFamilyCourtName(StringUtils.EMPTY);
 
         // For case data:
         // Need to add a child social worker then email address
+        SocialWorker childSocialWorker = new SocialWorker();
+        childSocialWorker.setLocalAuthorityEmail("child-sw@local-authority.gov.uk");
+        caseData.setChildSocialWorker(childSocialWorker);
 
         // Need to add an applicant social worker then email address
+        SocialWorker applicantSocialWorker = new SocialWorker();
+        applicantSocialWorker.setLocalAuthorityEmail("applicant-sw@local-authority.gov.uk");
+        caseData.setApplicantSocialWorker(applicantSocialWorker);
 
         // Need to add hyphenated case ref
+        caseData.setHyphenatedCaseRef("1234-2234-3234-4234");
 
         // Need to add child and fname sname
+        Children child = new Children();
+        child.setFirstName("Child First");
+        child.setLastName("Child Last");
+        child.setDateOfBirth(LocalDate.of(2023, Month.FEBRUARY, 1));
+        caseData.setChildren(child);
 
-        // Need to add date submitted / AND A SECOND TEST WITH NO DATE SUBMITTED
+        // Need to add date submitted / AND TODO A SECOND TEST WITH NO DATE SUBMITTED
+        caseData.getApplication().setDateSubmitted(LocalDate.of(2025, Month.MARCH, 1));
 
-        // Need to add LA Portal URL
+        // TODO Need to add LA Portal URL???
 
+    }
+
+    @Test
+    void localAuthorityAlertToSubmitToCourtTest_sendLocalAuthorityAlertToSubmitToCourt() {
         // Need to populate template vars, Hardcode them to match the values added to the case.
-
-
-        // This is code from a different test - use as an example for template vars.
         Map<String, Object> templateVars = new HashMap<>();
-        //        templateVars.put(APPLICANT_1_FULL_NAME, caseData.getApplicant1().getFirstName() + " "
-        //                + caseData.getApplicant1().getLastName());
-        //        if (caseData.getApplicant2() != null) {
-        //            templateVars.put(
-        //                    APPLICANT_2_FULL_NAME,
-        //                    caseData.getApplicant2().getFirstName() + " " + caseData.getApplicant2().getLastName()
-        //            );
-        //            templateVars.put(HAS_SECOND_APPLICANT, YES);
-        //        } else {
-        //            templateVars.put(HAS_SECOND_APPLICANT, NO);
-        //            templateVars.put(APPLICANT_2_FULL_NAME, StringUtils.EMPTY);
-        //        }
-        //        templateVars.put(HAS_MULTIPLE_APPLICANT, YES);
+        templateVars.put(HYPHENATED_REF, "1234-2234-3234-4234");
+        templateVars.put(LOCAL_COURT_NAME, StringUtils.EMPTY);
+        templateVars.put(CHILD_FULL_NAME, "Child First" + BLANK_SPACE + "Child Last");
+        templateVars.put(DATE_SUBMITTED, "1 March 2025");
+        templateVars.put(LA_PORTAL_URL, /*TEST_LA_PORTAL_URL*/ null); //TODO
 
-        localAuthorityAlertToSubmitToCourt.sendLocalAuthorityAlertToSubmitToCourt(caseData, 1234567890123456L);
+        localAuthorityAlertToSubmitToCourt.sendLocalAuthorityAlertToSubmitToCourt(caseData, 1234223432344234L);
 
-        //        verify(notificationService, times(2)).sendEmail(
-        //            TEST_USER_EMAIL,
-        //            MULTI_CHILD_SUBMIT_APPLICATION_EMAIL_ALERT,
-        //            templateVars,
-        //            ENGLISH
-        //        );
+        verify(notificationService, times(1)).sendEmail(
+            "child-sw@local-authority.gov.uk",
+            LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT,
+            templateVars,
+            ENGLISH
+        );
+
+        verify(notificationService, times(1)).sendEmail(
+            "applicant-sw@local-authority.gov.uk",
+            LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT,
+            templateVars,
+            ENGLISH
+        );
     }
 }
