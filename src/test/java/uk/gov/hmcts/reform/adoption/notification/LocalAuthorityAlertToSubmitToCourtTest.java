922fb84346bc94553e92cf27c33eb65b1241e018
hfilmore <hannah.filmore@justice.gov.uk>
2025-03-21 10:38:24 +0000

    [ADOP-2305] add LA_PORTAL_URL to test and tidy comments

--- src/test/java/uk/gov/hmcts/reform/adoption/notification/LocalAuthorityAlertToSubmitToCourtTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/notification/LocalAuthorityAlertToSubmitToCourtTest.java
@@ -11,7 +11,6 @@ import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.Children;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.SocialWorker;
 import uk.gov.hmcts.reform.adoption.common.config.EmailTemplatesConfig;
-import uk.gov.hmcts.reform.adoption.idam.IdamService;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -20,28 +19,22 @@ import java.util.Map;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.adoption.adoptioncase.model.LanguagePreference.ENGLISH;
-import static uk.gov.hmcts.reform.adoption.adoptioncase.search.CaseFieldsConstants.BLANK_SPACE;
 import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.DATE_SUBMITTED;
 import static uk.gov.hmcts.reform.adoption.document.DocumentConstants.HYPHENATED_REF;
 import static uk.gov.hmcts.reform.adoption.notification.EmailTemplateName.LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT;
 import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.CHILD_FULL_NAME;
 import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.LA_PORTAL_URL;
 import static uk.gov.hmcts.reform.adoption.notification.NotificationConstants.LOCAL_COURT_NAME;
+import static uk.gov.hmcts.reform.adoption.testutil.TestConstants.TEST_LA_PORTAL_URL;
 import static uk.gov.hmcts.reform.adoption.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class LocalAuthorityAlertToSubmitToCourtTest {
 
-
-    @Mock
-    IdamService idamService;
-
     @Mock
     private NotificationService notificationService;
-
-    @Mock
-    private CommonContent commonContent;
 
     @Mock
     private EmailTemplatesConfig emailTemplatesConfig;
@@ -51,63 +44,59 @@ class LocalAuthorityAlertToSubmitToCourtTest {
 
     private CaseData caseData;
 
+    private Map<String, String> templateVarsValues;
+
     @BeforeEach
     void setUp() {
+        templateVarsValues = new HashMap<>();
+        templateVarsValues.put(LA_PORTAL_URL, TEST_LA_PORTAL_URL);
+
         caseData = caseData();
-        caseData.setDueDate(LocalDate.of(2021, 4, 21));
+        caseData.setHyphenatedCaseRef("1234-2234-3234-4234");
+        // TODO A SECOND TEST WITH NO DATE SUBMITTED
+        caseData.getApplication().setDateSubmitted(LocalDate.of(2025, Month.MARCH, 1));
         caseData.setFamilyCourtName(StringUtils.EMPTY);
 
-        // For case data:
-        // Need to add a child social worker then email address
         SocialWorker childSocialWorker = new SocialWorker();
         childSocialWorker.setLocalAuthorityEmail("child-sw@local-authority.gov.uk");
         caseData.setChildSocialWorker(childSocialWorker);
 
-        // Need to add an applicant social worker then email address
         SocialWorker applicantSocialWorker = new SocialWorker();
         applicantSocialWorker.setLocalAuthorityEmail("applicant-sw@local-authority.gov.uk");
         caseData.setApplicantSocialWorker(applicantSocialWorker);
 
-        // Need to add hyphenated case ref
-        caseData.setHyphenatedCaseRef("1234-2234-3234-4234");
-
-        // Need to add child and fname sname
         Children child = new Children();
         child.setFirstName("Child First");
         child.setLastName("Child Last");
         child.setDateOfBirth(LocalDate.of(2023, Month.FEBRUARY, 1));
         caseData.setChildren(child);
-
-        // Need to add date submitted / AND TODO A SECOND TEST WITH NO DATE SUBMITTED
-        caseData.getApplication().setDateSubmitted(LocalDate.of(2025, Month.MARCH, 1));
-
-        // TODO Need to add LA Portal URL???
-
     }
 
     @Test
     void localAuthorityAlertToSubmitToCourtTest_sendLocalAuthorityAlertToSubmitToCourt() {
-        // Need to populate template vars, Hardcode them to match the values added to the case.
-        Map<String, Object> templateVars = new HashMap<>();
-        templateVars.put(HYPHENATED_REF, "1234-2234-3234-4234");
-        templateVars.put(LOCAL_COURT_NAME, StringUtils.EMPTY);
-        templateVars.put(CHILD_FULL_NAME, "Child First" + BLANK_SPACE + "Child Last");
-        templateVars.put(DATE_SUBMITTED, "1 March 2025");
-        templateVars.put(LA_PORTAL_URL, /*TEST_LA_PORTAL_URL*/ null); //TODO
+        // Stub LA Portal URL
+        when(emailTemplatesConfig.getTemplateVars()).thenReturn(templateVarsValues);
+
+        Map<String, Object> expectedTemplateVars = new HashMap<>();
+        expectedTemplateVars.put(HYPHENATED_REF, "1234-2234-3234-4234");
+        expectedTemplateVars.put(LOCAL_COURT_NAME, StringUtils.EMPTY);
+        expectedTemplateVars.put(CHILD_FULL_NAME, "Child First Child Last");
+        expectedTemplateVars.put(DATE_SUBMITTED, "1 March 2025");
+        expectedTemplateVars.put(LA_PORTAL_URL, TEST_LA_PORTAL_URL);
 
         localAuthorityAlertToSubmitToCourt.sendLocalAuthorityAlertToSubmitToCourt(caseData, 1234223432344234L);
 
         verify(notificationService, times(1)).sendEmail(
             "child-sw@local-authority.gov.uk",
             LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT,
-            templateVars,
+            expectedTemplateVars,
             ENGLISH
         );
 
         verify(notificationService, times(1)).sendEmail(
             "applicant-sw@local-authority.gov.uk",
             LOCAL_AUTHORITY_SUBMIT_TO_COURT_ALERT,
-            templateVars,
+            expectedTemplateVars,
             ENGLISH
         );
     }
