50c68777e1b044018faf0913d76364bf4ef4f6ea
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2025-04-28 13:32:17 +0100

    Fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/adoption/adoptioncase/service/MigrateCaseServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/adoptioncase/service/MigrateCaseServiceTest.java
@@ -1,19 +1,18 @@
 package uk.gov.hmcts.reform.adoption.adoptioncase.service;
 
-import java.util.List;
-import java.util.UUID;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.document.model.AdoptionDocument;
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
