a2db1044a8c55f20b510a24ee0d10d4453f898d5
Alex McAusland <a.m@user.com>
2025-10-16 12:58:24 +0100

    fix

--- src/contractTest/java/uk/gov/hmcts/reform/adoption/CaseDocumentClientPactTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/adoption/CaseDocumentClientPactTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.adoption.document.CaseDocumentClient;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
@@ -40,7 +40,7 @@ public class CaseDocumentClientPactTest {
     private static final String AUTH_TOKEN = "Bearer someAuthToken";
     private static final Boolean PERMANENT = false;
 
-    @MockBean
+    @MockitoBean
     private AuthTokenGenerator authTokenGenerator;
     @Autowired
     private CaseDocumentClient caseDocumentClient;
--- src/test/java/uk/gov/hmcts/reform/adoption/BaseTest.java
+++ src/test/java/uk/gov/hmcts/reform/adoption/BaseTest.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.adoption;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,6 +12,6 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 @ContextConfiguration(classes = Adoption.class)
 @TestPropertySource(locations = "/application-contract.properties")
 public abstract class BaseTest {
-    @MockBean
+    @MockitoBean
     protected AuthTokenGenerator authTokenGenerator;
 }
