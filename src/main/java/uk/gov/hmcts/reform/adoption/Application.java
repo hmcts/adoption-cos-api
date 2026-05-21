978ca8203df1710861033e1951cad17041a47b23
Steve Newman <115482991+NewmanJustice@users.noreply.github.com>
2026-05-21 12:35:15 +0000

    reduced length of comment.

--- src/main/java/uk/gov/hmcts/reform/adoption/Application.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/Application.java
@@ -21,7 +21,8 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 This change is a temporary compatibility patch so the app can run on the newer Spring Boot version.
 
-Spring Boot 4 changed how it discovers “automatic setup” from libraries. ccd-case-document-am-client tries to load an outdated health-check class, which crashes startup. So the PR does three things to avoid that crash:
+Spring Boot 4 changed how it discovers “automatic setup” from libraries. ccd-case-document-am-client 
+tries to load an outdated health-check class, which crashes startup. So the PR does three things to avoid that crash:
 
  1. Application.java: removes direct wiring to the old client class/package from that library.
  2. AutoConfiguration.imports: adds the library’s auto-config class in Boot 4’s new format (so Spring can recognise it in the new system).
