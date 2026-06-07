2d1708035893f9b00351dcb897425ab24606cf0d
Michael1142 <michael1142@hotmail.co.uk>
2026-06-07 20:37:45 +0100

    Refactor multi-child submit alert grouping

--- src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertMultiChildApplicationToSubmitTask.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/adoptioncase/schedule/AlertMultiChildApplicationToSubmitTask.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.reform.adoption.adoptioncase.schedule;
 
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.CaseData;
 import uk.gov.hmcts.reform.adoption.adoptioncase.model.State;
@@ -17,10 +15,9 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.idam.client.models.User;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.existsQuery;
@@ -33,39 +30,19 @@ import static uk.gov.hmcts.reform.adoption.adoptioncase.service.CcdSearchService
 @RequiredArgsConstructor
 public class AlertMultiChildApplicationToSubmitTask implements Runnable {
 
+    private final CcdSearchService ccdSearchService;
 
-    @Autowired
-    private CcdSearchService ccdSearchService;
+    private final IdamService idamService;
 
-    @Autowired
-    private IdamService idamService;
+    private final AuthTokenGenerator authTokenGenerator;
 
-    @Autowired
-    private AuthTokenGenerator authTokenGenerator;
+    private final MultiChildSubmitAlertEmailNotification multiChildSubmitAlertEmailNotification;
 
-    @Autowired
-    private MultiChildSubmitAlertEmailNotification multiChildSubmitAlertEmailNotification;
+    private final CaseDetailsConverter caseDetailsConverter;
 
-    @Autowired
-    private CaseDetailsConverter caseDetailsConverter;
-
-
-    /**
-     * When an objectclear implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     *
-     * <p>The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
-     */
     @Override
     public void run() {
         final User user = idamService.retrieveSystemUpdateUserDetails();
-        log.info("Idam user name: {}", user.getUserDetails().getEmail());
-
         final String serviceAuthorization = authTokenGenerator.generate();
 
         final BoolQueryBuilder query = boolQuery()
@@ -74,47 +51,36 @@ public class AlertMultiChildApplicationToSubmitTask implements Runnable {
                         .gte(LocalDate.now())
                         .lte(LocalDate.now()));
 
-        log.info("AlertMultiChildApplicationToSubmitTask Scheduled task is executed");
+        log.info("AlertMultiChildApplicationToSubmitTask scheduled task is executed");
 
         final List<CaseDetails> casesInDraftNeedingReminder =
             ccdSearchService.searchForAllCasesWithQuery(Draft, query, user, serviceAuthorization);
 
-        Map<String, List<CaseDetails>> emailCounts = new HashMap<>();
+        final Map<String, List<CaseDetails>> casesByApplicantEmail =
+            casesInDraftNeedingReminder.stream()
+                .filter(caseDetails -> caseDetails.getData().get("applicant1Email") != null)
+                .collect(Collectors.groupingBy(
+                    caseDetails -> (String) caseDetails.getData().get("applicant1Email")
+                ));
 
-        for (final CaseDetails caseDetails : casesInDraftNeedingReminder) {
-            log.info("AlertMultiChildApplicationToSubmitTask case details are present: {}", caseDetails.getId());
+        log.info(
+            "Checking the case lists of {} unique applicant1Emails for Draft multi-child cases",
+            casesByApplicantEmail.size()
+        );
 
-            String applicantEmail = (String) caseDetails.getData().get("applicant1Email");
-
-            emailCounts
-                .computeIfAbsent(applicantEmail, email -> getNewCaseList())
-                .add(caseDetails);
-        }
-
-        log.info("case list size {}", emailCounts.size());
-
-        emailCounts.values().stream()
+        casesByApplicantEmail.values().stream()
             .filter(caseList -> caseList.size() > 1)
             .flatMap(List::stream)
-            .filter(caseDetails -> State.Draft.toString().equals(caseDetails.getState()))
             .forEach(caseDetails -> {
-                log.info("state of the case {} for case id {}", caseDetails.getId(), caseDetails.getState());
                 sendReminderToApplicantsIfEligible(caseDetails);
                 log.info("Attempted to send reminder for case id {}", caseDetails.getId());
             });
     }
 
-    private List<CaseDetails> getNewCaseList() {
-        return new ArrayList<>();
-    }
-
-    private void sendReminderToApplicantsIfEligible(CaseDetails caseDetails) {
-
-        uk.gov.hmcts.ccd.sdk.api.CaseDetails<CaseData, State> caseData =
+    private void sendReminderToApplicantsIfEligible(final CaseDetails caseDetails) {
+        final uk.gov.hmcts.ccd.sdk.api.CaseDetails<CaseData, State> caseData =
             caseDetailsConverter.convertToCaseDetailsFromReformModel(caseDetails);
 
         multiChildSubmitAlertEmailNotification.sendToApplicants(caseData.getData(), caseDetails.getId());
     }
-
-
 }
